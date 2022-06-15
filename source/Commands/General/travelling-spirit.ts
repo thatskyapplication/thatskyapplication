14d694067cf2887768c006aec8ac22f297f21f81
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-06-15 17:40:55 +0100

    fix(travelling-spirit): Correct parameter and image

--- source/Commands/General/travelling-spirit.ts
+++ source/Commands/General/travelling-spirit.ts
@@ -28,9 +28,9 @@ export default class implements AutocompleteCommand {
 
     const embed = new EmbedBuilder();
     embed.setColor((await interaction.guild.members.fetch(Caelus.user.id)).displayColor);
-    embed.setImage(travellingSpirit.image);
+    embed.setImage(`${travellingSpirit.image}?cb=${Date.now()}`);
     embed.setTitle(`${formatEmoji(travellingSpirit.season.emoji)} ${name}`);
-    embed.setURL(`${travellingSpirit.url}?cd=${Date.now()}`);
+    embed.setURL(travellingSpirit.url);
     await interaction.reply({ embeds: [embed] });
   }
 
