3995d951b31691249e60fbbc90226307ae8d9b01
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-10-23 21:42:50 +0100

    feat: Seasonal candles calculator (#15)

--- source/Commands/index.ts
+++ source/Commands/index.ts
@@ -6,19 +6,19 @@ import { ApplicationCommandType } from "discord.js";
 import notifications from "./Events/notifications.js";
 
 // General
+import calculate from "./General/calculate.js";
 import roles from "./General/roles.js";
 import Sky_Profile from "./General/Sky Profile.js";
 import sky_profile from "./General/sky-profile.js";
 import spirit from "./General/spirit.js";
-import winged_light from "./General/winged-light.js";
 
 const commands = {
+	calculate: new calculate(),
 	notifications: new notifications(),
 	roles: new roles(),
 	"Sky Profile": new Sky_Profile(),
 	"sky-profile": new sky_profile(),
 	spirit: new spirit(),
-	"winged-light": new winged_light(),
 } as const;
 
 export type CommandName = keyof typeof commands;
