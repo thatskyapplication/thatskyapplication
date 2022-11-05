250382b98ef0b21e0c58f486b0b88626818c5524
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-11-05 19:05:50 +0000

    refactor: No startup message

--- source/Events/ready.ts
+++ source/Events/ready.ts
@@ -1,40 +1,13 @@
 import process from "node:process";
 import type { Client } from "discord.js";
-import { EmbedBuilder, Events, time, TimestampStyles } from "discord.js";
-import fetch from "node-fetch";
+import { Events } from "discord.js";
 import Notification from "../Structures/Notification.js";
 import Profile from "../Structures/Profile.js";
 import Rotations from "../Structures/Rotations.js";
-import { REPOSITORY, STARTUP_MESSAGE } from "../Utility/Constants.js";
 import { consoleLog } from "../Utility/Utility.js";
 import type { Event } from "./index.js";
 
-interface Commit {
-	message: string;
-}
-
-interface GitHubAuthor {
-	login: string;
-	avatar_url: string;
-	html_url: string;
-}
-
-interface GitHubCommit {
-	sha: string;
-	commit: Commit;
-	html_url: string;
-	author: GitHubAuthor;
-}
-
-interface GitHubError {
-	message: string;
-	documentation_url: string;
-}
-
 const name = Events.ClientReady;
-const branch = process.env.BRANCH;
-const commitHash = process.env.COMMIT_HASH;
-const githubToken = process.env.GITHUB_TOKEN;
 
 async function collectFromDatabase(client: Client<true>) {
 	try {
@@ -68,32 +41,6 @@ export const event: Event<typeof name> = {
 	once: true,
 	async fire(client) {
 		await collectFromDatabase(client);
-		if (!branch || !commitHash || !githubToken) {
-			return client.log(STARTUP_MESSAGE);
-		}
-
-		const json = await fetch(`https://api.github.com/repos/${REPOSITORY}/commits/${commitHash}`, {
-			headers: {
-				Accept: "application/vnd.github.v3+json",
-				Authorization: `token ${githubToken}`,
-			},
-		}).then(async (response) => response.json()) as GitHubCommit | GitHubError;
-
-		if ("message" in json) {
-			return client.log(STARTUP_MESSAGE);
-		}
-
-		const embed = new EmbedBuilder();
-
-		embed.setAuthor({
-			iconURL: json.author.avatar_url,
-			name: json.author.login,
-			url: json.author.html_url,
-		});
-
-		embed.setDescription(`Running [\`${commitHash.slice(0, 7)}\`](${json.html_url}) on [\`${branch}\`](https://github.com/${REPOSITORY}/tree/${encodeURIComponent(branch)}) at ${time(Math.floor(Date.now() / 1_000), TimestampStyles.ShortDateTime)}.\n${json.commit.message}`);
-		embed.setTimestamp();
 		await client.applyCommands();
-		await client.log({ embeds: [embed] });
 	},
 };
--- source/Utility/Constants.ts
+++ source/Utility/Constants.ts
@@ -1,5 +1,3 @@
-export const REPOSITORY = "Jiralite/Caelus" as const;
-export const STARTUP_MESSAGE = "Twirling in the air~" as const;
 export const LOG_CHANNEL_ID = "994581628804403250" as const;
 export const WIKI_URL = "https://sky-children-of-the-light.fandom.com/wiki/" as const;
 export const MAXIMUM_WINGED_LIGHT = 206 as const;
