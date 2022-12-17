9e8262f3d6752a0f79f12d7b85cb60a5f8c73095
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-12-17 16:27:44 +0000

    feat(bonk): More messages!

--- source/Commands/General/bonk.ts
+++ source/Commands/General/bonk.ts
@@ -2,6 +2,21 @@ import type { ApplicationCommandData, ChatInputCommandInteraction } from "discor
 import { ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
 import type { ChatInputCommand } from "../index.js";
 
+const bonkMessages = [
+	"{{bonker}} bonked {{bonkee}}. Bonk bonk bonk.",
+	"An ethereal force bonked {{bonkee}} on the head.",
+	"{{bonker}} proceeded to bonk {{bonkee}}, but slipped and fell on some bananas. The only thing they bonked was their own head.",
+	"BONK! {{bonkee}} was destroyed.",
+	"A terrifying, mystical force bonked {{bonkee}}. {{bonker}} is pleased.",
+	"{{bonker}} attempted to bonk {{bonkee}}. {{bonker}} was successful.",
+	"There was a 1% chance that {{bonkee}} would be bonked. {{bonker}} is that 1%. BONK.",
+	"How extraordinary! A MASSIVE, HUGE BONK was applied to the forehead of {{bonkee}} by {{bonker}}.",
+	"{{bonker}} took a flight, hiked mountains, conquered deserts, all to bonk {{bonkee}}. It was probably worth it.",
+	"Due to unfortunate weather conditions, {{bonker}} fell into a hole. They did not manage to bonk {{bonkee}}. {{bonkee}} fell into a separate hole, though.",
+	"{{bonker}} has bonked {{bonkee}}. Finally! You deserve it, my friend.",
+	"{{bonker}} bonked {{bonkee}}. This is the only physical touch you'll ever receive.",
+] as const;
+
 export default class implements ChatInputCommand {
 	public readonly name = "bonk";
 
@@ -10,10 +25,11 @@ export default class implements ChatInputCommand {
 	public async chatInput(interaction: ChatInputCommandInteraction) {
 		const { options } = interaction;
 		const user = options.getUser("user", true);
-		const member = options.getMember("user");
 
 		await interaction.reply(
-			member === null ? `An ethereal force bonked ${user} on the head.` : `${user} was bonked. Bonk bonk bonk.`,
+			bonkMessages[Math.floor(Math.random() * bonkMessages.length)]
+				.replaceAll("{{bonker}}", String(interaction.user))
+				.replaceAll("{{bonkee}}", String(user)),
 		);
 	}
 
