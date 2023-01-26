bb546b42c512b95f0baeb6bc80320f83c1d14929
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-01-26 06:56:00 +0000

    feat: add high-five

--- source/Structures/DailyGuides.ts
+++ source/Structures/DailyGuides.ts
@@ -223,6 +223,7 @@ export default new (class DailyGuides {
 		if (upperPureContent.includes("KNOCK OVER 5 DARK CREATURE")) return "Knock over 5 Dark Creatures Crabs";
 		if (upperPureContent.includes("FOLLOW A FRIEND")) return "Follow a Friend";
 		if (upperPureContent.includes("HUG A FRIEND")) return "Hug a Friend";
+		if (upperPureContent.includes("HIGH-FIVE")) return "High-Five a Friend";
 		if (upperPureContent.includes("EXPRESSION NEAR A FRIEND")) return "Use an Expression Near a Friend";
 		if (upperPureContent.includes("RIDE A MANTA")) return "Ride a Manta";
 		if (upperPureContent.includes("LIGHT BLOOM")) return "Recharge from a Light Bloom";
