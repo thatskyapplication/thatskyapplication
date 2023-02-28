5b5040b0332003ff0b05df46975c83c3ec7ac2cb
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-02-28 20:21:30 +0000

    fix: use GitHub Token

--- source/Utility/externalAPIs.ts
+++ source/Utility/externalAPIs.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import { makeURLSearchParams } from "discord.js";
 import { request } from "undici";
 import { RESOURCES_VERSION } from "./Constants.js";
@@ -22,6 +23,9 @@ export const enum WaifuCategory {
 	Slap = "slap",
 }
 
+const githubToken = process.env.GITHUB_TOKEN;
+if (!githubToken) throw new Error("No GitHub token.");
+
 export async function waifu(endpoint: WaifuCategory): Promise<WaifuResponse> {
 	return (await request(`https://api.waifu.pics/sfw/${endpoint}`)).body.json();
 }
@@ -33,7 +37,13 @@ export async function fetchResources(type: ResourceType) {
 				`https://api.github.com/repos/thatskyapplication/resources/contents/${type}?${makeURLSearchParams({
 					ref: RESOURCES_VERSION,
 				})}`,
-				{ headers: { "user-agent": "Caelus", "x-github-api-version": "2022-11-28" } },
+				{
+					headers: {
+						authorization: `Bearer ${githubToken}`,
+						"user-agent": "Caelus",
+						"x-github-api-version": "2022-11-28",
+					},
+				},
 			)
 		).body.json()) as GitHubContentResponse[]
 	).map(({ name, download_url }) => ({ name, downloadURL: download_url }));
