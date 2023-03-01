a0dff1b5b4347701bab4077dc98fc9951a31dec8
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-03-01 11:55:06 +0000

    feat: Heart history (#28)

--- source/Commands/Fun/heart.ts
+++ source/Commands/Fun/heart.ts
@@ -5,6 +5,12 @@ import type {
 	UserContextMenuCommandInteraction,
 } from "discord.js";
 import {
+	ButtonInteraction,
+	ButtonStyle,
+	ActionRowBuilder,
+	ButtonBuilder,
+	userMention,
+	EmbedBuilder,
 	time,
 	TimestampStyles,
 	PermissionFlagsBits,
@@ -22,6 +28,16 @@ interface HeartPacket {
 	timestamp: Date;
 }
 
+export const enum HeartHistoryNavigationType {
+	Back,
+	Forward,
+}
+
+interface HeartHistoryOptions {
+	type: HeartHistoryNavigationType;
+	timestamp: number;
+}
+
 const HEARTS = [
 	"{{gifter}} sent a heart to {{giftee}}. How lucky!",
 	"A heart from {{gifter}} to {{giftee}}. That was nice of them!",
@@ -33,6 +49,9 @@ const HEARTS = [
 	"{{gifter}}, sending a heart each day keeps the dark dragon away from {{giftee}}!",
 	"A wholesome heart delivered to {{giftee}} from {{gifter}}!",
 ] as const satisfies Readonly<string[]>;
+
+export const HEART_HISTORY_BACK = "HEART_HISTORY_BACK" as const;
+export const HEART_HISTORY_FORWARD = "HEART_HISTORY_FORWARD" as const;
 
 export default class implements ChatInputCommand {
 	public readonly name = "heart";
@@ -46,6 +65,9 @@ export default class implements ChatInputCommand {
 				return;
 			case "gift":
 				await this.gift(interaction);
+				break;
+			case "history":
+				await this.history(interaction);
 		}
 	}
 
@@ -156,12 +178,128 @@ export default class implements ChatInputCommand {
 		);
 	}
 
+	public async history(interaction: ChatInputCommandInteraction) {
+		await this.heartHistory(interaction);
+	}
+
+	public async heartHistory(
+		interaction: ButtonInteraction | ChatInputCommandInteraction,
+		options: HeartHistoryOptions | null = null,
+	) {
+		const buttonInteraction = interaction instanceof ButtonInteraction;
+
+		const hearts = (
+			await pg<HeartPacket>(Table.Hearts)
+				.select()
+				.where({ gifter_id: interaction.user.id })
+				.orWhere({ giftee_id: interaction.user.id })
+		).reverse();
+
+		if (hearts.length === 0) {
+			const response = {
+				components: [],
+				content: `You have ${resolveCurrencyEmoji({ interaction, emoji: Emoji.Heart, number: 0 })}.`,
+				embeds: [],
+				ephemeral: true,
+			};
+
+			if (buttonInteraction) {
+				await interaction.update(response);
+			} else {
+				await interaction.reply(response);
+			}
+
+			return;
+		}
+
+		const heartsFiltered = hearts
+			.filter((heart) => {
+				switch (options?.type) {
+					case HeartHistoryNavigationType.Back:
+						return heart.timestamp.getTime() > options.timestamp;
+					case HeartHistoryNavigationType.Forward:
+						return heart.timestamp.getTime() < options.timestamp;
+					default:
+						return true;
+				}
+			})
+			.slice(0, 24);
+
+		const embed = new EmbedBuilder()
+			.setColor((await interaction.guild?.members.fetchMe())?.displayColor ?? 0)
+			.setDescription(
+				`Gifted: ${resolveCurrencyEmoji({
+					interaction,
+					emoji: Emoji.Heart,
+					number: hearts.filter((heart) => heart.gifter_id === interaction.user.id).length,
+				})}\nReceived: ${resolveCurrencyEmoji({
+					interaction,
+					emoji: Emoji.Heart,
+					number: hearts.filter((heart) => heart.giftee_id === interaction.user.id).length,
+				})}`,
+			)
+			.setFields(
+				heartsFiltered.map((heart) => {
+					const gifted = heart.gifter_id === interaction.user.id;
+
+					return {
+						name: gifted ? "Gifted" : "Received",
+						value: `${userMention(gifted ? heart.giftee_id : heart.gifter_id)}\n${time(
+							Math.floor(heart.timestamp.getTime() / 1_000),
+							TimestampStyles.ShortDate,
+						)}\n(${time(Math.floor(heart.timestamp.getTime() / 1_000), TimestampStyles.RelativeTime)})`,
+						inline: true,
+					};
+				}),
+			)
+			.setTitle("Heart History");
+
+		const actionRow = new ActionRowBuilder<ButtonBuilder>();
+		const firstTimestamp = heartsFiltered.at(0)?.timestamp.getTime();
+		const lastTimestamp = heartsFiltered.at(-1)?.timestamp.getTime();
+
+		if (firstTimestamp && firstTimestamp < hearts.at(0)!.timestamp.getTime()) {
+			const button = new ButtonBuilder()
+				.setCustomId(`${HEART_HISTORY_BACK}-${firstTimestamp}`)
+				.setEmoji("◀️")
+				.setStyle(ButtonStyle.Primary);
+
+			actionRow.addComponents(button);
+		}
+
+		if (lastTimestamp && lastTimestamp > hearts.at(-1)!.timestamp.getTime()) {
+			const button2 = new ButtonBuilder()
+				.setCustomId(`${HEART_HISTORY_FORWARD}-${lastTimestamp}`)
+				.setEmoji("▶️")
+				.setStyle(ButtonStyle.Primary);
+
+			actionRow.addComponents(button2);
+		}
+
+		const response = {
+			components: actionRow.components.length > 0 ? [actionRow] : [],
+			embeds: [embed],
+			ephemeral: true,
+		};
+
+		if (buttonInteraction) {
+			await interaction.update(response);
+		} else {
+			await interaction.reply(response);
+		}
+	}
+
 	public get commandData(): ApplicationCommandData {
 		return {
 			name: this.name,
 			description: "Feeling generous? You have one heart to give per day!",
 			type: this.type,
 			options: [
+				{
+					type: ApplicationCommandOptionType.Subcommand,
+					name: "count",
+					description: "Count the number of hearts you have!",
+				},
 				{
 					type: ApplicationCommandOptionType.Subcommand,
 					name: "gift",
@@ -177,8 +315,8 @@ export default class implements ChatInputCommand {
 				},
 				{
 					type: ApplicationCommandOptionType.Subcommand,
-					name: "count",
-					description: "Count the number of hearts you have!",
+					name: "history",
+					description: "Display a history of your hearts!",
 				},
 			],
 			dmPermission: false,
