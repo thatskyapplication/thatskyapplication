afe57dfcfd4f4ae1f08a11b4ebb4335e365f2b99
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-14 16:29:21 +0100

    chore: Cleanup removed GitHub token

--- source/Utility/externalAPIs.ts
+++ source/Utility/externalAPIs.ts
@@ -1,4 +1,3 @@
-import process from "node:process";
 import { request } from "undici";
 
 interface WaifuResponse {
@@ -9,9 +8,6 @@ export const enum WaifuCategory {
 	Kick = "kick",
 	Slap = "slap",
 }
-
-const githubToken = process.env.GITHUB_TOKEN;
-if (!githubToken) throw new Error("No GitHub token.");
 
 export async function waifu(endpoint: WaifuCategory): Promise<WaifuResponse> {
 	return (await request(`https://api.waifu.pics/sfw/${endpoint}`)).body.json();
