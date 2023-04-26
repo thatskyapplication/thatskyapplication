48072d3d33b27a0caefc675db8b75d44c9af5b23
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-26 12:52:36 +0100

    fix: Handle names for AI

--- source/Events/messageCreate.ts
+++ source/Events/messageCreate.ts
@@ -1,12 +1,22 @@
 import process from "node:process";
 import { MessageType, type Message, Events } from "discord.js";
-import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from "openai";
+import {
+	ChatCompletionRequestMessageRoleEnum,
+	Configuration,
+	OpenAIApi,
+	type ChatCompletionRequestMessage,
+} from "openai";
 import DailyGuides from "../Structures/DailyGuides.js";
 import type { Event } from "./index.js";
 
 const name = Events.MessageCreate;
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY! });
 const openAI = new OpenAIApi(configuration);
+
+function parseAIName(input: string) {
+	const cleaned = input.replaceAll(/[^\w-]/g, "");
+	return cleaned.length >= 1 && cleaned.length <= 64 ? cleaned : null;
+}
 
 async function AIResponse(message: Message<true>) {
 	try {
@@ -19,14 +29,19 @@ async function AIResponse(message: Message<true>) {
 						role: ChatCompletionRequestMessageRoleEnum.System,
 						content: `You are ${message.client.user.username}. You are a kind, girly character that is upbeat based on the game Sky: Children of the Light. You are in a Discord server. Use emojis if you want. You are created by Jiralite.`,
 					},
-					...message.channel.messages.cache.map((message) => ({
-						content: message.content,
-						name: message.member?.displayName ?? message.author.username,
-						role:
-							message.author.id === message.client.user.id
-								? ChatCompletionRequestMessageRoleEnum.Assistant
-								: ChatCompletionRequestMessageRoleEnum.User,
-					})),
+					...message.channel.messages.cache.map((message) => {
+						const chatCompletionRequestMessage: ChatCompletionRequestMessage = {
+							content: message.content,
+							role:
+								message.author.id === message.client.user.id
+									? ChatCompletionRequestMessageRoleEnum.Assistant
+									: ChatCompletionRequestMessageRoleEnum.User,
+						};
+
+						const name = parseAIName(message.member?.displayName ?? message.author.username);
+						if (name) chatCompletionRequestMessage.name = name;
+						return chatCompletionRequestMessage;
+					}),
 				],
 				model: "gpt-3.5-turbo",
 				user: message.author.id,
