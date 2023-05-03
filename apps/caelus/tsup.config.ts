4e3f171822e0d445fc19258e07e595938f3145cc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-05-03 11:59:40 +0100

    refactor: Standalone Caelus

--- apps/caelus/tsup.config.ts
+++ apps/caelus/tsup.config.ts
@@ -1,1 +1,13 @@
-export { default } from "../../tsup.config.js";
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+	entry: ["source/index.ts"],
+	platform: "node",
+	format: "esm",
+	target: "esnext",
+	skipNodeModulesBundle: true,
+	clean: true,
+	splitting: false,
+	keepNames: true,
+	sourcemap: true,
+});
