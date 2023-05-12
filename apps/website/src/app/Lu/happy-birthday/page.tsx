5d4502620f5833fb9f90ad03617447d176fcb486
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-05-12 05:12:18 +0100

    feat: Add video

--- apps/website/src/app/Lu/happy-birthday/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-	return (
-		<div className="mx-auto flex min-h-screen max-w-6xl flex-col place-items-center gap-12 px-8 py-16 lg:place-content-center lg:px-8 lg:py-0">
-			<div className="flex flex-col place-items-center gap-10 lg:flex-row lg:gap-6">
-				<h1 className="text-3xl font-black leading-tight sm:text-7xl sm:leading-tight">Oh??</h1>
-			</div>
-		</div>
-	);
-}
--- apps/website/src/components/Countdown.tsx
+++ apps/website/src/components/Countdown.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import Link from "next/link.js";
 import { useCountdown } from "~/hooks/useCountdown";
+import { CDN_URL } from "~/util/constants";
 
 function Expired() {
-	return (
-		<Link
-			className="bg-white dark:bg-caelus-100 hover:bg-lightblue-100 focus:ring focus:ring-width-2 flex h-11 transform-gpu cursor-pointer select-none appearance-none flex-row place-items-center gap-2 rounded px-4 text-base font-semibold leading-none text-black no-underline outline-none focus:ring-caelus dark:focus:ring-white active:translate-y-px"
-			href="Lu/happy-birthday"
-		>
-			??
-		</Link>
-	);
+	return <video controls src={new URL("Lucía.mp4", CDN_URL).toString()} />;
 }
 
 export default function Countdown() {
