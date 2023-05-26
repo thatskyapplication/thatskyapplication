fe4287f9be44ca761fa47ca9944a217cdbb191f2
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-05-26 18:09:25 +0100

    fix(MessageBaseReply): Account for small screens

--- apps/website/src/components/MessageBaseReply.tsx
+++ apps/website/src/components/MessageBaseReply.tsx
@@ -11,7 +11,7 @@ export function DiscordMessageBaseReply({
 			className="relative mb-1 flex place-items-center before:absolute before:bottom-0 before:left-[-36px] before:right-full before:top-[50%] before:mr-1 before:block before:border-l-2 before:border-t-2 before:border-[rgb(79_84_92)] before:rounded-tl-1.5 before:content-none"
 			id="reply-wrapper"
 		>
-			<div className="flex place-items-center [&>span]:opacity-60">
+			<div className="flex flex-wrap place-items-center [&>span]:opacity-60">
 				{author ? <DiscordMessageAuthorReply {...author} /> : authorNode}
 			</div>
 			{children}
