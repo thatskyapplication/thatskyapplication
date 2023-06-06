bfa1ea67627936ebc95dbaa3220237478475b905
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 00:19:56 +0100

    feat: Add Rhythm Guide

--- apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/index.ts
@@ -1,11 +1,13 @@
 import AdmiringActor from "./AdmiringActor.js";
 import FestivalSpinDancer from "./FestivalSpinDancer.js";
 import RespectfulPianist from "./RespectfulPianist.js";
+import RhythmGuide from "./RhythmGuide.js";
 import ThoughtfulDirector from "./ThoughtfulDirector.js";
 import TroupeGreeter from "./TroupeGreeter.js";
 import TroupeJuggler from "./TroupeJuggler.js";
 
 export default [
+	RhythmGuide,
 	TroupeGreeter,
 	FestivalSpinDancer,
 	AdmiringActor,
