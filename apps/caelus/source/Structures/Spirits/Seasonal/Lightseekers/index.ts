4e63c32b0ec7b52008b5f0b0ca89a9a0d4b5a751
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 00:17:30 +0100

    feat: Add Lightseekers Guide

--- apps/caelus/source/Structures/Spirits/Seasonal/Lightseekers/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Lightseekers/index.ts
@@ -1,11 +1,13 @@
 import CrabWhisperer from "./CrabWhisperer.js";
 import DoublefiveLightCatcher from "./DoublefiveLightCatcher.js";
 import LaidbackPioneer from "./LaidbackPioneer.js";
+import LightseekersGuide from "./LightseekersGuide.js";
 import PiggybackLightseeker from "./PiggybackLightseeker.js";
 import ShushingLightScholar from "./ShushingLightScholar.js";
 import TwirlingChampion from "./TwirlingChampion.js";
 
 export default [
+	LightseekersGuide,
 	PiggybackLightseeker,
 	DoublefiveLightCatcher,
 	LaidbackPioneer,
