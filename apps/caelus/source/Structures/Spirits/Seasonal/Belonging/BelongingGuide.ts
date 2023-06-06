8cd2602108c78c1abcc1d304906d6706b65f44c4
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 00:19:08 +0100

    feat: Add Belonging Guide

---
+++ apps/caelus/source/Structures/Spirits/Seasonal/Belonging/BelongingGuide.ts
@@ -0,0 +1,7 @@
+import { Season } from "../../../../Utility/Constants.js";
+import { SpiritName, GuideSpirit } from "../../Base.js";
+
+export default new GuideSpirit({
+	name: SpiritName.BelongingGuide,
+	season: Season.Belonging,
+});
--- apps/caelus/source/Structures/Spirits/Seasonal/Belonging/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Belonging/index.ts
@@ -1,3 +1,4 @@
+import BelongingGuide from "./BelongingGuide.js";
 import BoogieKid from "./BoogieKid.js";
 import ConfettiCousin from "./ConfettiCousin.js";
 import HairtousleTeen from "./HairtousleTeen.js";
@@ -5,4 +6,12 @@ import PleafulParent from "./PleafulParent.js";
 import SparklerParent from "./SparklerParent.js";
 import WiseGrandparent from "./WiseGrandparent.js";
 
-export default [BoogieKid, ConfettiCousin, HairtousleTeen, SparklerParent, PleafulParent, WiseGrandparent] as const;
+export default [
+	BelongingGuide,
+	BoogieKid,
+	ConfettiCousin,
+	HairtousleTeen,
+	SparklerParent,
+	PleafulParent,
+	WiseGrandparent,
+] as const;
