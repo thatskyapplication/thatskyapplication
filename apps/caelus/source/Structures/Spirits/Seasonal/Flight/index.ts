bd0c090ea24de8917fb5cc013e5e347e4ce2144f
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 23:05:09 +0100

    feat(Flight): Add Flight Guide

--- apps/caelus/source/Structures/Spirits/Seasonal/Flight/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Flight/index.ts
@@ -1,6 +1,7 @@
+import FlightGuide from "./FlightGuide.js";
 import LightWhisperer from "./LightWhisperer.js";
 import LivelyNavigator from "./LivelyNavigator.js";
 import TalentedBuilder from "./TalentedBuilder.js";
 import TinkeringChimesmith from "./TinkeringChimesmith.js";
 
-export default [LivelyNavigator, LightWhisperer, TinkeringChimesmith, TalentedBuilder] as const;
+export default [FlightGuide, LivelyNavigator, LightWhisperer, TinkeringChimesmith, TalentedBuilder] as const;
