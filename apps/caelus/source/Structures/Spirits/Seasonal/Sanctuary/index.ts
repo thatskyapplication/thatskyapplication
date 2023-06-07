127dbd48b1d61e9b6bd881099c8d8d148e927a7b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 21:47:34 +0100

    feat: Add Sanctuary Guide

--- apps/caelus/source/Structures/Spirits/Seasonal/Sanctuary/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Sanctuary/index.ts
@@ -3,9 +3,11 @@ import GratefulShellCollector from "./GratefulShellCollector.js";
 import HikingGrouch from "./HikingGrouch.js";
 import JellyWhisperer from "./JellyWhisperer.js";
 import RallyingThrillseeker from "./RallyingThrillseeker.js";
+import SanctuaryGuide from "./SanctuaryGuide.js";
 import TimidBookworm from "./TimidBookworm.js";
 
 export default [
+	SanctuaryGuide,
 	JellyWhisperer,
 	TimidBookworm,
 	RallyingThrillseeker,
