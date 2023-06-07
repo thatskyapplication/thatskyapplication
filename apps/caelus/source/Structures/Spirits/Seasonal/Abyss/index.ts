a23653aeecf74d88e570ccdc91828daf847afffe
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-07 23:12:02 +0100

    feat(Abyss): Add Abyss Guide

--- apps/caelus/source/Structures/Spirits/Seasonal/Abyss/index.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Abyss/index.ts
@@ -1,6 +1,7 @@
+import AbyssGuide from "./AbyssGuide.js";
 import AnxiousAngler from "./AnxiousAngler.js";
 import BumblingBoatswain from "./BumblingBoatswain.js";
 import CacklingCannoneer from "./CacklingCannoneer.js";
 import CeasingCommodore from "./CeasingCommodore.js";
 
-export default [AnxiousAngler, CeasingCommodore, BumblingBoatswain, CacklingCannoneer] as const;
+export default [AbyssGuide, AnxiousAngler, CeasingCommodore, BumblingBoatswain, CacklingCannoneer] as const;
