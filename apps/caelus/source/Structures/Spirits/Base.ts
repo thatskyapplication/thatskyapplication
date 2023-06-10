2a4714de12f7a790cd322a24a85bcf537c12b0ce
shuerry <sherrychn2003@gmail.com>
2023-06-10 16:35:34 -0400

    feat: Add Lookout Scout

--- apps/caelus/source/Structures/Spirits/Base.ts
+++ apps/caelus/source/Structures/Spirits/Base.ts
@@ -268,6 +268,7 @@ export const enum Expression {
 	Duck = "Duck",
 	Faint = "Faint",
 	Respect = "Respect",
+	LookAround = "Look Around",
 	Salute = "Salute",
 	Dismiss = "Dismiss",
 	Greeting = "Greeting",
--- apps/caelus/source/Structures/Spirits/Standard/GoldenWasteland/index.ts
+++ apps/caelus/source/Structures/Spirits/Standard/GoldenWasteland/index.ts
@@ -1,13 +1,15 @@
 import CourageousSoldier from "./CourageousSoldier.js";
 import FaintingWarrior from "./FaintingWarrior.js";
 import FrightenedRefugee from "./FrightenedRefugee.js";
+import LookoutScout from "./LookoutScout.js";
 import SalutingCaptain from "./SalutingCaptain.js";
 import StealthySurvivor from "./StealthySurvivor.js";
 
 export default [
-    FrightenedRefugee,
-    FaintingWarrior, 
-    CourageousSoldier, 
-    StealthySurvivor, 
-    SalutingCaptain
+	FrightenedRefugee,
+	FaintingWarrior,
+	CourageousSoldier,
+	StealthySurvivor,
+	SalutingCaptain,
+	LookoutScout,
 ] as const;
