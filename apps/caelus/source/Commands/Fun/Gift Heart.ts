aafb85c135199f130533b22fc3138e5d807bb0a4
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-06-17 22:38:42 +0100

    style(Gift Heart): Add line break after `data`

--- apps/caelus/source/Commands/Fun/Gift Heart.ts
+++ apps/caelus/source/Commands/Fun/Gift Heart.ts
@@ -6,6 +6,7 @@ export default new (class implements UserContextMenuCommand {
 		name: "Gift Heart",
 		type: ApplicationCommandType.User,
 	} as const;
+
 	public async userContextMenu(interaction: UserContextMenuCommandInteraction) {
 		await COMMANDS.heart.gift(interaction);
 	}
