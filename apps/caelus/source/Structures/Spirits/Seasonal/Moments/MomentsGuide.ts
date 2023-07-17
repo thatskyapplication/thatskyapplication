6150fa8d73b6eea4d068861fce3cd8caf3bb48d6
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-17 09:56:54 +0100

    feat(MomentsGuide): Add `offer`

--- apps/caelus/source/Structures/Spirits/Seasonal/Moments/MomentsGuide.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Moments/MomentsGuide.ts
@@ -1,5 +1,7 @@
+/* eslint-disable unicorn/prefer-math-trunc */
+import { Collection } from "discord.js";
 import { Realm, Season } from "../../../../Utility/Constants.js";
-import { GuideSpirit, SpiritName } from "../../Base.js";
+import { type ItemsData, GuideSpirit, SpiritName } from "../../Base.js";
 
 export default new GuideSpirit({
 	name: SpiritName.MomentsGuide,
@@ -7,4 +9,12 @@ export default new GuideSpirit({
 	realm: Realm.DaylightPrairie,
 	inProgress: true,
 	hasInfographic: false,
+	offer: new Collection<number, ItemsData>()
+		.set(1 << 0, { item: "Camera", cost: null })
+		.set(1 << 1, { item: "Pendant", cost: null })
+		.set(1 << 2, { item: "Ultimate glasses", cost: { seasonalHearts: 1 } })
+		.set(1 << 3, { item: "Ultimate accessory", cost: { seasonalHearts: 1 } })
+		.set(1 << 4, { item: "Ultimate hat", cost: { seasonalHearts: 2 } })
+		.set(1 << 5, { item: "Quest 1", cost: null })
+		.set(1 << 6, { item: "Heart 1", cost: null }),
 });
