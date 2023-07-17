854c077f4f590379af9f2d835515e21c1334c63c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-17 10:47:04 +0100

    feat(JollyGeologist): Update friendship tree

--- apps/caelus/source/Structures/Spirits/Seasonal/Moments/JollyGeologist.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Moments/JollyGeologist.ts
@@ -1,8 +1,9 @@
+/* eslint-disable unicorn/prefer-math-trunc */
+import { Collection } from "discord.js";
 import { Realm, Season } from "../../../../Utility/Constants.js";
-import { SeasonalSpirit, SpiritName } from "../../Base.js";
+import { type ItemsData, Expression, SeasonalSpirit, SpiritName } from "../../Base.js";
 
-// TODO: Add expression, call, or stance.
-const expression = undefined;
+const expression = Expression.JollyDance;
 
 export default new SeasonalSpirit({
 	name: SpiritName.JollyGeologist,
@@ -10,5 +11,16 @@ export default new SeasonalSpirit({
 	expression,
 	realm: Realm.DaylightPrairie,
 	hasInfographic: false,
-	// TODO: Add offer.
+	offer: new Collection<number, ItemsData>()
+		.set(1 << 0, { item: `${expression} 1`, cost: null })
+		.set(1 << 1, { item: `${expression} 2`, cost: null })
+		.set(1 << 2, { item: "Mask accessory", cost: { seasonalCandles: 8 } })
+		.set(1 << 3, { item: "Hair", cost: null })
+		.set(1 << 4, { item: `${expression} 3`, cost: { seasonalCandles: 20 } })
+		.set(1 << 5, { item: `${expression} 4`, cost: null })
+		.set(1 << 6, { item: "Blessing 1", cost: { seasonalCandles: 28 } })
+		.set(1 << 7, { item: "Blessing 2", cost: null })
+		.set(1 << 8, { item: "Music sheet", cost: { seasonalCandles: 34 } })
+		.set(1 << 9, { item: "Prop", cost: null })
+		.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
 });
