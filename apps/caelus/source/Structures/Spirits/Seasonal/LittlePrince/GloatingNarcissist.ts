1e170119120e548a293b28d686a9aa375cd6a3b6
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-20 09:12:14 +0100

    feat(GloatingNarcissist): Update friendship tree

--- apps/caelus/source/Structures/Spirits/Seasonal/LittlePrince/GloatingNarcissist.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/LittlePrince/GloatingNarcissist.ts
@@ -18,18 +18,19 @@ export default new SeasonalSpirit({
 	season: Season.LittlePrince,
 	expression,
 	realm: Realm.VaultOfKnowledge,
+	hasInfographic: false,
 	offer: new Collection<number, ItemsData>()
 		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: null })
-		.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 14 } })
-		.set(1 << 3, { item: "Music sheet", cost: null })
-		.set(1 << 4, { item: `${expression} 3`, cost: { seasonalCandles: 18 } })
-		.set(1 << 5, { item: `${expression} 4`, cost: null })
-		.set(1 << 6, { item: "Blessing 2", cost: { seasonalCandles: 22 } })
-		.set(1 << 7, { item: "Outfit", cost: null })
-		.set(1 << 8, { item: "Hair", cost: { seasonalCandles: 26 } })
-		.set(1 << 9, { item: "Blessing 3", cost: null })
-		.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
+		.set(1 << 10, { item: "Heart", cost: { candles: 3 } })
+		.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 } })
+		.set(1 << 3, { item: "Music sheet", cost: { candles: 15 } })
+		.set(1 << 8, { item: "Hair", cost: { candles: 46 } })
+		.set(1 << 11, { item: "Wing buff", cost: { ascendedCandles: 2 } })
+		.set(1 << 4, { item: `${expression} 3`, cost: { hearts: 3 } })
+		.set(1 << 5, { item: `${expression} 4`, cost: { hearts: 6} })
+		.set(1 << 6, { item: "Blessing 2", cost: { candles: 5 } })
+		.set(1 << 7, { item: "Outfit", cost: { candles: 65 } }),
 	visits: {
 		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>().set(92, skyDate(2_023, 7, 20)),
 		returning: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>(),
