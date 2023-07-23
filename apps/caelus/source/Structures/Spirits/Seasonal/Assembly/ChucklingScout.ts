86b3ffb9a3b6a8e60622bfa5b385604865ed6a0a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-23 10:14:36 +0100

    feat(Assembly): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/Assembly/ChucklingScout.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Assembly/ChucklingScout.ts
@@ -18,6 +18,19 @@ export default new SeasonalSpirit({
 	season: Season.Assembly,
 	expression,
 	realm: Realm.HiddenForest,
+	hasSeasonalInfographic: false,
+	seasonalOffer: new Collection<number, ItemsData>()
+		.set(1 << 0, { item: `${expression} 1`, cost: null })
+		.set(1 << 1, { item: `${expression} 2`, cost: null })
+		.set(1 << 3, { item: "Mask", cost: { seasonalCandles: 12 } })
+		.set(1 << 2, { item: "Blessing 1", cost: null })
+		.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 14 } })
+		.set(1 << 7, { item: `${expression} 4`, cost: null })
+		.set(1 << 9, { item: "Blessing 2", cost: { seasonalCandles: 17 } })
+		.set(1 << 8, { item: "Outfit", cost: null })
+		.set(1 << 10, { item: "Prop", cost: { seasonalCandles: 20 } })
+		.set(1 << 11, { item: "Blessing 3", cost: null })
+		.set(1 << 4, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
 	offer: new Collection<number, ItemsData>()
 		.set(1 << 0, { item: `${expression} 1`, cost: null })
 		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
--- apps/caelus/source/Structures/Spirits/Seasonal/Assembly/MarchingAdventurer.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Assembly/MarchingAdventurer.ts
@@ -18,6 +18,19 @@ export default new SeasonalSpirit({
 	season: Season.Assembly,
 	expression,
 	realm: Realm.HiddenForest,
+	hasSeasonalInfographic: false,
+	seasonalOffer: new Collection<number, ItemsData>()
+		.set(1 << 0, { item: `${expression} 1`, cost: null })
+		.set(1 << 1, { item: `${expression} 2`, cost: null })
+		.set(1 << 8, { item: "Hair", cost: { seasonalCandles: 12 } })
+		.set(1 << 2, { item: "Blessing 1", cost: null })
+		.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 14 } })
+		.set(1 << 7, { item: `${expression} 4`, cost: null })
+		.set(1 << 9, { item: "Blessing 2", cost: { seasonalCandles: 16 } })
+		.set(1 << 3, { item: "Mask", cost: null })
+		.set(1 << 10, { item: "Tiki torch", cost: { seasonalCandles: 22 } })
+		.set(1 << 11, { item: "Blessing 3", cost: null })
+		.set(1 << 4, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
 	offer: new Collection<number, ItemsData>()
 		.set(1 << 0, { item: `${expression} 1`, cost: null })
 		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
