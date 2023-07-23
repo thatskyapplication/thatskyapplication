51c29a51dff383614b5e465083ca421b6f21be7c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-23 07:05:00 +0100

    feat(GloatingNarcissist): Add back offer during season

--- apps/caelus/source/Structures/Spirits/Seasonal/LittlePrince/GloatingNarcissist.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/LittlePrince/GloatingNarcissist.ts
@@ -19,10 +19,18 @@ export default new SeasonalSpirit({
 	expression,
 	realm: Realm.VaultOfKnowledge,
 	hasMarketingVideo: true,
-	/*
-	 * Removed offers:
-	 * .set(1 << 9, { item: "Blessing 3", cost: null })
-	 */
+	seasonalOffer: new Collection<number, ItemsData>()
+		.set(1 << 0, { item: `${expression} 1`, cost: null })
+		.set(1 << 1, { item: `${expression} 2`, cost: null })
+		.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 14 } })
+		.set(1 << 3, { item: "Music sheet", cost: null })
+		.set(1 << 4, { item: `${expression} 3`, cost: { seasonalCandles: 18 } })
+		.set(1 << 5, { item: `${expression} 4`, cost: null })
+		.set(1 << 6, { item: "Blessing 2", cost: { seasonalCandles: 22 } })
+		.set(1 << 7, { item: "Outfit", cost: null })
+		.set(1 << 8, { item: "Hair", cost: { seasonalCandles: 26 } })
+		.set(1 << 9, { item: "Blessing 3", cost: null })
+		.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
 	offer: new Collection<number, ItemsData>()
 		.set(1 << 0, { item: `${expression} 1`, cost: null })
 		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
