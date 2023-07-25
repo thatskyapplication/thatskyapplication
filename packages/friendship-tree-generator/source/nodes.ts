3c0e4c41b0a09e0e208a87ad660578e3899c02d2
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-25 20:08:23 +0100

    chore(nodes): Empty file data

--- packages/friendship-tree-generator/source/nodes.ts
+++ packages/friendship-tree-generator/source/nodes.ts
@@ -2,15 +2,4 @@ interface Node {
 	icon: string;
 }
 
-export default [
-	[
-		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-		},
-	],
-	[
-		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-		},
-	],
-] as const satisfies Readonly<Readonly<Node[]>[]>;
+export default [[]] as const satisfies Readonly<Readonly<Node[]>[]>;
