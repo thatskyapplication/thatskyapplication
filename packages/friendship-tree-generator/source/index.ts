6c6f21c9b79e48d6468db134ecce7dc5328b587e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-25 02:25:22 +0100

    feat: Create nodes file

--- packages/friendship-tree-generator/source/index.ts
+++ packages/friendship-tree-generator/source/index.ts
@@ -1,3 +1,32 @@
-export default {
-	
-} as const;
+import { writeFile } from "node:fs/promises";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
+import { fetch } from "undici";
+import NODES from "./nodes.js";
+
+const canvas = createCanvas(1_024, 1_024);
+const context = canvas.getContext("2d");
+context.lineWidth = 5;
+context.strokeStyle = "#FFFFFF";
+context.fillStyle = "#FFFFFF";
+
+const nodes = [];
+for (let index = 0; index < NODES.length; index += 3) nodes.push(NODES.slice(index, index + 3));
+let height = canvas.height - 150;
+
+for (const level of nodes) {
+	let nodeIndex = 0;
+
+	for (const node of level) {
+		const arrayBuffer = await (await fetch(node.icon)).arrayBuffer();
+		const icon = await loadImage(arrayBuffer);
+		context.drawImage(icon, canvas.width / 2 - 36, height, 75, 75);
+
+		if (++nodeIndex !== level.length) {
+			height -= 80;
+			context.strokeRect(canvas.width / 2, height, 0, 75);
+			height -= 80;
+		}
+	}
+}
+
+await writeFile("./simple.webp", await canvas.encode("webp"));
---
+++ packages/friendship-tree-generator/source/nodes.ts
@@ -0,0 +1,21 @@
+interface Node {
+	icon: string;
+}
+
+export default [
+	{
+		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+	},
+	{
+		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+	},
+	{
+		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+	},
+	{
+		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+	},
+	{
+		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+	},
+] as const satisfies readonly Node[];
