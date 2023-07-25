4f43ee93ad2db5b4193eba4c399c9184813cf23b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-25 03:42:41 +0100

    chore: Update testing file

--- packages/friendship-tree-generator/source/nodes.ts
+++ packages/friendship-tree-generator/source/nodes.ts
@@ -3,19 +3,14 @@ interface Node {
 }
 
 export default [
-	{
-		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-	},
-	{
-		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-	},
-	{
-		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-	},
-	{
-		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-	},
-	{
-		icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
-	},
-] as const satisfies readonly Node[];
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Sky-Anniversary-Musical-shell-prop-icon-Morybel-0146.png/revision/latest",
+		},
+	],
+] as const satisfies Readonly<Readonly<Node[]>[]>;
