46538e01e6190cba437ab20c6cc1257b927f26e5
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-25 19:06:41 +0100

    feat(constants): Add `IMAGE_SIZE`

--- packages/friendship-tree-generator/source/constants.ts
+++ packages/friendship-tree-generator/source/constants.ts
@@ -1,1 +1,2 @@
+export const IMAGE_SIZE = 75 as const;
 export const WIDTH_MODIFIER = 200 as const;
--- packages/friendship-tree-generator/source/index.ts
+++ packages/friendship-tree-generator/source/index.ts
@@ -1,7 +1,7 @@
 import { writeFile } from "node:fs/promises";
 import { createCanvas, loadImage } from "@napi-rs/canvas";
 import { fetch } from "undici";
-import { WIDTH_MODIFIER } from "./constants.js";
+import { IMAGE_SIZE, WIDTH_MODIFIER } from "./constants.js";
 import NODES from "./nodes.js";
 
 const canvas = createCanvas(700, 900);
@@ -10,7 +10,7 @@ context.translate(0.5, 0.5);
 context.lineWidth = 5;
 context.strokeStyle = "#FFFFFF";
 context.fillStyle = "#FFFFFF";
-const widthStartMiddle = canvas.width / 2 - 37.5;
+const widthStartMiddle = canvas.width / 2 - IMAGE_SIZE / 2;
 const widthStartLeft = widthStartMiddle - WIDTH_MODIFIER;
 const widthStartRight = widthStartMiddle + WIDTH_MODIFIER;
 let heightStartMiddle = canvas.height - 100;
@@ -46,10 +46,10 @@ for (const nodes of NODES) {
 			context.beginPath();
 
 			if (nodeIndex === 1) {
-				context.moveTo(canvas.width / 2 - 50, heightStartMiddle + 15);
+				context.moveTo(canvas.width / 2 - (IMAGE_SIZE / 2 + 12.5), heightStartMiddle + 15);
 				context.lineTo(widthStartLeft + 85, heightStartSides + 85);
 			} else {
-				context.moveTo(canvas.width / 2 + 50, heightStartMiddle + 15);
+				context.moveTo(canvas.width / 2 + (IMAGE_SIZE / 2 + 12.5), heightStartMiddle + 15);
 				context.lineTo(widthStartRight - 10, heightStartSides + 85);
 			}
 
@@ -58,12 +58,12 @@ for (const nodes of NODES) {
 
 		const arrayBuffer = await (await fetch(node.icon)).arrayBuffer();
 		const icon = await loadImage(arrayBuffer);
-		context.drawImage(icon, dx, dy, 75, 75);
+		context.drawImage(icon, dx, dy, IMAGE_SIZE, IMAGE_SIZE);
 
 		if (++nodeIndex === nodes.length && ++nodesIndex !== NODES.length) {
 			context.beginPath();
 			context.moveTo(canvas.width / 2, heightStartMiddle - 10);
-			context.lineTo(canvas.width / 2, heightStartMiddle - 155);
+			context.lineTo(canvas.width / 2, heightStartMiddle - (240 - IMAGE_SIZE - 10));
 			context.stroke();
 			heightStartMiddle -= 240;
 			heightStartSides -= 240;
