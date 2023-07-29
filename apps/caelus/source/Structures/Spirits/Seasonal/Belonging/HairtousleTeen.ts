86c9e701d3ce21b42799dfa823dabe6f27c7bdfc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-29 06:11:21 +0100

    feat(Belonging): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/Belonging/HairtousleTeen.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Belonging/HairtousleTeen.ts
@@ -18,28 +18,30 @@ export default new SeasonalSpirit({
 	season: Season.Belonging,
 	expression,
 	realm: Realm.HiddenForest,
-	hasSeasonalInfographic: false,
-	seasonalOffer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: "Blessing 1", cost: { seasonalCandles: 12 } })
-		.set(1 << 5, { item: "Blessing 2", cost: null })
-		.set(1 << 2, { item: "Music sheet", cost: { seasonalCandles: 14 } })
-		.set(1 << 6, { item: `${expression} 2`, cost: null })
-		.set(1 << 9, { item: "Blessing 3", cost: { seasonalCandles: 16 } })
-		.set(1 << 8, { item: "Earmuffs", cost: null })
-		.set(1 << 7, { item: "Ukelele", cost: { seasonalCandles: 18 } })
-		.set(1 << 10, { item: "Blessing 4", cost: null })
-		.set(1 << 3, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: "Blessing 1", cost: { candles: 5 } })
-		.set(1 << 2, { item: "Music sheet", cost: { candles: 15 } })
-		.set(1 << 3, { item: "Heart", cost: { candles: 3 } })
-		.set(1 << 4, { item: "Wing buff", cost: { ascendedCandles: 2 } })
-		.set(1 << 5, { item: "Blessing 2", cost: { candles: 5 } })
-		.set(1 << 6, { item: `${expression} 2`, cost: { hearts: 9 } })
-		.set(1 << 7, { item: "Ukelele", cost: { candles: 70 } })
-		.set(1 << 8, { item: "Earmuffs", cost: { candles: 50 } }),
+	offer: {
+		hasInfographicSeasonal: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: "Blessing 1", cost: { seasonalCandles: 12 } })
+			.set(1 << 5, { item: "Blessing 2", cost: null })
+			.set(1 << 2, { item: "Music sheet", cost: { seasonalCandles: 14 } })
+			.set(1 << 6, { item: `${expression} 2`, cost: null })
+			.set(1 << 9, { item: "Blessing 3", cost: { seasonalCandles: 16 } })
+			.set(1 << 8, { item: "Earmuffs", cost: null })
+			.set(1 << 7, { item: "Ukelele", cost: { seasonalCandles: 18 } })
+			.set(1 << 10, { item: "Blessing 4", cost: null })
+			.set(1 << 3, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: "Blessing 1", cost: { candles: 5 } })
+			.set(1 << 2, { item: "Music sheet", cost: { candles: 15 } })
+			.set(1 << 3, { item: "Heart", cost: { candles: 3 } })
+			.set(1 << 4, { item: "Wing buff", cost: { ascendedCandles: 2 } })
+			.set(1 << 5, { item: "Blessing 2", cost: { candles: 5 } })
+			.set(1 << 6, { item: `${expression} 2`, cost: { hearts: 9 } })
+			.set(1 << 7, { item: "Ukelele", cost: { candles: 70 } })
+			.set(1 << 8, { item: "Earmuffs", cost: { candles: 50 } }),
+	},
 	visits: {
 		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>()
 			.set(11, skyDate(2_020, 6, 11))
--- apps/caelus/source/Structures/Spirits/Seasonal/Belonging/PleafulParent.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Belonging/PleafulParent.ts
@@ -19,29 +19,31 @@ export default new SeasonalSpirit({
 	expression: Expression.DontGo,
 	realm: Realm.GoldenWasteland,
 	hasMarketingVideo: true,
-	hasSeasonalInfographic: false,
-	seasonalOffer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: null })
-		.set(1 << 2, { item: "Blessing", cost: { seasonalCandles: 14 } })
-		.set(1 << 10, { item: "Guitar", cost: null })
-		.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 16 } })
-		.set(1 << 7, { item: `${expression} 4`, cost: null })
-		.set(1 << 3, { item: "Mask", cost: { seasonalCandles: 18 } })
-		.set(1 << 9, { item: "Cape", cost: null })
-		.set(1 << 4, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
-		.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 } })
-		.set(1 << 3, { item: "Mask", cost: { candles: 42 } })
-		.set(1 << 4, { item: "Heart", cost: { candles: 3 } })
-		.set(1 << 5, { item: "Wing buff", cost: { ascendedCandles: 2 } })
-		.set(1 << 6, { item: `${expression} 3`, cost: { hearts: 3 } })
-		.set(1 << 7, { item: `${expression} 4`, cost: { hearts: 6 } })
-		.set(1 << 8, { item: "Blessing 2", cost: { candles: 5 } })
-		.set(1 << 9, { item: "Cape", cost: { candles: 65 } })
-		.set(1 << 10, { item: "Guitar", cost: { candles: 75 } }),
+	offer: {
+		hasInfographicSeasonal: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: null })
+			.set(1 << 2, { item: "Blessing", cost: { seasonalCandles: 14 } })
+			.set(1 << 10, { item: "Guitar", cost: null })
+			.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 16 } })
+			.set(1 << 7, { item: `${expression} 4`, cost: null })
+			.set(1 << 3, { item: "Mask", cost: { seasonalCandles: 18 } })
+			.set(1 << 9, { item: "Cape", cost: null })
+			.set(1 << 4, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
+			.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 } })
+			.set(1 << 3, { item: "Mask", cost: { candles: 42 } })
+			.set(1 << 4, { item: "Heart", cost: { candles: 3 } })
+			.set(1 << 5, { item: "Wing buff", cost: { ascendedCandles: 2 } })
+			.set(1 << 6, { item: `${expression} 3`, cost: { hearts: 3 } })
+			.set(1 << 7, { item: `${expression} 4`, cost: { hearts: 6 } })
+			.set(1 << 8, { item: "Blessing 2", cost: { candles: 5 } })
+			.set(1 << 9, { item: "Cape", cost: { candles: 65 } })
+			.set(1 << 10, { item: "Guitar", cost: { candles: 75 } }),
+	},
 	visits: {
 		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>()
 			.set(5, skyDate(2_020, 3, 26))
