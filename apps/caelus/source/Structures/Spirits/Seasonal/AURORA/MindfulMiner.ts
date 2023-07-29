407e81d920d966b06c4adbb338b484fe5b7dc6dc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-29 06:05:20 +0100

    feat(AURORA): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/AURORA/MindfulMiner.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/AURORA/MindfulMiner.ts
@@ -10,19 +10,21 @@ export default new SeasonalSpirit({
 	season: Season.Aurora,
 	expression,
 	realm: Realm.ValleyOfTriumph,
-	hasInfographic: false,
-	seasonalOffer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: null })
-		.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 10 } })
-		.set(1 << 3, { item: "Mask", cost: null })
-		.set(1 << 4, { item: `${expression} 3`, cost: { seasonalCandles: 18 } })
-		.set(1 << 5, { item: `${expression} 4`, cost: null })
-		.set(1 << 6, { item: "Hair", cost: { seasonalCandles: 24 } })
-		.set(1 << 7, { item: "Blessing 2", cost: null })
-		.set(1 << 8, { item: "Blessing 3", cost: { seasonalCandles: 28 } })
-		.set(1 << 9, { item: "Outfit", cost: null })
-		.set(1 << 10, { item: "Cape", cost: { seasonalCandles: 32 } })
-		.set(1 << 11, { item: "Blessing 4", cost: null })
-		.set(1 << 12, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	offer: {
+		hasInfographic: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: null })
+			.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 10 } })
+			.set(1 << 3, { item: "Mask", cost: null })
+			.set(1 << 4, { item: `${expression} 3`, cost: { seasonalCandles: 18 } })
+			.set(1 << 5, { item: `${expression} 4`, cost: null })
+			.set(1 << 6, { item: "Hair", cost: { seasonalCandles: 24 } })
+			.set(1 << 7, { item: "Blessing 2", cost: null })
+			.set(1 << 8, { item: "Blessing 3", cost: { seasonalCandles: 28 } })
+			.set(1 << 9, { item: "Outfit", cost: null })
+			.set(1 << 10, { item: "Cape", cost: { seasonalCandles: 32 } })
+			.set(1 << 11, { item: "Blessing 4", cost: null })
+			.set(1 << 12, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
 });
--- apps/caelus/source/Structures/Spirits/Seasonal/AURORA/SeedOfHope.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/AURORA/SeedOfHope.ts
@@ -10,19 +10,21 @@ export default new SeasonalSpirit({
 	season: Season.Aurora,
 	expression,
 	realm: Realm.ValleyOfTriumph,
-	hasInfographic: false,
-	seasonalOffer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: null })
-		.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 8 } })
-		.set(1 << 3, { item: "Mask", cost: null })
-		.set(1 << 4, { item: "Music sheet", cost: { seasonalCandles: 12 } })
-		.set(1 << 5, { item: "Blessing 2", cost: null })
-		.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 16 } })
-		.set(1 << 7, { item: `${expression} 4`, cost: null })
-		.set(1 << 8, { item: "Hair", cost: { seasonalCandles: 20 } })
-		.set(1 << 9, { item: "Blessing 3", cost: null })
-		.set(1 << 10, { item: "Blessing 4", cost: { seasonalCandles: 26 } })
-		.set(1 << 11, { item: "Cape", cost: null })
-		.set(1 << 12, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	offer: {
+		hasInfographic: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: null })
+			.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 8 } })
+			.set(1 << 3, { item: "Mask", cost: null })
+			.set(1 << 4, { item: "Music sheet", cost: { seasonalCandles: 12 } })
+			.set(1 << 5, { item: "Blessing 2", cost: null })
+			.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 16 } })
+			.set(1 << 7, { item: `${expression} 4`, cost: null })
+			.set(1 << 8, { item: "Hair", cost: { seasonalCandles: 20 } })
+			.set(1 << 9, { item: "Blessing 3", cost: null })
+			.set(1 << 10, { item: "Blessing 4", cost: { seasonalCandles: 26 } })
+			.set(1 << 11, { item: "Cape", cost: null })
+			.set(1 << 12, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
 });
