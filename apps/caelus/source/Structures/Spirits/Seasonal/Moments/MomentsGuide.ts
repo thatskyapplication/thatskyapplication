209823ec24b902629d8d0843c9ec10d4e864a562
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-29 17:58:28 +0100

    feat(Moments): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/Moments/MomentsGuide.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Moments/MomentsGuide.ts
@@ -8,12 +8,14 @@ export default new GuideSpirit({
 	season: Season.Moments,
 	realm: Realm.DaylightPrairie,
 	inProgress: true,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: "Camera", cost: null })
-		.set(1 << 1, { item: "Pendant", cost: null })
-		.set(1 << 2, { item: "Ultimate mask accessory", cost: { seasonalHearts: 1 } })
-		.set(1 << 3, { item: "Ultimate camera", cost: { seasonalHearts: 1 } })
-		.set(1 << 4, { item: "Ultimate hair accessory", cost: { seasonalHearts: 2 } })
-		.set(1 << 5, { item: "Quest 1", cost: null })
-		.set(1 << 6, { item: "Heart 1", cost: null }),
+	offer: {
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Camera", cost: null })
+			.set(1 << 1, { item: "Pendant", cost: null })
+			.set(1 << 2, { item: "Ultimate mask accessory", cost: { seasonalHearts: 1 } })
+			.set(1 << 3, { item: "Ultimate camera", cost: { seasonalHearts: 1 } })
+			.set(1 << 4, { item: "Ultimate hair accessory", cost: { seasonalHearts: 2 } })
+			.set(1 << 5, { item: "Quest 1", cost: null })
+			.set(1 << 6, { item: "Heart 1", cost: null }),
+	},
 });
--- apps/caelus/source/Structures/Spirits/Seasonal/Moments/ReassuringRanger.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Moments/ReassuringRanger.ts
@@ -10,16 +10,19 @@ export default new SeasonalSpirit({
 	season: Season.Moments,
 	expression,
 	realm: Realm.DaylightPrairie,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: null })
-		.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 16 } })
-		.set(1 << 3, { item: "Mask accessory", cost: null })
-		.set(1 << 4, { item: "Mask", cost: { seasonalCandles: 26 } })
-		.set(1 << 5, { item: "Blessing 2", cost: null })
-		.set(1 << 6, { item: "Blessing 3", cost: { seasonalCandles: 30 } })
-		.set(1 << 7, { item: "Cape", cost: null })
-		.set(1 << 8, { item: "Hair accessory", cost: { seasonalCandles: 36 } })
-		.set(1 << 9, { item: "Blessing 4", cost: null })
-		.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	offer: {
+		hasInfographic: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: null })
+			.set(1 << 2, { item: "Blessing 1", cost: { seasonalCandles: 16 } })
+			.set(1 << 3, { item: "Mask accessory", cost: null })
+			.set(1 << 4, { item: "Mask", cost: { seasonalCandles: 26 } })
+			.set(1 << 5, { item: "Blessing 2", cost: null })
+			.set(1 << 6, { item: "Blessing 3", cost: { seasonalCandles: 30 } })
+			.set(1 << 7, { item: "Cape", cost: null })
+			.set(1 << 8, { item: "Hair accessory", cost: { seasonalCandles: 36 } })
+			.set(1 << 9, { item: "Blessing 4", cost: null })
+			.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
 });
