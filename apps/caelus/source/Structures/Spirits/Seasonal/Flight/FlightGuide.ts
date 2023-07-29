ad8e5345b5daaea13c586d6b3b9581b63b1d004e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-29 07:33:37 +0100

    feat(Flight): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/Flight/FlightGuide.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Flight/FlightGuide.ts
@@ -7,21 +7,23 @@ export default new GuideSpirit({
 	name: SpiritName.FlightGuide,
 	season: Season.Flight,
 	realm: Realm.HiddenForest,
-	hasInfographic: false,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: "Quest 1", cost: null })
-		.set(1 << 1, { item: "Heart 1", cost: { candles: 3 } })
-		.set(1 << 2, { item: "Pendant", cost: null })
-		.set(1 << 3, { item: "Ultimate hair accessory", cost: { seasonalHearts: 2 } })
-		.set(1 << 4, { item: "Ultimate outfit", cost: { seasonalHearts: 2 } })
-		.set(1 << 5, { item: "Quest 2", cost: null })
-		.set(1 << 6, { item: "Heart 2", cost: { candles: 3 } })
-		.set(1 << 7, { item: Expression.HighFive, cost: null })
-		.set(1 << 8, { item: "Quest 3", cost: null })
-		.set(1 << 9, { item: "Heart 3", cost: { candles: 3 } })
-		.set(1 << 10, { item: "Quest 4", cost: null })
-		.set(1 << 11, { item: "Heart 4", cost: { candles: 3 } })
-		.set(1 << 12, { item: "Quest 5", cost: null })
-		.set(1 << 13, { item: "Heart 5", cost: { candles: 3 } })
-		.set(1 << 14, { item: Expression.Hug, cost: null }),
+	offer: {
+		hasInfographic: false,
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Quest 1", cost: null })
+			.set(1 << 1, { item: "Heart 1", cost: { candles: 3 } })
+			.set(1 << 2, { item: "Pendant", cost: null })
+			.set(1 << 3, { item: "Ultimate hair accessory", cost: { seasonalHearts: 2 } })
+			.set(1 << 4, { item: "Ultimate outfit", cost: { seasonalHearts: 2 } })
+			.set(1 << 5, { item: "Quest 2", cost: null })
+			.set(1 << 6, { item: "Heart 2", cost: { candles: 3 } })
+			.set(1 << 7, { item: Expression.HighFive, cost: null })
+			.set(1 << 8, { item: "Quest 3", cost: null })
+			.set(1 << 9, { item: "Heart 3", cost: { candles: 3 } })
+			.set(1 << 10, { item: "Quest 4", cost: null })
+			.set(1 << 11, { item: "Heart 4", cost: { candles: 3 } })
+			.set(1 << 12, { item: "Quest 5", cost: null })
+			.set(1 << 13, { item: "Heart 5", cost: { candles: 3 } })
+			.set(1 << 14, { item: Expression.Hug, cost: null }),
+	},
 });
--- apps/caelus/source/Structures/Spirits/Seasonal/Flight/LightWhisperer.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Flight/LightWhisperer.ts
@@ -10,15 +10,18 @@ export default new SeasonalSpirit({
 	season: Season.Flight,
 	call,
 	realm: Realm.HiddenForest,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${call} call`, cost: null })
-		.set(1 << 1, { item: "Blessing 1", cost: { seasonalCandles: 14 } })
-		.set(1 << 2, { item: "Hair accessory", cost: null })
-		.set(1 << 3, { item: "Hair", cost: { seasonalCandles: 22 } })
-		.set(1 << 4, { item: "Blessing 2", cost: null })
-		.set(1 << 5, { item: "Trail spell 1", cost: { seasonalCandles: 26 } })
-		.set(1 << 6, { item: "Cape", cost: null })
-		.set(1 << 7, { item: "Outfit", cost: { seasonalCandles: 28 } })
-		.set(1 << 8, { item: "Trail spell 2", cost: null })
-		.set(1 << 9, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	offer: {
+		hasInfographic: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${call} call`, cost: null })
+			.set(1 << 1, { item: "Blessing 1", cost: { seasonalCandles: 14 } })
+			.set(1 << 2, { item: "Hair accessory", cost: null })
+			.set(1 << 3, { item: "Hair", cost: { seasonalCandles: 22 } })
+			.set(1 << 4, { item: "Blessing 2", cost: null })
+			.set(1 << 5, { item: "Trail spell 1", cost: { seasonalCandles: 26 } })
+			.set(1 << 6, { item: "Cape", cost: null })
+			.set(1 << 7, { item: "Outfit", cost: { seasonalCandles: 28 } })
+			.set(1 << 8, { item: "Trail spell 2", cost: null })
+			.set(1 << 9, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
 });
