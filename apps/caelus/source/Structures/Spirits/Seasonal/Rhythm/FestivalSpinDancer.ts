f227d078dc14f37dcdfe2cd4eae5d381343f2161
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-07-30 01:40:51 +0100

    feat(Rhythm): Migrate spirits

--- apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/FestivalSpinDancer.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/FestivalSpinDancer.ts
@@ -18,19 +18,32 @@ export default new SeasonalSpirit({
 	season: Season.Rhythm,
 	expression,
 	realm: Realm.DaylightPrairie,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: `${expression} 1`, cost: null })
-		.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
-		.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 } })
-		.set(1 << 3, { item: "Music sheet", cost: { candles: 15 } })
-		.set(1 << 4, { item: "Heart", cost: { candles: 3 } })
-		.set(1 << 5, { item: "Wing buff", cost: { ascendedCandles: 2 } })
-		.set(1 << 6, { item: `${expression} 3`, cost: { hearts: 5 } })
-		.set(1 << 7, { item: `${expression} 4`, cost: { hearts: 10 } })
-		.set(1 << 8, { item: "Blessing 2", cost: { candles: 5 } })
-		.set(1 << 9, { item: "Hair", cost: { candles: 34 } })
-		.set(1 << 10, { item: "Prop", cost: { candles: 30 } })
-		.set(1 << 11, { item: "Outfit", cost: { candles: 65 } }),
+	offer: {
+		hasInfographicSeasonal: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: null })
+			.set(1 << 2, { item: "Blessing", cost: { seasonalCandles: 10 } })
+			.set(1 << 3, { item: "Music sheet", cost: null })
+			.set(1 << 6, { item: `${expression} 3`, cost: { seasonalCandles: 12 } })
+			.set(1 << 7, { item: `${expression} 4`, cost: null })
+			.set(1 << 9, { item: "Hair", cost: { seasonalCandles: 14 } })
+			.set(1 << 11, { item: "Outfit", cost: null })
+			.set(1 << 4, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
+			.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 } })
+			.set(1 << 3, { item: "Music sheet", cost: { candles: 15 } })
+			.set(1 << 4, { item: "Heart", cost: { candles: 3 } })
+			.set(1 << 5, { item: "Wing buff", cost: { ascendedCandles: 2 } })
+			.set(1 << 6, { item: `${expression} 3`, cost: { hearts: 5 } })
+			.set(1 << 7, { item: `${expression} 4`, cost: { hearts: 10 } })
+			.set(1 << 8, { item: "Blessing 2", cost: { candles: 5 } })
+			.set(1 << 9, { item: "Hair", cost: { candles: 34 } })
+			.set(1 << 10, { item: "Prop", cost: { candles: 30 } })
+			.set(1 << 11, { item: "Outfit", cost: { candles: 65 } }),
+	},
 	visits: {
 		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>()
 			.set(17, skyDate(2_020, 9, 3))
--- apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/RhythmGuide.ts
+++ apps/caelus/source/Structures/Spirits/Seasonal/Rhythm/RhythmGuide.ts
@@ -6,9 +6,11 @@ import { type ItemsData, SpiritName, GuideSpirit } from "../../Base.js";
 export default new GuideSpirit({
 	name: SpiritName.RhythmGuide,
 	season: Season.Rhythm,
-	hasInfographic: false,
-	offer: new Collection<number, ItemsData>()
-		.set(1 << 0, { item: "Pendant", cost: null })
-		.set(1 << 1, { item: "Ultimate mask", cost: { seasonalHearts: 2 } })
-		.set(1 << 2, { item: "Ultimate hair", cost: { seasonalHearts: 4 } }),
+	offer: {
+		hasInfographic: false,
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Pendant", cost: null })
+			.set(1 << 1, { item: "Ultimate mask", cost: { seasonalHearts: 2 } })
+			.set(1 << 2, { item: "Ultimate hair", cost: { seasonalHearts: 4 } }),
+	},
 });
