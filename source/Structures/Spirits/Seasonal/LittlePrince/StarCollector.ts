359c48420da03fcb5c361837875d2023c72cb3e6
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-09-12 19:43:32 +0100

    feat(StarCollector): Add upcoming visit date

--- source/Structures/Spirits/Seasonal/LittlePrince/StarCollector.ts
+++ source/Structures/Spirits/Seasonal/LittlePrince/StarCollector.ts
@@ -1,7 +1,15 @@
 /* eslint-disable unicorn/prefer-math-trunc */
+import type { Dayjs } from "dayjs";
 import { Collection } from "discord.js";
 import { Realm, Season } from "../../../../Utility/Constants.js";
-import { type ItemsData, Expression, SeasonalSpirit, SpiritName } from "../../Base.js";
+import { skyDate } from "../../../../Utility/Utility.js";
+import {
+	type ItemsData,
+	type SeasonalSpiritVisitCollectionKey,
+	Expression,
+	SeasonalSpirit,
+	SpiritName,
+} from "../../Base.js";
 
 const expression = Expression.HandRub;
 
@@ -24,5 +32,8 @@ export default new SeasonalSpirit({
 			.set(1 << 8, { item: "Prop", cost: { seasonalCandles: 24 } })
 			.set(1 << 9, { item: "Blessing 3", cost: null })
 			.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
+	visits: {
+		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>().set(96, skyDate(2_023, 9, 14)),
 	},
 });
