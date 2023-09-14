99696913de35bebd1900150e9f46ea21f7326a46
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-09-14 10:30:29 +0100

    feat(StarCollector): Update friendship tree

--- source/Structures/Spirits/Seasonal/LittlePrince/StarCollector.ts
+++ source/Structures/Spirits/Seasonal/LittlePrince/StarCollector.ts
@@ -19,7 +19,6 @@ export default new SeasonalSpirit({
 	expression,
 	realm: Realm.VaultOfKnowledge,
 	offer: {
-		hasInfographic: false,
 		seasonal: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: `${expression} 1`, cost: null })
 			.set(1 << 1, { item: `${expression} 2`, cost: null })
@@ -32,6 +31,18 @@ export default new SeasonalSpirit({
 			.set(1 << 8, { item: "Prop", cost: { seasonalCandles: 24 } })
 			.set(1 << 9, { item: "Blessing 3", cost: null })
 			.set(1 << 10, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${expression} 1`, cost: null })
+			.set(1 << 1, { item: `${expression} 2`, cost: { hearts: 4 } })
+			.set(1 << 10, { item: "Heart", cost: { candles: 3 } })
+			.set(1 << 3, { item: "Blessing 1", cost: { candles: 5 } })
+			.set(1 << 2, { item: "Necktie", cost: { candles: 40 } })
+			.set(1 << 11, { item: "Wing buff", cost: { ascendedCandles: 2 } })
+			.set(1 << 4, { item: `${expression} 3`, cost: { hearts: 3 } })
+			.set(1 << 5, { item: `${expression} 4`, cost: { hearts: 6 } })
+			.set(1 << 6, { item: "Blessing 2", cost: { candles: 5 } })
+			.set(1 << 7, { item: "Cape", cost: { candles: 75 } })
+			.set(1 << 8, { item: "Prop", cost: { candles: 70 } }),
 	},
 	visits: {
 		travelling: new Collection<SeasonalSpiritVisitCollectionKey, Dayjs>().set(96, skyDate(2_023, 9, 14)),
