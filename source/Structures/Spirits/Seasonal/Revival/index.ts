4a190fdd717b756b088355205dcaf4cae947a228
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-16 13:57:43 +0100

    feat: Add Remnant of a Forgotten Haven

--- source/Structures/Spirits/Seasonal/Revival/index.ts
+++ source/Structures/Spirits/Seasonal/Revival/index.ts
@@ -1,6 +1,13 @@
 import EchoOfAnAbandonedRefuge from "./EchoOfAnAbandonedRefuge.js";
 import HopefulSteward from "./HopefulSteward.js";
 import MemoryOfALostVillage from "./MemoryOfALostVillage.js";
+import RemnantOfAForgottenHaven from "./RemnantOfAForgottenHaven.js";
 import VestigeOfADesertedOasis from "./VestigeOfADesertedOasis.js";
 
-export default [HopefulSteward, VestigeOfADesertedOasis, MemoryOfALostVillage, EchoOfAnAbandonedRefuge] as const;
+export default [
+	HopefulSteward,
+	VestigeOfADesertedOasis,
+	MemoryOfALostVillage,
+	EchoOfAnAbandonedRefuge,
+	RemnantOfAForgottenHaven,
+] as const;
