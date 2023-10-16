d688d638b0d94824998af4729a37a349a6390171
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-16 13:45:06 +0100

    feat: Add Memory Of A Lost Village

--- source/Structures/Spirits/Seasonal/Revival/index.ts
+++ source/Structures/Spirits/Seasonal/Revival/index.ts
@@ -1,4 +1,5 @@
 import HopefulSteward from "./HopefulSteward.js";
+import MemoryOfALostVillage from "./MemoryOfALostVillage.js";
 import VestigeOfADesertedOasis from "./VestigeOfADesertedOasis.js";
 
-export default [HopefulSteward, VestigeOfADesertedOasis] as const;
+export default [HopefulSteward, VestigeOfADesertedOasis, MemoryOfALostVillage] as const;
