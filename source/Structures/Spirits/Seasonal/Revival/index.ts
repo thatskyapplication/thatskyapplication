455c5103fc7608c8e4f0c39bfd43f0dd973c0864
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-16 13:38:12 +0100

    feat: Add Vestige of a Deserted Oasis

--- source/Structures/Spirits/Seasonal/Revival/index.ts
+++ source/Structures/Spirits/Seasonal/Revival/index.ts
@@ -1,3 +1,4 @@
 import HopefulSteward from "./Hopeful Steward.js";
+import VestigeOfADesertedOasis from "./VestigeOfADesertedOasis.js";
 
-export default [HopefulSteward] as const;
+export default [HopefulSteward, VestigeOfADesertedOasis] as const;
