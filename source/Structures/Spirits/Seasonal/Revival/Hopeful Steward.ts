33723cf80e638a722bab34b5376425fbd3cb03d0
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-16 13:29:01 +0100

    feat: Add Hopeful Steward

---
+++ source/Structures/Spirits/Seasonal/Revival/Hopeful Steward.ts
@@ -0,0 +1,18 @@
+/* eslint-disable unicorn/prefer-math-trunc */
+import { Collection } from "discord.js";
+import { Season } from "../../../../Utility/Constants.js";
+import { type ItemsData, GuideSpirit, SpiritName } from "../../Base.js";
+
+export default new GuideSpirit({
+	name: SpiritName.HopefulSteward,
+	season: Season.Revival,
+	offer: {
+		hasInfographic: false,
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Quest 1", cost: null })
+			.set(1 << 1, { item: "Heart 1", cost: null })
+			.set(1 << 2, { item: "Pendant", cost: null })
+			.set(1 << 3, { item: "Ultimate hair", cost: { seasonalHearts: 2 } })
+			.set(1 << 4, { item: "Ultimate cape", cost: { seasonalHearts: 2 } }),
+	},
+});
---
+++ source/Structures/Spirits/Seasonal/Revival/index.ts
@@ -0,0 +1,3 @@
+import HopefulSteward from "./Hopeful Steward.js";
+
+export default [HopefulSteward] as const;
