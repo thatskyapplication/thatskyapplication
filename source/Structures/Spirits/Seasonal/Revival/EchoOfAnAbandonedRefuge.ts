28bb4618c5971a9208e9a081a11f5af71ddf437e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-16 13:53:36 +0100

    feat: Add Echo of an Abandoned Refuge

---
+++ source/Structures/Spirits/Seasonal/Revival/EchoOfAnAbandonedRefuge.ts
@@ -0,0 +1,23 @@
+/* eslint-disable unicorn/prefer-math-trunc */
+import { Collection } from "discord.js";
+import { Season } from "../../../../Utility/Constants.js";
+import { type ItemsData, SeasonalSpirit, SpiritName } from "../../Base.js";
+
+export default new SeasonalSpirit({
+	name: SpiritName.EchoOfAnAbandonedRefuge,
+	season: Season.Revival,
+	offer: {
+		hasInfographic: false,
+		hasInfographicSeasonal: false,
+		seasonal: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Blessing 1", cost: { seasonalCandles: 18 } })
+			.set(1 << 1, { item: "Shoes", cost: null })
+			.set(1 << 2, { item: "Music sheet", cost: { seasonalCandles: 24 } })
+			.set(1 << 3, { item: "Blessing 2", cost: null })
+			.set(1 << 4, { item: "Blessing 3", cost: { seasonalCandles: 32 } })
+			.set(1 << 5, { item: "Cape", cost: null })
+			.set(1 << 6, { item: "Hair accessory", cost: { seasonalCandles: 42 } })
+			.set(1 << 7, { item: "Blessing 4", cost: null })
+			.set(1 << 8, { item: "Seasonal heart", cost: { seasonalCandles: 3 } }),
+	},
+});
--- source/Structures/Spirits/Seasonal/Revival/index.ts
+++ source/Structures/Spirits/Seasonal/Revival/index.ts
@@ -1,5 +1,6 @@
+import EchoOfAnAbandonedRefuge from "./EchoOfAnAbandonedRefuge.js";
 import HopefulSteward from "./HopefulSteward.js";
 import MemoryOfALostVillage from "./MemoryOfALostVillage.js";
 import VestigeOfADesertedOasis from "./VestigeOfADesertedOasis.js";
 
-export default [HopefulSteward, VestigeOfADesertedOasis, MemoryOfALostVillage] as const;
+export default [HopefulSteward, VestigeOfADesertedOasis, MemoryOfALostVillage, EchoOfAnAbandonedRefuge] as const;
