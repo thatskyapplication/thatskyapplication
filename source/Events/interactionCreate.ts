d452b72ce08c80574aeafb26b8f680873695c7bc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-10-25 18:07:22 +0100

    fix: Do not check via `startsWith()`

--- source/Events/interactionCreate.ts
+++ source/Events/interactionCreate.ts
@@ -258,7 +258,7 @@ export const event: Event<typeof name> = {
 					return;
 				}
 
-				if (customId.startsWith(SPIRIT_TRACKER_ELDERS_EVERYTHING_CUSTOM_ID)) {
+				if (customId === SPIRIT_TRACKER_ELDERS_EVERYTHING_CUSTOM_ID) {
 					await SpiritTracker.setElders(interaction);
 					return;
 				}
