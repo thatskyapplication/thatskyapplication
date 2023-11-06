6d50a9b7d7aaa63113cc62c2cb1d2748c24efd8b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-06 18:22:53 +0000

    refactor(Constants): Remove unused imports

--- source/Utility/Constants.ts
+++ source/Utility/Constants.ts
@@ -1,13 +1,7 @@
 import process from "node:process";
 import { URL } from "node:url";
-import dayjs from "dayjs";
-import timezone from "dayjs/plugin/timezone.js";
-import utc from "dayjs/plugin/utc.js";
 import { Locale } from "discord.js";
 import { resolveShardEruptionMapURL, skyDate } from "./Utility.js";
-
-dayjs.extend(timezone);
-dayjs.extend(utc);
 
 export const enum Emoji {
 	AscendedCandle = "1074399464627912755",
