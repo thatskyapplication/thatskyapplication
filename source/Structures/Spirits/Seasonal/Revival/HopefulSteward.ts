35b2587048959d1690064a24c567e28731ad1fe4
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-11-27 12:34:46 +0000

    feat(HopefulSteward): Update friendship tree

--- source/Structures/Spirits/Seasonal/Revival/HopefulSteward.ts
+++ source/Structures/Spirits/Seasonal/Revival/HopefulSteward.ts
@@ -21,6 +21,8 @@ export default new GuideSpirit({
 			.set(1 << 9, { item: "Quest 4", cost: null })
 			.set(1 << 10, { item: "Heart 4", cost: null })
 			.set(1 << 11, { item: "Quest 5", cost: null })
-			.set(1 << 12, { item: "Heart 5", cost: null }),
+			.set(1 << 12, { item: "Heart 5", cost: null })
+			.set(1 << 13, { item: "Quest 6", cost: null })
+			.set(1 << 14, { item: "Heart 6", cost: null }),
 	},
 });
