b2498d652f09cd3b3162dea9ed9155defc9ff31c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-12-08 18:20:25 +0000

    chore(DoublefiveLightCatcher): Fix typo

--- source/Structures/Spirits/Seasonal/Lightseekers/DoublefiveLightCatcher.ts
+++ source/Structures/Spirits/Seasonal/Lightseekers/DoublefiveLightCatcher.ts
@@ -14,7 +14,7 @@ import {
 } from "../../Base.js";
 
 const action = FriendAction.DoubleFive;
-const actonEmoji = FRIEND_ACTIONS_EMOJIS.DoubleFive;
+const actionEmoji = FRIEND_ACTIONS_EMOJIS.DoubleFive;
 
 export default new SeasonalSpirit({
 	name: SpiritName.DoublefiveLightCatcher,
@@ -24,21 +24,21 @@ export default new SeasonalSpirit({
 	offer: {
 		hasInfographicSeasonal: false,
 		seasonal: new Collection<number, ItemsData>()
-			.set(1 << 0, { item: `${action} 1`, cost: null, emoji: actonEmoji })
+			.set(1 << 0, { item: `${action} 1`, cost: null, emoji: actionEmoji })
 			.set(1 << 1, { item: "Blessing 1", cost: { seasonalCandles: 4 } })
 			.set(1 << 8, { item: "Hair", cost: null })
 			.set(1 << 2, { item: "Mask", cost: { seasonalCandles: 6 } })
-			.set(1 << 6, { item: `${action} 2`, cost: null, emoji: actonEmoji })
+			.set(1 << 6, { item: `${action} 2`, cost: null, emoji: actionEmoji })
 			.set(1 << 5, { item: "Blessing 2", cost: { seasonalCandles: 8 } })
 			.set(1 << 7, { item: "Flute", cost: null }),
 		current: new Collection<number, ItemsData>()
-			.set(1 << 0, { item: `${action} 1`, cost: null, emoji: actonEmoji })
+			.set(1 << 0, { item: `${action} 1`, cost: null, emoji: actionEmoji })
 			.set(1 << 1, { item: "Blessing 1", cost: { candles: 5 } })
 			.set(1 << 2, { item: "Mask", cost: { candles: 24 } })
 			.set(1 << 3, { item: "Heart", cost: { candles: 3 }, emoji: MISCELLANEOUS_EMOJIS.Heart })
 			.set(1 << 4, { item: "Wing buff", cost: { ascendedCandles: 2 } })
 			.set(1 << 5, { item: "Blessing 2", cost: { candles: 5 } })
-			.set(1 << 6, { item: `${action} 2`, cost: { hearts: 7 }, emoji: actonEmoji })
+			.set(1 << 6, { item: `${action} 2`, cost: { hearts: 7 }, emoji: actionEmoji })
 			.set(1 << 7, { item: "Flute", cost: { candles: 55 } })
 			.set(1 << 8, { item: "Hair", cost: { candles: 34 } }),
 	},
