02d1850d108546d97e7e5ea5ee758415e9ba3674
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-01-08 00:42:28 +0000

    feat(AURORAGuide): Add emote emojis

--- source/Structures/Spirits/Seasonal/AURORA/AURORAGuide.ts
+++ source/Structures/Spirits/Seasonal/AURORA/AURORAGuide.ts
@@ -3,6 +3,7 @@ import { Collection } from "discord.js";
 import { Realm } from "../../../../Utility/Constants.js";
 import {
 	CAPE_EMOJIS,
+	EMOTES_EMOJIS,
 	HAIR_EMOJIS,
 	MASK_EMOJIS,
 	MISCELLANEOUS_EMOJIS,
@@ -19,25 +20,25 @@ export default new GuideSpirit({
 	offer: {
 		current: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: "Quest 1", cost: null })
-			.set(1 << 1, { item: `${Emote.SilentClap} 2`, cost: { hearts: 3 } })
+			.set(1 << 1, { item: `${Emote.SilentClap} 2`, cost: { hearts: 3 }, emoji: EMOTES_EMOJIS.SilentClap })
 			.set(1 << 2, { item: "Pendant", cost: null, emoji: NECKLACE_EMOJIS.Necklace23 })
 			.set(1 << 3, { item: "Aurora hair", cost: { seasonalHearts: 1 }, emoji: HAIR_EMOJIS.Hair116 })
 			.set(1 << 4, { item: "Ultimate outfit", cost: { seasonalHearts: 2 }, emoji: OUTFIT_EMOJIS.Outfit36 })
 			.set(1 << 5, { item: "Ultimate cape", cost: { seasonalHearts: 1 }, emoji: CAPE_EMOJIS.Cape95 })
-			.set(1 << 6, { item: `${Emote.SilentClap} 1`, cost: null })
-			.set(1 << 7, { item: `${Emote.SilentClap} 3`, cost: { candles: 5 } })
-			.set(1 << 8, { item: `${Emote.SilentClap} 4`, cost: { hearts: 5 } })
+			.set(1 << 6, { item: `${Emote.SilentClap} 1`, cost: null, emoji: EMOTES_EMOJIS.SilentClap })
+			.set(1 << 7, { item: `${Emote.SilentClap} 3`, cost: { candles: 5 }, emoji: EMOTES_EMOJIS.SilentClap })
+			.set(1 << 8, { item: `${Emote.SilentClap} 4`, cost: { hearts: 5 }, emoji: EMOTES_EMOJIS.SilentClap })
 			.set(1 << 9, { item: "Quest 2", cost: null })
-			.set(1 << 10, { item: `${Emote.Conduct} 1`, cost: null })
-			.set(1 << 11, { item: `${Emote.Conduct} 2`, cost: { hearts: 3 } })
-			.set(1 << 12, { item: `${Emote.Conduct} 3`, cost: { candles: 5 } })
-			.set(1 << 13, { item: `${Emote.Conduct} 4`, cost: { hearts: 5 } })
+			.set(1 << 10, { item: `${Emote.Conduct} 1`, cost: null, emoji: EMOTES_EMOJIS.Conduct })
+			.set(1 << 11, { item: `${Emote.Conduct} 2`, cost: { hearts: 3 }, emoji: EMOTES_EMOJIS.Conduct })
+			.set(1 << 12, { item: `${Emote.Conduct} 3`, cost: { candles: 5 }, emoji: EMOTES_EMOJIS.Conduct })
+			.set(1 << 13, { item: `${Emote.Conduct} 4`, cost: { hearts: 5 }, emoji: EMOTES_EMOJIS.Conduct })
 			.set(1 << 14, { item: "Quest 3", cost: null })
 			.set(1 << 15, { item: "Heart", cost: null, emoji: MISCELLANEOUS_EMOJIS.Heart })
-			.set(1 << 16, { item: `${Emote.Skipping} 1`, cost: null })
-			.set(1 << 17, { item: `${Emote.Skipping} 2`, cost: { hearts: 3 } })
-			.set(1 << 18, { item: `${Emote.Skipping} 3`, cost: { candles: 5 } })
-			.set(1 << 19, { item: `${Emote.Skipping} 4`, cost: { hearts: 5 } })
+			.set(1 << 16, { item: `${Emote.Skipping} 1`, cost: null, emoji: EMOTES_EMOJIS.Skipping })
+			.set(1 << 17, { item: `${Emote.Skipping} 2`, cost: { hearts: 3 }, emoji: EMOTES_EMOJIS.Skipping })
+			.set(1 << 18, { item: `${Emote.Skipping} 3`, cost: { candles: 5 }, emoji: EMOTES_EMOJIS.Skipping })
+			.set(1 << 19, { item: `${Emote.Skipping} 4`, cost: { hearts: 5 }, emoji: EMOTES_EMOJIS.Skipping })
 			.set(1 << 20, { item: "Quest 4", cost: null })
 			.set(1 << 21, { item: "Music sheet 1", cost: { candles: 20 } })
 			.set(1 << 22, { item: "Quest 5", cost: null })
