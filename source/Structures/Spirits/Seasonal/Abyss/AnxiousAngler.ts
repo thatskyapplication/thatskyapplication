879e6d53fdd3ae164dab956fd66d9db5175a6e01
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-01-18 09:06:38 +0000

    feat(AnxiousAngler): Update friendship tree

--- source/Structures/Spirits/Seasonal/Abyss/AnxiousAngler.ts
+++ source/Structures/Spirits/Seasonal/Abyss/AnxiousAngler.ts
@@ -36,7 +36,19 @@ export default new SeasonalSpirit({
 	emote,
 	realm: Realm.GoldenWasteland,
 	offer: {
-		hasInfographic: false,
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${emote} 1`, cost: null, emoji: emoteEmoji })
+			.set(1 << 1, { item: `${emote} 2`, cost: { hearts: 4 }, emoji: emoteEmoji })
+			.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 }, emoji: blessing2 })
+			.set(1 << 4, { item: "Hair", cost: { candles: 45 }, emoji: hairEmoji })
+			.set(1 << 12, { item: "heart", cost: { candles: 3 }, emoji: MISCELLANEOUS_EMOJIS.Heart })
+			.set(1 << 13, { item: "Wing buff", cost: { ascendedCandles: 2 }, emoji: MISCELLANEOUS_EMOJIS.WingBuff })
+			.set(1 << 6, { item: `${emote} 3`, cost: { hearts: 3 }, emoji: emoteEmoji })
+			.set(1 << 7, { item: `${emote} 4`, cost: { hearts: 6 }, emoji: emoteEmoji })
+			.set(1 << 3, { item: "Mask", cost: { candles: 35 }, emoji: maskEmoji })
+			.set(1 << 5, { item: "Blessing 2", cost: { candles: 5 }, emoji: blessing3 })
+			.set(1 << 9, { item: "Cape", cost: { candles: 70 }, emoji: capeEmoji })
+			.set(1 << 10, { item: "Outfit", cost: { candles: 65 }, emoji: outfitEmoji }),
 		seasonal: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: `${emote} 1`, cost: null, emoji: emoteEmoji })
 			.set(1 << 1, { item: `${emote} 2`, cost: null, emoji: emoteEmoji })
