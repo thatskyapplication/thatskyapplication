24500cf5bec84166664cf443a1305f355d5a276c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-01-28 13:41:02 +0000

    feat(Constants): Add 7th krilling GIF

--- source/Utility/Constants.ts
+++ source/Utility/Constants.ts
@@ -31,7 +31,7 @@ export const MAXIMUM_NOTIFICATION_CONCURRENCY_LIMIT = 45 as const;
 
 // Maximum GIF numbers.
 export const MAX_HUG_NO = 25 as const;
-export const MAX_KRILL_NO = 6 as const;
+export const MAX_KRILL_NO = 7 as const;
 
 // Miscellaneous constants.
 export const DEVELOPER_GUILD_ID = "1017993798170726411" as const;
