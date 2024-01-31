16158f80562be5f26033a117021389603185a007
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-01-31 18:57:23 +0000

    feat(SpiritOfMural): Update friendship tree

--- source/Structures/Spirits/Seasonal/NineColoredDeer/SpiritOfMural.ts
+++ source/Structures/Spirits/Seasonal/NineColoredDeer/SpiritOfMural.ts
@@ -23,6 +23,8 @@ export default new GuideSpirit({
 			.set(1 << 2, { item: "Pendant", cost: null, emoji: NECKLACE_EMOJIS.Necklace33 })
 			.set(1 << 3, { item: "Ultimate hair", cost: { seasonalHearts: 1 }, emoji: HAIR_EMOJIS.Hair136 })
 			.set(1 << 4, { item: "Ultimate outfit", cost: { seasonalHearts: 1 }, emoji: OUTFIT_EMOJIS.Outfit52 })
-			.set(1 << 5, { item: "Ultimate cape", cost: { seasonalHearts: 2 }, emoji: CAPE_EMOJIS.Cape121 }),
+			.set(1 << 5, { item: "Ultimate cape", cost: { seasonalHearts: 2 }, emoji: CAPE_EMOJIS.Cape121 })
+			.set(1 << 6, { item: "Quest 2", cost: null, emoji: MISCELLANEOUS_EMOJIS.Quest })
+			.set(1 << 7, { item: "Heart 2", cost: null, emoji: MISCELLANEOUS_EMOJIS.Heart }),
 	},
 });
