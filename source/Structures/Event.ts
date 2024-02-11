13c5d4e54a70bfd430aea9599550adbdb5f8f332
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-02-11 18:11:57 +0000

    refactor(Event): Days of Fortune extended by 3 days

--- source/Structures/Event.ts
+++ source/Structures/Event.ts
@@ -93,7 +93,7 @@ const EVENTS = [
 	new Event({
 		name: EventName.DaysOfFortune,
 		start: skyDate(2_024, 1, 29),
-		end: skyDate(2_024, 2, 11),
+		end: skyDate(2_024, 2, 14),
 		url: String(new URL("daily_guides/events/days_of_fortune/2024.webp", CDN_URL)),
 		eventCurrencyPerDay: 5,
 		eventCurrencyEmoji: EVENT_EMOJIS.Fortune,
