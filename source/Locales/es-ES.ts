946c53f40d7c6736b17ac7f5a33e49b68482b243
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-02-13 09:15:18 +0000

    feat(es-ES): Add hug translations

--- source/Locales/es-ES.ts
+++ source/Locales/es-ES.ts
@@ -246,5 +246,16 @@ export default {
 				"you-should-have": "Deberías tener",
 			},
 		},
+		hug: {
+			"command-name": "abrazo",
+			"command-description": "¡Abraza a alguien!",
+			user: "usuario",
+			"user-description": "El individuo para ser abrazado.",
+			"hug-self": "¡Comparte el amor! ¡Abraza a alguien que no seas tú!",
+			"not-in-server": "{{-user}} no está en el servidor para recibir tu abrazo.",
+			"not-around": "{{-user}} no está aquí para recibir el abrazo!",
+			bot: "{{-user}} es un bot. No tiene emociones. Es inmune a los abrazos, diría yo.",
+			message: "{{-user}}, ¡{{-invoker}} te ha abrazado!",
+		},
 	},
 } as const;
