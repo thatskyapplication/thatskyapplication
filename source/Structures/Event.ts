17ad60810ec0a4a5f8ffabd999d8fc43224c9c3a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-04-14 09:08:25 +0100

    feat(Event): Add Days of Bloom 2024 week 3 day 7 infographic

--- source/Structures/Event.ts
+++ source/Structures/Event.ts
@@ -159,6 +159,10 @@ const EVENTS = [
 				date: skyDate(2_024, 4, 13),
 				url: String(new URL("daily_guides/events/days_of_bloom/2024/week_3/6.webp", CDN_URL)),
 			},
+			{
+				date: skyDate(2_024, 4, 14),
+				url: String(new URL("daily_guides/events/days_of_bloom/2024/week_3/7.webp", CDN_URL)),
+			},
 		],
 		eventCurrencyPerDay: 5,
 		eventCurrencyEmoji: EVENT_EMOJIS.Bloom,
