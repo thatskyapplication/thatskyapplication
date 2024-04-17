c0749c11b19432c1d143d26746f72db241bb9c35
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-04-17 09:59:50 +0100

    feat(NestingGuide): Add and update friendship tree

--- source/Structures/Spirits/Seasonal/Nesting/NestingGuide.ts
+++ source/Structures/Spirits/Seasonal/Nesting/NestingGuide.ts
@@ -1,6 +1,11 @@
 /* eslint-disable unicorn/prefer-math-trunc */
 import { Collection } from "discord.js";
-import { MISCELLANEOUS_EMOJIS } from "../../../../Utility/emojis.js";
+import {
+	MISCELLANEOUS_EMOJIS,
+	NECKLACE_EMOJIS,
+	OUTFIT_EMOJIS,
+	SMALL_PLACEABLE_PROPS_EMOJIS,
+} from "../../../../Utility/emojis.js";
 import { SeasonName } from "../../../Season.js";
 import { type ItemsData, GuideSpirit, SpiritName } from "../../Base.js";
 
@@ -9,11 +14,15 @@ export default new GuideSpirit({
 	season: SeasonName.Nesting,
 	offer: {
 		inProgress: true,
-		hasInfographic: false,
-		current: new Collection<number, ItemsData>().set(1 << 0, {
-			item: "Quest 1",
-			cost: null,
-			emoji: MISCELLANEOUS_EMOJIS.Quest,
-		}),
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: "Quest 1", cost: null, emoji: MISCELLANEOUS_EMOJIS.Quest })
+			.set(1 << 1, { item: "Heart 1", cost: null, emoji: MISCELLANEOUS_EMOJIS.Heart })
+			.set(1 << 2, { item: "Pendant", cost: null, emoji: NECKLACE_EMOJIS.Necklace34 })
+			.set(1 << 3, { item: "Ultimate outfit", cost: { seasonalHearts: 2 }, emoji: OUTFIT_EMOJIS.Outfit57 })
+			.set(1 << 4, {
+				item: "Ultimate prop",
+				cost: { seasonalHearts: 2 },
+				emoji: SMALL_PLACEABLE_PROPS_EMOJIS.SmallPlaceableProp37,
+			}),
 	},
 });
