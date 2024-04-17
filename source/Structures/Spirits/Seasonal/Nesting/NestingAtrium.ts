d5e556c7612f55ee8a4c49e9c124eeae242d5c16
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-04-17 10:14:32 +0100

    feat(Nesting): Add and update friendship trees (#125)

--- source/Structures/Spirits/Seasonal/Nesting/NestingAtrium.ts
+++ source/Structures/Spirits/Seasonal/Nesting/NestingAtrium.ts
@@ -20,7 +20,6 @@ export default new SeasonalSpirit({
 	season: SeasonName.Nesting,
 	offer: {
 		hasInfographic: false,
-		hasInfographicSeasonal: false,
 		seasonal: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: "Prop 1", cost: { seasonalCandles: 16 }, emoji: SmallPlaceableProp40 })
 			.set(1 << 1, { item: "Blessing 1", cost: null, emoji: blessing3 })
--- source/Structures/Spirits/Seasonal/Nesting/NestingSolarium.ts
+++ source/Structures/Spirits/Seasonal/Nesting/NestingSolarium.ts
@@ -18,7 +18,6 @@ export default new SeasonalSpirit({
 	season: SeasonName.Nesting,
 	offer: {
 		hasInfographic: false,
-		hasInfographicSeasonal: false,
 		seasonal: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: "Blessing 1", cost: { seasonalCandles: 14 }, emoji: blessing3 })
 			.set(1 << 1, { item: "Prop 1", cost: null, emoji: SmallPlaceableProp38 })
