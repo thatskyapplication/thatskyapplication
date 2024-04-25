d5b7edb7329efeefe46d7cef2555c93635c53f7f
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-04-25 08:17:37 +0100

    feat(DancingPerformer): Update friendship tree

--- source/Structures/Spirits/Seasonal/Dreams/DancingPerformer.ts
+++ source/Structures/Spirits/Seasonal/Dreams/DancingPerformer.ts
@@ -36,6 +36,19 @@ export default new SeasonalSpirit({
 	realm: Realm.ValleyOfTriumph,
 	offer: {
 		hasInfographic: false,
+		current: new Collection<number, ItemsData>()
+			.set(1 << 0, { item: `${emote} 1`, cost: null, emoji: emoteEmoji })
+			.set(1 << 1, { item: `${emote} 2`, cost: { hearts: 4 }, emoji: emoteEmoji })
+			.set(1 << 2, { item: "Blessing 1", cost: { candles: 5 }, emoji: blessing2 })
+			.set(1 << 3, { item: "Hair", cost: { candles: 45 }, emoji: hairEmoji })
+			.set(1 << 10, { item: "Heart", cost: { candles: 3 }, emoji: MISCELLANEOUS_EMOJIS.Heart })
+			.set(1 << 11, { item: "Wing buff", cost: { ascendedCandles: 2 }, emoji: MISCELLANEOUS_EMOJIS.AscendedCandle })
+			.set(1 << 4, { item: `${emote} 3`, cost: { hearts: 3 }, emoji: emoteEmoji })
+			.set(1 << 5, { item: `${emote} 4`, cost: { hearts: 6 }, emoji: emoteEmoji })
+			.set(1 << 7, { item: "Mask", cost: { candles: 48 }, emoji: maskEmoji })
+			.set(1 << 6, { item: "Blessing 2", cost: { candles: 5 }, emoji: blessing2 })
+			.set(1 << 9, { item: "Lute", cost: { candles: 48 }, emoji: heldProp })
+			.set(1 << 8, { item: "Cape", cost: { candles: 70 }, emoji: capeEmoji }),
 		seasonal: new Collection<number, ItemsData>()
 			.set(1 << 0, { item: `${emote} 1`, cost: null, emoji: emoteEmoji })
 			.set(1 << 1, { item: `${emote} 2`, cost: null, emoji: emoteEmoji })
