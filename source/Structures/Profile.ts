6e132bc503e1521f382b8214655b2d57fed8c15d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-05-13 21:09:52 +0100

    feat(Profile): Add to progression

--- source/Structures/Profile.ts
+++ source/Structures/Profile.ts
@@ -19,6 +19,7 @@ import commands from "../Commands/index.js";
 import S3Client from "../S3Client.js";
 import { CDN_BUCKET, CDN_URL, DEFAULT_EMBED_COLOUR, MAXIMUM_WINGED_LIGHT } from "../Utility/Constants.js";
 import { formatEmoji, formatEmojiURL, MISCELLANEOUS_EMOJIS } from "../Utility/emojis.js";
+import { EVENTS } from "../catalogue/events/index.js";
 import { ELDER_SPIRITS, STANDARD_SPIRITS } from "../catalogue/spirits/realms/index.js";
 import { SEASON_SPIRITS, resolveBitsToSeasons } from "../catalogue/spirits/seasons/index.js";
 import pg, { Table } from "../pg.js";
@@ -447,10 +448,11 @@ export default class Profile {
 			const standardProgress = spiritTracker?.spiritProgress(STANDARD_SPIRITS, true) ?? 0;
 			const elderProgress = spiritTracker?.spiritProgress(ELDER_SPIRITS, true) ?? 0;
 			const seasonalProgress = spiritTracker?.spiritProgress(SEASON_SPIRITS, true) ?? 0;
+			const eventProgress = spiritTracker?.spiritProgress(EVENTS, true) ?? 0;
 
 			fields.push({
 				name: "Spirit Progression",
-				value: `Standard: ${standardProgress}%\nElder: ${elderProgress}%\nSeasonal: ${seasonalProgress}%`,
+				value: `Standard: ${standardProgress}%\nElder: ${elderProgress}%\nSeasonal: ${seasonalProgress}%\nEvent: ${eventProgress}%`,
 				inline: true,
 			});
 		} else if (commandId) {
