fed22bbb42f96c3fd24fad5b803c8afbaea50b15
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-05-16 16:33:45 +0100

    fix(SkyXCinnamorollPopUpCafe): Correct end date

--- source/catalogue/events/2024/SkyXCinnamorollPopUpCafe.ts
+++ source/catalogue/events/2024/SkyXCinnamorollPopUpCafe.ts
@@ -8,7 +8,7 @@ import { EVENT_EMOJIS } from "../../../Utility/emojis.js";
 export default new Event({
 	name: EventName.SkyXCinnamorollPopUpCafe,
 	start: skyDate(2_024, 4, 27),
-	end: skyDate(2_024, 5, 18),
+	end: skyDate(2_024, 5, 17),
 	url: String(new URL("daily_guides/events/sky_x_cinnamoroll_pop_up_cafe/2024.webp", CDN_URL)),
 	eventCurrencyPerDay: 5,
 	eventCurrencyEmoji: EVENT_EMOJIS.SkyXCinnamorollPopUpCafe,
