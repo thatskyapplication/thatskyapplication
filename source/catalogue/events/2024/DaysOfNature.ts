f84213f8c303cdce71cb53407726947a0e5bda43
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-05-22 21:49:44 +0100

    feat(DaysOfNature): Add offer

--- source/catalogue/events/2024/DaysOfNature.ts
+++ source/catalogue/events/2024/DaysOfNature.ts
@@ -1,5 +1,6 @@
+import { Collection } from "discord.js";
 import { Event } from "../../../Structures/Event.js";
-import { EventNameUnique } from "../../../Utility/catalogue.js";
+import { type ItemRaw, EventNameUnique } from "../../../Utility/catalogue.js";
 import { skyDate } from "../../../Utility/dates.js";
 
 export default new Event({
@@ -7,4 +8,11 @@ export default new Event({
 	start: skyDate(2_024, 5, 27),
 	end: skyDate(2_024, 6, 16),
 	eventCurrencyPerDay: 4,
+	offer: new Collection<number, ItemRaw>()
+		// @ts-expect-error Emoji has not yet been created.
+		.set(1 << 0, { name: "Ocean Mask", cost: { eventCurrency: 16 }, emoji: null })
+		// @ts-expect-error Emoji has not yet been created.
+		.set(1 << 1, { name: "Ocean Blue Scarf", cost: { eventCurrency: 40 }, emoji: null })
+		// @ts-expect-error Emoji has not yet been created.
+		.set(1 << 2, { name: "Nature Wave Pack", cost: { money: 19.99 }, emoji: null }),
 });
