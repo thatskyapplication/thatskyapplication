667f08b7350d487fc45173cfdb89797f3c3311ce
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-05-26 12:07:57 +0100

    refactor: Default emojis

--- source/catalogue/events/2024/DaysOfNature.ts
+++ source/catalogue/events/2024/DaysOfNature.ts
@@ -2,6 +2,7 @@ import { Collection } from "discord.js";
 import { Event } from "../../../Structures/Event.js";
 import { type ItemRaw, EventNameUnique } from "../../../Utility/catalogue.js";
 import { skyDate } from "../../../Utility/dates.js";
+import { MISCELLANEOUS_EMOJIS } from "../../../Utility/emojis.js";
 
 export default new Event({
 	nameUnique: EventNameUnique.DaysOfNature2024,
@@ -9,10 +10,10 @@ export default new Event({
 	end: skyDate(2_024, 6, 16),
 	eventCurrencyPerDay: 4,
 	offer: new Collection<number, ItemRaw>()
-		// @ts-expect-error Emoji has not yet been created.
-		.set(1 << 0, { name: "Ocean Mask", cost: { eventCurrency: 16 }, emoji: null })
-		// @ts-expect-error Emoji has not yet been created.
-		.set(1 << 1, { name: "Ocean Blue Scarf", cost: { eventCurrency: 40 }, emoji: null })
-		// @ts-expect-error Emoji has not yet been created.
-		.set(1 << 2, { name: "Nature Wave Pack", cost: { money: 19.99 }, emoji: null }),
+		// TODO: Add emoji.
+		.set(1 << 0, { name: "Ocean Mask", cost: { eventCurrency: 16 }, emoji: MISCELLANEOUS_EMOJIS.EventCurrency })
+		// TODO: Add emoji.
+		.set(1 << 1, { name: "Ocean Blue Scarf", cost: { eventCurrency: 40 }, emoji: MISCELLANEOUS_EMOJIS.EventCurrency })
+		// TODO: Add emoji.
+		.set(1 << 2, { name: "Nature Wave Pack", cost: { money: 19.99 }, emoji: MISCELLANEOUS_EMOJIS.EventCurrency }),
 });
