237ff5ecb3b6229e3a9bedde5df7c7ab001bf0c1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-05-27 08:45:37 +0100

    feat: Add Days of Nature 2024 emojis

--- source/catalogue/events/2024/DaysOfNature.ts
+++ source/catalogue/events/2024/DaysOfNature.ts
@@ -1,7 +1,7 @@
 import { Event } from "../../../Structures/Event.js";
 import { EventName } from "../../../Utility/catalogue.js";
 import { skyDate } from "../../../Utility/dates.js";
-import { MISCELLANEOUS_EMOJIS } from "../../../Utility/emojis.js";
+import { EVENT_EMOJIS } from "../../../Utility/emojis.js";
 
 export default new Event({
 	name: EventName.DaysOfNature,
@@ -9,6 +9,5 @@ export default new Event({
 	end: skyDate(2_024, 6, 16),
 	url: null,
 	eventCurrencyPerDay: 4,
-	// TODO: Add event currency emoji.
-	eventCurrencyEmoji: MISCELLANEOUS_EMOJIS.EventCurrency,
+	eventCurrencyEmoji: EVENT_EMOJIS.Nature,
 });
