e4df32823d24ea2ebe51964dfa469a11704b84d7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-07-14 04:04:46 +0100

    feat: Season of Rhythm

--- source/catalogue/spirits/seasons/Rhythm/AdmiringActor.ts
+++ source/catalogue/spirits/seasons/Rhythm/AdmiringActor.ts
@@ -5,7 +5,7 @@ import {
 	type SeasonalSpiritVisitCollectionKey,
 } from "../../../../Structures/Spirits.js";
 import { RealmName } from "../../../../Utility/Constants.js";
-import { SeasonName } from "../../../../Utility/catalogue.js";
+import { Cosmetic, SeasonName } from "../../../../Utility/catalogue.js";
 import { skyDate } from "../../../../Utility/dates.js";
 import {
 	MASK_EMOJIS,
@@ -31,38 +31,96 @@ export default new SeasonalSpirit({
 	offer: {
 		hasInfographicSeasonal: false,
 		seasonal: [
-			{ name: `${emote} 1`, bit: 1 << 0, emoji: emoteEmoji },
-			{ name: `${emote} 2`, bit: 1 << 1, emoji: emoteEmoji },
-			{ name: "Blessing", bit: 1 << 2, cost: { seasonalCandles: 12 }, emoji: blessing2 },
-			{ name: "Music sheet", bit: 1 << 3, emoji: musicSheet },
-			{ name: `${emote} 3`, bit: 1 << 6, cost: { seasonalCandles: 14 }, emoji: emoteEmoji },
-			{ name: `${emote} 4`, bit: 1 << 7, emoji: emoteEmoji },
-			{ name: "Outfit", bit: 1 << 9, cost: { seasonalCandles: 16 }, emoji: outfitEmoji },
-			{ name: "Mask", bit: 1 << 10, emoji: maskEmoji },
+			{ name: `${emote} 1`, cosmetic: Cosmetic.EmoteBlowKiss1, emoji: emoteEmoji },
+			{ name: `${emote} 2`, cosmetic: Cosmetic.EmoteBlowKiss2, emoji: emoteEmoji },
+			{
+				name: "Blessing",
+				cosmetic: Cosmetic.AdmiringActorBlessing1,
+				cost: { seasonalCandles: 12 },
+				emoji: blessing2,
+			},
+			{ name: "Music sheet", cosmetic: Cosmetic.AdmiringActorMusicSheet, emoji: musicSheet },
+			{
+				name: `${emote} 3`,
+				cosmetic: Cosmetic.EmoteBlowKiss3,
+				cost: { seasonalCandles: 14 },
+				emoji: emoteEmoji,
+			},
+			{ name: `${emote} 4`, cosmetic: Cosmetic.EmoteBlowKiss4, emoji: emoteEmoji },
+			{
+				name: "Outfit",
+				cosmetic: Cosmetic.AdmiringActorOutfit,
+				cost: { seasonalCandles: 16 },
+				emoji: outfitEmoji,
+			},
+			{ name: "Mask", cosmetic: Cosmetic.AdmiringActorMask, emoji: maskEmoji },
 			{
 				name: "Seasonal heart",
-				bit: 1 << 4,
+				cosmetic: Cosmetic.AdmiringActorSeasonalHeart,
 				cost: { seasonalCandles: 3 },
 				emoji: SEASON_EMOJIS.RhythmHeart,
 			},
 		],
 		current: [
-			{ name: `${emote} 1`, bit: 1 << 0, emoji: emoteEmoji },
-			{ name: `${emote} 2`, bit: 1 << 1, cost: { hearts: 4 }, emoji: emoteEmoji },
-			{ name: "Blessing 1", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing2 },
-			{ name: "Music sheet", bit: 1 << 3, cost: { candles: 15 }, emoji: musicSheet },
-			{ name: "Heart", bit: 1 << 4, cost: { candles: 3 }, emoji: MISCELLANEOUS_EMOJIS.Heart },
+			{ name: `${emote} 1`, cosmetic: Cosmetic.EmoteBlowKiss1, emoji: emoteEmoji },
+			{
+				name: `${emote} 2`,
+				cosmetic: Cosmetic.EmoteBlowKiss2,
+				cost: { hearts: 4 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: "Blessing 1",
+				cosmetic: Cosmetic.AdmiringActorBlessing1,
+				cost: { candles: 5 },
+				emoji: blessing2,
+			},
+			{
+				name: "Music sheet",
+				cosmetic: Cosmetic.AdmiringActorMusicSheet,
+				cost: { candles: 15 },
+				emoji: musicSheet,
+			},
+			{
+				name: "Heart",
+				cosmetic: Cosmetic.AdmiringActorSeasonalHeart,
+				cost: { candles: 3 },
+				emoji: MISCELLANEOUS_EMOJIS.Heart,
+			},
 			{
 				name: "Wing buff",
-				bit: 1 << 5,
 				cost: { ascendedCandles: 2 },
 				emoji: MISCELLANEOUS_EMOJIS.WingBuff,
 			},
-			{ name: `${emote} 3`, bit: 1 << 6, cost: { hearts: 3 }, emoji: emoteEmoji },
-			{ name: `${emote} 4`, bit: 1 << 7, cost: { hearts: 6 }, emoji: emoteEmoji },
-			{ name: "Blessing 2", bit: 1 << 8, cost: { candles: 5 }, emoji: blessing2 },
-			{ name: "Outfit", bit: 1 << 9, cost: { candles: 65 }, emoji: outfitEmoji },
-			{ name: "Mask", bit: 1 << 10, cost: { candles: 42 }, emoji: maskEmoji },
+			{
+				name: `${emote} 3`,
+				cosmetic: Cosmetic.EmoteBlowKiss3,
+				cost: { hearts: 3 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: `${emote} 4`,
+				cosmetic: Cosmetic.EmoteBlowKiss4,
+				cost: { hearts: 6 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: "Blessing 2",
+				cost: { candles: 5 },
+				emoji: blessing2,
+			},
+			{
+				name: "Outfit",
+				cosmetic: Cosmetic.AdmiringActorOutfit,
+				cost: { candles: 65 },
+				emoji: outfitEmoji,
+			},
+			{
+				name: "Mask",
+				cosmetic: Cosmetic.AdmiringActorMask,
+				cost: { candles: 42 },
+				emoji: maskEmoji,
+			},
 		],
 	},
 	visits: {
--- source/catalogue/spirits/seasons/Rhythm/RespectfulPianist.ts
+++ source/catalogue/spirits/seasons/Rhythm/RespectfulPianist.ts
@@ -5,7 +5,7 @@ import {
 	type SeasonalSpiritVisitCollectionKey,
 } from "../../../../Structures/Spirits.js";
 import { RealmName } from "../../../../Utility/Constants.js";
-import { SeasonName } from "../../../../Utility/catalogue.js";
+import { Cosmetic, SeasonName } from "../../../../Utility/catalogue.js";
 import { skyDate } from "../../../../Utility/dates.js";
 import {
 	HAIR_EMOJIS,
@@ -31,40 +31,104 @@ export default new SeasonalSpirit({
 	offer: {
 		hasInfographicSeasonal: false,
 		seasonal: [
-			{ name: `${emote} 1`, bit: 1 << 0, emoji: emoteEmoji },
-			{ name: `${emote} 2`, bit: 1 << 1, emoji: emoteEmoji },
-			{ name: "Hair", bit: 1 << 3, cost: { seasonalCandles: 14 }, emoji: hairEmoji },
-			{ name: "Blessing 1", bit: 1 << 2, emoji: blessing2 },
-			{ name: `${emote} 3`, bit: 1 << 6, cost: { seasonalCandles: 16 }, emoji: emoteEmoji },
-			{ name: `${emote} 4`, bit: 1 << 7, emoji: emoteEmoji },
-			{ name: "Blessing 2", bit: 1 << 8, cost: { seasonalCandles: 18 }, emoji: blessing2 },
-			{ name: "Winter piano", bit: 1 << 9, emoji: heldProp },
-			{ name: "Duck mask", bit: 1 << 10, cost: { seasonalCandles: 20 }, emoji: maskEmoji },
-			{ name: "Blessing 3", bit: 1 << 11, emoji: blessing2 },
+			{ name: `${emote} 1`, cosmetic: Cosmetic.EmoteRespect1, emoji: emoteEmoji },
+			{ name: `${emote} 2`, cosmetic: Cosmetic.EmoteRespect2, emoji: emoteEmoji },
+			{
+				name: "Hair",
+				cosmetic: Cosmetic.RespectfulPianistHair,
+				cost: { seasonalCandles: 14 },
+				emoji: hairEmoji,
+			},
+			{ name: "Blessing 1", cosmetic: Cosmetic.RespectfulPianistBlessing1, emoji: blessing2 },
+			{
+				name: `${emote} 3`,
+				cosmetic: Cosmetic.EmoteRespect3,
+				cost: { seasonalCandles: 16 },
+				emoji: emoteEmoji,
+			},
+			{ name: `${emote} 4`, cosmetic: Cosmetic.EmoteRespect4, emoji: emoteEmoji },
+			{
+				name: "Blessing 2",
+				cosmetic: Cosmetic.RespectfulPianistBlessing2,
+				cost: { seasonalCandles: 18 },
+				emoji: blessing2,
+			},
+			{ name: "Winter piano", cosmetic: Cosmetic.RespectfulPianistWinterPiano, emoji: heldProp },
+			{
+				name: "Duck mask",
+				cosmetic: Cosmetic.RespectfulPianistMask,
+				cost: { seasonalCandles: 20 },
+				emoji: maskEmoji,
+			},
+			{ name: "Blessing 3", cosmetic: Cosmetic.RespectfulPianistBlessing3, emoji: blessing2 },
 			{
 				name: "Seasonal heart",
-				bit: 1 << 4,
+				cosmetic: Cosmetic.RespectfulPianistSeasonalHeart,
 				cost: { seasonalCandles: 3 },
 				emoji: SEASON_EMOJIS.RhythmHeart,
 			},
 		],
 		current: [
-			{ name: `${emote} 1`, bit: 1 << 0, emoji: emoteEmoji },
-			{ name: `${emote} 2`, bit: 1 << 1, cost: { hearts: 4 }, emoji: emoteEmoji },
-			{ name: "Blessing 1", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing2 },
-			{ name: "Hair", bit: 1 << 3, cost: { candles: 26 }, emoji: hairEmoji },
-			{ name: "Heart", bit: 1 << 4, cost: { candles: 3 }, emoji: MISCELLANEOUS_EMOJIS.Heart },
+			{ name: `${emote} 1`, cosmetic: Cosmetic.EmoteRespect1, emoji: emoteEmoji },
+			{
+				name: `${emote} 2`,
+				cosmetic: Cosmetic.EmoteRespect2,
+				cost: { hearts: 4 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: "Blessing 1",
+				cosmetic: Cosmetic.RespectfulPianistBlessing1,
+				cost: { candles: 5 },
+				emoji: blessing2,
+			},
+			{
+				name: "Hair",
+				cosmetic: Cosmetic.RespectfulPianistHair,
+				cost: { candles: 26 },
+				emoji: hairEmoji,
+			},
+			{
+				name: "Heart",
+				cosmetic: Cosmetic.RespectfulPianistSeasonalHeart,
+				cost: { candles: 3 },
+				emoji: MISCELLANEOUS_EMOJIS.Heart,
+			},
 			{
 				name: "Wing buff",
-				bit: 1 << 5,
 				cost: { ascendedCandles: 2 },
 				emoji: MISCELLANEOUS_EMOJIS.WingBuff,
 			},
-			{ name: `${emote} 3`, bit: 1 << 6, cost: { hearts: 3 }, emoji: emoteEmoji },
-			{ name: `${emote} 4`, bit: 1 << 7, cost: { hearts: 6 }, emoji: emoteEmoji },
-			{ name: "Blessing 2", bit: 1 << 8, cost: { candles: 5 }, emoji: blessing2 },
-			{ name: "Winter piano", bit: 1 << 9, cost: { candles: 75 }, emoji: heldProp },
-			{ name: "Duck mask", bit: 1 << 10, cost: { candles: 48 }, emoji: maskEmoji },
+			{
+				name: `${emote} 3`,
+				cosmetic: Cosmetic.EmoteRespect3,
+				cost: { hearts: 3 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: `${emote} 4`,
+				cosmetic: Cosmetic.EmoteRespect4,
+				cost: { hearts: 6 },
+				emoji: emoteEmoji,
+			},
+			{
+				name: "Blessing 2",
+				cosmetic: Cosmetic.RespectfulPianistBlessing2,
+				cost: { candles: 5 },
+				emoji: blessing2,
+			},
+			{
+				name: "Winter piano",
+				cosmetic: Cosmetic.RespectfulPianistWinterPiano,
+				cost: { candles: 75 },
+				emoji: heldProp,
+			},
+			{
+				name: "Duck mask",
+				cosmetic: Cosmetic.RespectfulPianistMask,
+				cost: { candles: 48 },
+				emoji: maskEmoji,
+			},
 		],
 	},
 	visits: {
