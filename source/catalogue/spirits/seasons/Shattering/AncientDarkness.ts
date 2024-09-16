8c01584d2e58e39a68399ddd3c6134b409df0310
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-09-16 09:06:21 +0100

    feat(Shattering): Add current friendship trees

--- source/catalogue/spirits/seasons/Shattering/AncientDarkness.ts
+++ source/catalogue/spirits/seasons/Shattering/AncientDarkness.ts
@@ -26,7 +26,6 @@ export default [
 		name: SpiritName.AncientDarkness1,
 		season: SeasonName.Shattering,
 		offer: {
-			hasInfographic: false,
 			seasonal: [
 				{
 					name: "Hair accessory",
@@ -48,6 +47,32 @@ export default [
 					emoji: SEASON_EMOJIS.ShatteringHeart,
 				},
 			],
+			current: [
+				{ name: "Blessing 1", bit: 1 << 1, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Music sheet", bit: 1 << 4, cost: { candles: 15 }, emoji: musicSheet },
+				{
+					name: "Heart",
+					bit: 1 << 8,
+					cost: { candles: 3 },
+					emoji: MISCELLANEOUS_EMOJIS.Heart,
+				},
+				{ name: "Blessing 2", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing3 },
+				{
+					name: "Hair accessory",
+					bit: 1 << 0,
+					cost: { candles: 47 },
+					emoji: hairAccessoryEmoji,
+				},
+				{
+					name: "Wing buff",
+					bit: 1 << 9,
+					cost: { ascendedCandles: 2 },
+					emoji: MISCELLANEOUS_EMOJIS.WingBuff,
+				},
+				{ name: "Blessing 3", bit: 1 << 5, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Mask", bit: 1 << 3, cost: { candles: 50 }, emoji: maskEmoji },
+				{ name: "Cape", bit: 1 << 7, cost: { candles: 80 }, emoji: capeEmoji },
+			],
 		},
 		visits: {
 			returning: [6],
@@ -57,7 +82,6 @@ export default [
 		name: SpiritName.AncientDarkness2,
 		season: SeasonName.Shattering,
 		offer: {
-			hasInfographic: false,
 			seasonal: [
 				{
 					name: "Neck accessory",
@@ -76,6 +100,31 @@ export default [
 					cost: { seasonalCandles: 3 },
 					emoji: SEASON_EMOJIS.ShatteringHeart,
 				},
+			],
+			current: [
+				{ name: "Blessing 1", bit: 1 << 1, cost: { candles: 5 }, emoji: blessing3 },
+				{
+					name: "Neck accessory",
+					bit: 1 << 0,
+					cost: { candles: 70 },
+					emoji: necklaceEmoji,
+				},
+				{
+					name: "Heart",
+					bit: 1 << 6,
+					cost: { candles: 3 },
+					emoji: MISCELLANEOUS_EMOJIS.Heart,
+				},
+				{ name: "Blessing 2", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing3 },
+				{
+					name: "Wing buff",
+					bit: 1 << 7,
+					cost: { ascendedCandles: 2 },
+					emoji: MISCELLANEOUS_EMOJIS.WingBuff,
+				},
+				{ name: "Blessing 3", bit: 1 << 5, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Hair", bit: 1 << 4, cost: { candles: 70 }, emoji: hairEmoji },
+				{ name: "Dark horn", bit: 1 << 3, cost: { candles: 50 }, emoji: heldProp },
 			],
 		},
 		visits: {
--- source/catalogue/spirits/seasons/Shattering/AncientLight.ts
+++ source/catalogue/spirits/seasons/Shattering/AncientLight.ts
@@ -24,7 +24,6 @@ export default [
 		name: SpiritName.AncientLight1,
 		season: SeasonName.Shattering,
 		offer: {
-			hasInfographic: false,
 			seasonal: [
 				{ name: "Hair", bit: 1 << 0, cost: { seasonalCandles: 35 }, emoji: hairEmoji2 },
 				{ name: "Blessing 1", bit: 1 << 1, emoji: blessing3 },
@@ -39,6 +38,26 @@ export default [
 					emoji: SEASON_EMOJIS.ShatteringHeart,
 				},
 			],
+			current: [
+				{ name: "Blessing 1", bit: 1 << 1, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Hair", bit: 1 << 0, cost: { candles: 45 }, emoji: hairEmoji2 },
+				{
+					name: "Heart",
+					bit: 1 << 6,
+					cost: { candles: 3 },
+					emoji: MISCELLANEOUS_EMOJIS.Heart,
+				},
+				{ name: "Blessing 2", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing3 },
+				{
+					name: "Wing buff",
+					bit: 1 << 7,
+					cost: { ascendedCandles: 2 },
+					emoji: MISCELLANEOUS_EMOJIS.WingBuff,
+				},
+				{ name: "Blessing 3", bit: 1 << 5, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Hair accessory", bit: 1 << 3, cost: { candles: 50 }, emoji: hairAccessoryEmoji },
+				{ name: "Cape", bit: 1 << 4, cost: { candles: 80 }, emoji: capeEmoji1 },
+			],
 		},
 		visits: {
 			returning: [6],
@@ -48,7 +67,6 @@ export default [
 		name: SpiritName.AncientLight2,
 		season: SeasonName.Shattering,
 		offer: {
-			hasInfographic: false,
 			seasonal: [
 				{ name: "Music sheet", bit: 1 << 0, cost: { seasonalCandles: 27 }, emoji: musicSheet },
 				{ name: "Blessing 1", bit: 1 << 1, emoji: blessing3 },
@@ -64,6 +82,27 @@ export default [
 					cost: { seasonalCandles: 3 },
 					emoji: SEASON_EMOJIS.ShatteringHeart,
 				},
+			],
+			current: [
+				{ name: "Blessing 1", bit: 1 << 1, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Music sheet", bit: 1 << 0, cost: { candles: 15 }, emoji: musicSheet },
+				{
+					name: "Heart",
+					bit: 1 << 8,
+					cost: { candles: 3 },
+					emoji: MISCELLANEOUS_EMOJIS.Heart,
+				},
+				{ name: "Blessing 2", bit: 1 << 2, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Hair", bit: 1 << 3, cost: { candles: 50 }, emoji: hairEmoji1 },
+				{
+					name: "Wing buff",
+					bit: 1 << 9,
+					cost: { ascendedCandles: 2 },
+					emoji: MISCELLANEOUS_EMOJIS.WingBuff,
+				},
+				{ name: "Blessing 3", bit: 1 << 5, cost: { candles: 5 }, emoji: blessing3 },
+				{ name: "Outfit", bit: 1 << 7, cost: { candles: 75 }, emoji: outfitEmoji },
+				{ name: "Cape", bit: 1 << 4, cost: { candles: 80 }, emoji: capeEmoji2 },
 			],
 		},
 		visits: {
