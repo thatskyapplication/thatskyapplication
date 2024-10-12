4c0fc999c993b74646f4ad2e85cf4d040cfc3a73
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-10-12 21:46:14 +0100

    refactor: Move translations into own file

---
+++ source/i18next.ts
@@ -0,0 +1,40 @@
+import { Locale } from "discord.js";
+import { init } from "i18next";
+import de from "./Locales/de.js";
+import enGB from "./Locales/en-GB.js";
+import es419 from "./Locales/es-419.js";
+import esES from "./Locales/es-ES.js";
+import fr from "./Locales/fr.js";
+import it from "./Locales/it.js";
+import ja from "./Locales/ja.js";
+import ko from "./Locales/ko.js";
+import ptBR from "./Locales/pt-BR.js";
+import ru from "./Locales/ru.js";
+import vi from "./Locales/vi.js";
+import zhCN from "./Locales/zh-CN.js";
+import zhTW from "./Locales/zh-TW.js";
+import pino from "./pino.js";
+
+void init({
+	fallbackLng: Locale.EnglishGB,
+	missingKeyHandler: (locale, namespace, key) =>
+		pino.warn(`Locale ${locale} had a missing translation in namespace ${namespace} for "${key}".`),
+	ns: ["general", "commands"],
+	resources: {
+		[Locale.German]: de,
+		[Locale.EnglishGB]: enGB,
+		[Locale.SpanishLATAM]: es419,
+		[Locale.SpanishES]: esES,
+		[Locale.French]: fr,
+		[Locale.Italian]: it,
+		[Locale.Japanese]: ja,
+		[Locale.Korean]: ko,
+		[Locale.PortugueseBR]: ptBR,
+		[Locale.Russian]: ru,
+		[Locale.Vietnamese]: vi,
+		[Locale.ChineseCN]: zhCN,
+		[Locale.ChineseTW]: zhTW,
+	},
+	returnEmptyString: false,
+	saveMissing: true,
+});
--- source/index.ts
+++ source/index.ts
@@ -6,29 +6,14 @@ import {
 	Client,
 	EmbedBuilder,
 	GatewayIntentBits,
-	Locale,
 	Options,
 	Partials,
 	PermissionFlagsBits,
 	type Snowflake,
 	TextChannel,
 } from "discord.js";
-import { init } from "i18next";
 import commands from "./Commands/index.js";
 import events, { type Event } from "./Events/index.js";
-import de from "./Locales/de.js";
-import enGB from "./Locales/en-GB.js";
-import es419 from "./Locales/es-419.js";
-import esES from "./Locales/es-ES.js";
-import fr from "./Locales/fr.js";
-import it from "./Locales/it.js";
-import ja from "./Locales/ja.js";
-import ko from "./Locales/ko.js";
-import ptBR from "./Locales/pt-BR.js";
-import ru from "./Locales/ru.js";
-import vi from "./Locales/vi.js";
-import zhCN from "./Locales/zh-CN.js";
-import zhTW from "./Locales/zh-TW.js";
 import {
 	APPLICATION_ID,
 	DEFAULT_EMBED_COLOUR,
@@ -36,6 +21,7 @@ import {
 	PRODUCTION,
 } from "./Utility/Constants.js";
 import pino from "./pino.js";
+import "./i18next.js";
 
 interface LogOptions {
 	content?: string;
@@ -49,30 +35,6 @@ declare module "discord.js" {
 		applyCommands(): Promise<void>;
 	}
 }
-
-void init({
-	fallbackLng: Locale.EnglishGB,
-	missingKeyHandler: (locale, namespace, key) =>
-		pino.warn(`Locale ${locale} had a missing translation in namespace ${namespace} for "${key}".`),
-	ns: ["general", "commands"],
-	resources: {
-		[Locale.German]: de,
-		[Locale.EnglishGB]: enGB,
-		[Locale.SpanishLATAM]: es419,
-		[Locale.SpanishES]: esES,
-		[Locale.French]: fr,
-		[Locale.Italian]: it,
-		[Locale.Japanese]: ja,
-		[Locale.Korean]: ko,
-		[Locale.PortugueseBR]: ptBR,
-		[Locale.Russian]: ru,
-		[Locale.Vietnamese]: vi,
-		[Locale.ChineseCN]: zhCN,
-		[Locale.ChineseTW]: zhTW,
-	},
-	returnEmptyString: false,
-	saveMissing: true,
-});
 
 class Caelus extends Client {
 	public override async log({ content, embeds = [], error }: LogOptions) {
