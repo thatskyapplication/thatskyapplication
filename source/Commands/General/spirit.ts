e4089e89d6f5d5e31e2c8e6e02687524ae5a8485
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-10-17 13:45:43 +0100

    fix(spirit): Correct command name

--- source/Commands/General/spirit.ts
+++ source/Commands/General/spirit.ts
@@ -47,7 +47,7 @@ export const SPIRIT_SEASONAL_FRIENDSHIP_TREE_BUTTON_CUSTOM_ID =
 	"SPIRIT_VIEW_SEASONAL_BUTTON_CUSTOM_ID" as const;
 
 export default new (class implements AutocompleteCommand {
-	public readonly name = t("sky-profile.command-name", { lng: Locale.EnglishGB, ns: "commands" });
+	public readonly name = t("spirit.command-name", { lng: Locale.EnglishGB, ns: "commands" });
 
 	public async chatInput(interaction: ChatInputCommandInteraction) {
 		switch (interaction.options.getSubcommand()) {
