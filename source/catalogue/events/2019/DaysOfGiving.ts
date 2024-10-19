f524d1630578a580f6acbd9db2f239491fc438f7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-10-19 17:36:24 +0100

    fix(DaysOfGiving): Correct end date

--- source/catalogue/events/2019/DaysOfGiving.ts
+++ source/catalogue/events/2019/DaysOfGiving.ts
@@ -5,5 +5,5 @@ import { skyDate } from "../../../Utility/dates.js";
 export default new Event({
 	id: EventId.DaysOfGiving2019,
 	start: skyDate(2_019, 11, 26),
-	end: skyDate(2_019, 12, 3, 12),
+	end: skyDate(2_019, 12, 4, 12),
 });
