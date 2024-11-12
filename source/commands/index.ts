a8688eb58f8251da9efe28877bcc1ae4bf71b13e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-11-12 22:07:59 +0000

    feat: Bonk

--- source/commands/index.ts
+++ source/commands/index.ts
@@ -1,7 +1,7 @@
 import about from "./chat-inputs/about.js";
 import admin from "./chat-inputs/admin.js";
 import ai from "./chat-inputs/ai.js";
-// import bonk from "./chat-inputs/bonk.js";
+import bonk from "./chat-inputs/bonk.js";
 // import calculate from "./chat-inputs/calculate.js";
 // import catalogue from "./chat-inputs/catalogue.js";
 // import daily_guides from "./chat-inputs/daily-guides.js";
@@ -24,7 +24,7 @@ export const CHAT_INPUT_COMMANDS = [
 	about,
 	admin,
 	ai,
-	// bonk,
+	bonk,
 	// calculate,
 	// catalogue,
 	// daily_guides,
