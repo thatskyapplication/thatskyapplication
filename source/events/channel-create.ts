3ef995dd5b75a7e9a7ef9929feadf8f102c3eedc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-11-27 17:51:59 +0000

    refactor(channel-create): Clearer logic

--- source/events/channel-create.ts
+++ source/events/channel-create.ts
@@ -8,7 +8,13 @@ const name = GatewayDispatchEvents.ChannelCreate;
 export default {
 	name,
 	fire({ data }) {
-		GUILD_CACHE.get(data.guild_id)?.channels.set(data.id, data) ??
+		const guild = GUILD_CACHE.get(data.guild_id);
+
+		if (!guild) {
 			pino.warn({ data }, `Received a ${name} packet for an uncached guild.`);
+			return;
+		}
+
+		guild.channels.set(data.id, data);
 	},
 } satisfies Event<typeof name>;
