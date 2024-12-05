e5890ade6b9a842482547cdc5cf7bcf9685291cf
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-12-05 02:40:06 +0000

    refactor(days-of-music): Extend end date by 2 days
    https://x.com/thatskygame/status/1864496753686512115

--- source/data/events/2024/days-of-music.ts
+++ source/data/events/2024/days-of-music.ts
@@ -11,7 +11,7 @@ import {
 const eventCurrencyAmount = [];
 
 for (
-	let start = skyDate(2_024, 11, 25), end = skyDate(2_024, 12, 9);
+	let start = skyDate(2_024, 11, 25), end = skyDate(2_024, 12, 11);
 	start < end;
 	start = start.plus({ days: 1 })
 ) {
@@ -24,14 +24,14 @@ for (
 export default new Event({
 	id: EventId.DaysOfMusic2024,
 	start: skyDate(2_024, 11, 25),
-	end: skyDate(2_024, 12, 9),
+	end: skyDate(2_024, 12, 11),
 	eventCurrency: {
 		amount: eventCurrencyAmount,
 		pool: [
 			{
 				amount: 15,
 				start: skyDate(2_024, 11, 25),
-				end: skyDate(2_024, 12, 8),
+				end: skyDate(2_024, 12, 10),
 			},
 		],
 	},
