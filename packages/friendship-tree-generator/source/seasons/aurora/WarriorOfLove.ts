e889058427e592e81888a1b45c5a19770ea85186
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-02-27 01:53:36 +0000

    feat(WarriorOfLove): Add spirit

---
+++ packages/friendship-tree-generator/source/seasons/aurora/WarriorOfLove.ts
@@ -0,0 +1,142 @@
+import { SeasonId } from "@thatskyapplication/utility";
+import { HEART_URL } from "../../constants.js";
+import type { FriendshipTreeData } from "../../nodes.js";
+
+const seasonId = SeasonId.AURORA;
+
+export const SEASONAL = [
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 2,
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b5/SoAurora-Warrior-of-Love-Mask-Icon-Morybel-0146.png/revision/latest",
+			cost: { seasonalCandles: { cost: 6, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { seasonalCandles: { cost: 12, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6b/SoAurora-Warrior-of-Love-Hair-Icon-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 3,
+			cost: { seasonalCandles: { cost: 20, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 4,
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4e/MusicSheet.png/revision/latest",
+			cost: { seasonalCandles: { cost: 24, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { seasonalCandles: { cost: 30, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a9/SoAurora-Warrior-of-Love-Cape-Icon-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6a/SoAurora-Seasonal-Heart-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+			cost: { seasonalCandles: { cost: 3, season: seasonId } },
+		},
+	],
+] as const satisfies FriendshipTreeData;
+
+export const CURRENT = [
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 2,
+			cost: { hearts: 4 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { candles: 5 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b5/SoAurora-Warrior-of-Love-Mask-Icon-Morybel-0146.png/revision/latest/scale-to-width-down/51?cb=20230802004908",
+			cost: { candles: 35 },
+		},
+		{
+			icon: HEART_URL,
+			cost: { candles: 3 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/31/Winglight.png/revision/latest/scale-to-width-down/51?cb=20230719193544",
+			cost: { ascendedCandles: 2 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 3,
+			cost: { hearts: 3 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/61/SoAurora-Warrior-of-Love-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 4,
+			cost: { hearts: 6 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6b/SoAurora-Warrior-of-Love-Hair-Icon-Morybel-0146.png/revision/latest/scale-to-width-down/51?cb=20230716154247",
+			cost: { candles: 40 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { candles: 5 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4e/MusicSheet.png/revision/latest",
+			cost: { candles: 15 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a9/SoAurora-Warrior-of-Love-Cape-Icon-Morybel-0146.png/revision/latest",
+			cost: { candles: 75 },
+		},
+	],
+] as const satisfies FriendshipTreeData;
