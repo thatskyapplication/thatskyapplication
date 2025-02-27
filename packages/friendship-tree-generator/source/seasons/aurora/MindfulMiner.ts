f9c3e7b9f80c26dae62c5532acbf9bf420071bb2
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-02-27 08:52:25 +0000

    feat(MindfulMiner): Add friendship tree

--- packages/friendship-tree-generator/source/seasons/aurora/MindfulMiner.ts
+++ packages/friendship-tree-generator/source/seasons/aurora/MindfulMiner.ts
@@ -1,5 +1,6 @@
 import { SeasonId } from "@thatskyapplication/utility";
 import type { FriendshipTreeData } from "../../nodes.js";
+import { HEART_URL } from "../../constants.js";
 
 const seasonId = SeasonId.AURORA;
 
@@ -71,6 +72,71 @@ export const SEASONAL = [
 			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6a/SoAurora-Seasonal-Heart-Morybel-0146.png/revision/latest",
 			seasonIcon: seasonId,
 			cost: { seasonalCandles: { cost: 3, season: seasonId } },
+		},
+	],
+] as const satisfies FriendshipTreeData;
+
+export const CURRENT = [
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c1/SoAurora-Mindful-Miner-Emote-Icon-Morybel-0146.png/revision/latest",
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c1/SoAurora-Mindful-Miner-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 2,
+			cost: { hearts: 4 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { candles: 5 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/bc/SoAurora-Mindful-Miner-Mask-Icon-Morybel-0146.png/revision/latest",
+			cost: { candles: 35 },
+		},
+		{
+			icon: HEART_URL,
+			cost: { candles: 3 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/31/Winglight.png/revision/latest/scale-to-width-down/51?cb=20230719193544",
+			cost: { ascendedCandles: 2 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c1/SoAurora-Mindful-Miner-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 3,
+			cost: { hearts: 3 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c1/SoAurora-Mindful-Miner-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 4,
+			cost: { hearts: 6 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6b/SoAurora-Warrior-of-Love-Hair-Icon-Morybel-0146.png/revision/latest/scale-to-width-down/51?cb=20230716154247",
+			cost: { candles: 40 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { candles: 5 },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/73/SoAurora-Mindful-Miner-Outfit-Icon-Morybel-0146.png/revision/latest",
+			cost: { candles: 55 },
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/66/SoAurora-Mindful-Miner-Cape-Icon-Morybel-0146.png/revision/latest",
+			cost: { candles: 75 },
 		},
 	],
 ] as const satisfies FriendshipTreeData;
