3cf4327ec8d0f9a9c01656211d24c7b57e560b11
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-02-27 08:51:44 +0000

    feat(constants): Increase maximum winged light

--- packages/utility/source/constants.ts
+++ packages/utility/source/constants.ts
@@ -1,3 +1,3 @@
 export const MINIMUM_WINGED_LIGHT = 0 as const;
-export const MAXIMUM_WINGED_LIGHT = 244 as const;
+export const MAXIMUM_WINGED_LIGHT = 245 as const;
 export const WIKI_URL = "https://sky-children-of-the-light.fandom.com" as const;
