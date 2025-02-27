796e9daca5494c9d2f364f96a51b366df4edbe6b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-02-27 01:37:16 +0000

    feat(SeedOfHope): Add spirit

---
+++ packages/friendship-tree-generator/source/seasons/aurora/SeedOfHope.ts
@@ -0,0 +1,76 @@
+import { SeasonId } from "@thatskyapplication/utility";
+import type { FriendshipTreeData } from "../../nodes.js";
+
+const seasonId = SeasonId.AURORA;
+
+export const SEASONAL = [
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f1/SoAurora-Seed-of-Hope-Emote-Icon-Morybel-0146.png/revision/latest",
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f1/SoAurora-Seed-of-Hope-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 2,
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { seasonalCandles: { cost: 8, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/76/SoAurora-Seed-of-Hope-Mask-Icon-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4e/MusicSheet.png/revision/latest",
+			cost: { seasonalCandles: { cost: 12, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f1/SoAurora-Seed-of-Hope-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 3,
+			cost: { seasonalCandles: { cost: 16, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f1/SoAurora-Seed-of-Hope-Emote-Icon-Morybel-0146.png/revision/latest",
+			level: 4,
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c2/SoAurora-Seed-of-Hope-Hair-Icon-Morybel-0146.png/revision/latest",
+			cost: { seasonalCandles: { cost: 20, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { seasonalCandles: { cost: 26, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2a/SoAurora-Seed-of-Hope-Cape-Icon-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6a/SoAurora-Seasonal-Heart-Morybel-0146.png/revision/latest",
+			seasonIcon: seasonId,
+			cost: { seasonalCandles: { cost: 3, season: seasonId } },
+		},
+	],
+] as const satisfies FriendshipTreeData;
