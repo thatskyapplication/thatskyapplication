ba451a571b9aa78c82398722612247ba9a4f82f9
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-02-28 21:17:55 +0000

    fix(configuration): Remove unused environment variable

--- apps/notifications/source/utility/configuration.ts
+++ apps/notifications/source/utility/configuration.ts
@@ -5,7 +5,7 @@ export const PRODUCTION = process.env.NODE_ENV === "production";
 
 // Environment variables.
 if (
-	!(process.env.DISCORD_TOKEN && process.env.DATABASE_URL && process.env.WIND_PATHS_URL) ||
+	!(process.env.DISCORD_TOKEN && process.env.DATABASE_URL) ||
 	(PRODUCTION && !(process.env.BETTER_STACK_TOKEN && process.env.BETTER_STACK_ENDPOINT_URL))
 ) {
 	throw new Error("Missing environment variables.");
@@ -13,6 +13,5 @@ if (
 
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 export const DATABASE_URL = process.env.DATABASE_URL;
-export const WIND_PATHS_URL = process.env.WIND_PATHS_URL;
 export const BETTER_STACK_TOKEN = process.env.BETTER_STACK_TOKEN;
 export const BETTER_STACK_ENDPOINT_URL = process.env.BETTER_STACK_ENDPOINT_URL;
