06f961f1eeebc232300d0f07796e3be36cc1a52e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-03-08 21:04:19 +0000

    feat(play-fight): Migrate

--- apps/caelus/source/services/play-fight.ts
+++ apps/caelus/source/services/play-fight.ts
@@ -2,6 +2,7 @@ import {
 	type APIChatInputApplicationCommandInteraction,
 	type APIInteractionDataResolvedGuildMember,
 	type APIUser,
+	ComponentType,
 	Locale,
 	MessageFlags,
 	PermissionFlagsBits,
@@ -75,19 +76,33 @@ export async function playFight(
 
 	await client.api.interactions.reply(interaction.id, interaction.token, {
 		allowed_mentions: { users: [user.id] },
-		content: `<@${invoker.id}> is fighting <@${user.id}>!`,
-		embeds: [
+		components: [
 			{
-				color: DEFAULT_EMBED_COLOUR,
-				image: {
-					url: String(
-						new URL(
-							`play_fights/${Math.floor(Math.random() * MAXIMUM_PLAY_FIGHT_GIF + 1)}.gif`,
-							CDN_URL,
-						),
-					),
-				},
+				type: ComponentType.Container,
+				accent_color: DEFAULT_EMBED_COLOUR,
+				components: [
+					{
+						type: ComponentType.TextDisplay,
+						content: `<@${invoker.id}> is fighting <@${user.id}>!`,
+					},
+					{
+						type: ComponentType.MediaGallery,
+						items: [
+							{
+								media: {
+									url: String(
+										new URL(
+											`play_fights/${Math.floor(Math.random() * MAXIMUM_PLAY_FIGHT_GIF + 1)}.gif`,
+											CDN_URL,
+										),
+									),
+								},
+							},
+						],
+					},
+				],
 			},
 		],
+		flags: MessageFlags.IsComponentsV2,
 	});
 }
