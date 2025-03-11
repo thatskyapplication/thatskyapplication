af860eb0da547e480ea38c943da0fa6294e8ee08
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-03-11 22:14:18 +0000

    refactor: Move event imports to where they are used

--- apps/caelus/source/events/index.ts
+++ apps/caelus/source/events/index.ts
@@ -1,58 +1,7 @@
 import type { MappedEvents } from "@discordjs/core";
-import channelCreate from "./channel-create.js";
-import channelDelete from "./channel-delete.js";
-import channelUpdate from "./channel-update.js";
-import entitlementDelete from "./entitlement-delete.js";
-import entitlementUpdate from "./entitlement-update.js";
-import guildCreate from "./guild-create.js";
-import guildDelete from "./guild-delete.js";
-import guildMemberAdd from "./guild-member-add.js";
-import guildMemberRemove from "./guild-member-remove.js";
-import guildMemberUpdate from "./guild-member-update.js";
-import guildRoleCreate from "./guild-role-create.js";
-import guildRoleDelete from "./guild-role-delete.js";
-import guildRoleUpdate from "./guild-role-update.js";
-import guildStickersUpdate from "./guild-stickers-update.js";
-import guildUpdate from "./guild-update.js";
-import interactionCreate from "./interaction-create.js";
-import messageCreate from "./message-create.js";
-import messageDelete from "./message-delete.js";
-import messageUpdate from "./message-update.js";
-import ready from "./ready.js";
-import threadCreate from "./thread-create.js";
-import threadDelete from "./thread-delete.js";
-import threadListSync from "./thread-list-sync.js";
-import threadUpdate from "./thread-update.js";
 
 export interface Event<T extends keyof MappedEvents = keyof MappedEvents> {
 	name: T;
 	once?: boolean;
 	fire(this: void, ...parameters: MappedEvents[T]): Promise<void> | void;
 }
-
-export default [
-	channelCreate,
-	channelDelete,
-	channelUpdate,
-	entitlementDelete,
-	entitlementUpdate,
-	guildCreate,
-	guildDelete,
-	guildMemberAdd,
-	guildMemberRemove,
-	guildMemberUpdate,
-	guildRoleCreate,
-	guildRoleDelete,
-	guildRoleUpdate,
-	guildStickersUpdate,
-	guildUpdate,
-	interactionCreate,
-	messageCreate,
-	messageDelete,
-	messageUpdate,
-	ready,
-	threadCreate,
-	threadDelete,
-	threadListSync,
-	threadUpdate,
-] as const;
--- apps/caelus/source/index.ts
+++ apps/caelus/source/index.ts
@@ -3,11 +3,60 @@ import { Locale } from "@discordjs/core";
 import { t } from "i18next";
 import { COMMAND_CACHE } from "./caches/commands.js";
 import { client, gateway } from "./discord.js";
-import events, { type Event } from "./events/index.js";
+import channelCreate from "./events/channel-create.js";
+import channelDelete from "./events/channel-delete.js";
+import channelUpdate from "./events/channel-update.js";
+import entitlementDelete from "./events/entitlement-delete.js";
+import entitlementUpdate from "./events/entitlement-update.js";
+import guildCreate from "./events/guild-create.js";
+import guildDelete from "./events/guild-delete.js";
+import guildMemberAdd from "./events/guild-member-add.js";
+import guildMemberRemove from "./events/guild-member-remove.js";
+import guildMemberUpdate from "./events/guild-member-update.js";
+import guildRoleCreate from "./events/guild-role-create.js";
+import guildRoleDelete from "./events/guild-role-delete.js";
+import guildRoleUpdate from "./events/guild-role-update.js";
+import guildStickersUpdate from "./events/guild-stickers-update.js";
+import guildUpdate from "./events/guild-update.js";
+import type { Event } from "./events/index.js";
+import interactionCreate from "./events/interaction-create.js";
+import messageCreate from "./events/message-create.js";
+import messageDelete from "./events/message-delete.js";
+import messageUpdate from "./events/message-update.js";
+import ready from "./events/ready.js";
+import threadCreate from "./events/thread-create.js";
+import threadDelete from "./events/thread-delete.js";
+import threadListSync from "./events/thread-list-sync.js";
+import threadUpdate from "./events/thread-update.js";
 import pino from "./pino.js";
 import { APPLICATION_ID } from "./utility/constants.js";
 
-for (const event of events) {
+for (const event of [
+	channelCreate,
+	channelDelete,
+	channelUpdate,
+	entitlementDelete,
+	entitlementUpdate,
+	guildCreate,
+	guildDelete,
+	guildMemberAdd,
+	guildMemberRemove,
+	guildMemberUpdate,
+	guildRoleCreate,
+	guildRoleDelete,
+	guildRoleUpdate,
+	guildStickersUpdate,
+	guildUpdate,
+	interactionCreate,
+	messageCreate,
+	messageDelete,
+	messageUpdate,
+	ready,
+	threadCreate,
+	threadDelete,
+	threadListSync,
+	threadUpdate,
+]) {
 	const { name, once, fire }: Event = event;
 	client[once ? "once" : "on"](name, fire);
 }
