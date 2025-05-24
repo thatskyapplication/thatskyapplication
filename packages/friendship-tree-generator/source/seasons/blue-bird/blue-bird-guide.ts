f82348d182219674f1f36a0088394106a6415383
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-05-24 22:11:21 +0100

    fix(blue-bird-guide): Remove scaling

--- packages/friendship-tree-generator/source/seasons/blue-bird/blue-bird-guide.ts
+++ packages/friendship-tree-generator/source/seasons/blue-bird/blue-bird-guide.ts
@@ -13,16 +13,16 @@ export const CURRENT = [
 			icon: HEART_URL,
 		},
 		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/63/Season-of-Blue-Bird-icon.png/revision/latest/scale-to-width-down/51?cb=20250228214357",
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/63/Season-of-Blue-Bird-icon.png/revision/latest",
 			seasonIcon: seasonId,
 			nodes: [
 				{
-					icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/da/Blue-Bird-Guide-Ultimate-Facepiece-icon.png/revision/latest/scale-to-width-down/51?cb=20250421204129",
+					icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/da/Blue-Bird-Guide-Ultimate-Facepiece-icon.png/revision/latest",
 					seasonIcon: seasonId,
 					cost: { seasonalHearts: { cost: 2, season: seasonId } },
 				},
 				{
-					icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3e/Blue-Bird-Guide-Ultimate-Cape-icon.png/revision/latest/scale-to-width-down/51?cb=20250422001410",
+					icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3e/Blue-Bird-Guide-Ultimate-Cape-icon.png/revision/latest",
 					seasonIcon: seasonId,
 					cost: { seasonalHearts: { cost: 3, season: seasonId } },
 				},
@@ -31,10 +31,10 @@ export const CURRENT = [
 	],
 	[
 		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest/scale-to-width-down/51?cb=20250316000859",
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest",
 		},
 		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest/scale-to-width-down/51?cb=20250316000859",
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest",
 			level: 2,
 			cost: { hearts: 3 },
 		},
@@ -49,11 +49,11 @@ export const CURRENT = [
 	],
 	[
 		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest/scale-to-width-down/51?cb=20250316000859",
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest",
 			level: 3,
 		},
 		{
-			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest/scale-to-width-down/51?cb=20250316000859",
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2e/Blue-Bird-Guide-ultimate-expression.png/revision/latest",
 			level: 4,
 			cost: { hearts: 5 },
 		},
