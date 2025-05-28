5c29203925ae7ae0b55069f1bd6f091881fc4a7d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-05-28 22:31:26 +0100

    fix(aurora-homecoming): Remove infographic URL

--- packages/utility/source/events/2025/aurora-homecoming.ts
+++ packages/utility/source/events/2025/aurora-homecoming.ts
@@ -7,6 +7,5 @@ export default new Event({
 	id: EventId.AURORAHomecoming2025,
 	start: skyDate(2_025, 6, 9),
 	end: skyDate(2_025, 6, 23),
-	offerInfographicURL: true,
 	patchNotesURL: String(new URL("p0295", LINK_REDIRECTOR_URL)),
 });
