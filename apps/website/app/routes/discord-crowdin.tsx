e0f13aafffab51156dde0b2f5feeb90085ddaf74
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-01 22:35:59 +0100

    fix(discord-crowdin): Padding

--- apps/website/app/routes/discord-crowdin.tsx
+++ apps/website/app/routes/discord-crowdin.tsx
@@ -245,7 +245,7 @@ export default function CrowdinDiscord() {
 		useLoaderData<typeof loader>();
 
 	return (
-		<div className="min-h-screen flex items-center justify-center">
+		<div className="min-h-screen flex items-center justify-center pt-20 lg:pt-0 pb-4 lg:pb-0 px-4">
 			<TopBar />
 			<div className="bg-gray-100 dark:bg-gray-900 border border-gray-200 dark:border-gray-700 shadow-lg rounded-lg p-8 w-full max-w-md">
 				<h1 className="text-center mb-6">Crowdin & Discord Authorisation</h1>
