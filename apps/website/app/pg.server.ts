fa91b8e3d421304219d581dc26009d94a4573008
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-05 15:34:03 +0100

    refactor: Remove redundant check

--- apps/website/app/pg.server.ts
+++ apps/website/app/pg.server.ts
@@ -1,10 +1,6 @@
 import knex from "knex";
 import { DATABASE_URL } from "./config.server";
 
-if (!DATABASE_URL) {
-	throw new Error("Database URL missing.");
-}
-
 export default knex({
 	client: "pg",
 	connection: DATABASE_URL,
