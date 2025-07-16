43e31d8488d52ed58622222bf55250b013a3b0dc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-16 23:12:11 +0100

    feat(jolly-geologist): Add friendship tree

---
+++ packages/friendship-tree-generator/source/seasons/moments/jolly-geologist.ts
@@ -0,0 +1,66 @@
+import { SeasonId } from "@thatskyapplication/utility";
+import type { FriendshipTreeData } from "../../nodes.js";
+
+const seasonId = SeasonId.Moments;
+
+export const SEASONAL = [
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/78/Jolly-Geologist-Emote-icon-Credit-Morybel.png/revision/latest",
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/78/Jolly-Geologist-Emote-icon-Credit-Morybel.png/revision/latest",
+			seasonIcon: seasonId,
+			level: 2,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/81/Jolly-Geologist-Head-accessory-icon-Credit-Morybel.png/revision/latest",
+			cost: { seasonalCandles: { cost: 8, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/bf/Jolly-Geologist-Hair-icon-Credit-Morybel.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/78/Jolly-Geologist-Emote-icon-Credit-Morybel.png/revision/latest",
+			cost: { seasonalCandles: { cost: 20, season: seasonId } },
+			level: 3,
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/78/Jolly-Geologist-Emote-icon-Credit-Morybel.png/revision/latest",
+			seasonIcon: seasonId,
+			level: 4,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			cost: { seasonalCandles: { cost: 28, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Special-event-spell-icon.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4e/MusicSheet.png/revision/latest",
+			cost: { seasonalCandles: { cost: 34, season: seasonId } },
+		},
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/05/Jolly-Geologist-Prop-icon-Credit-Morybel.png/revision/latest",
+			seasonIcon: seasonId,
+		},
+	],
+	[
+		{
+			icon: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c7/SOMoments-seasonal-heart-icon-Credit-Morybel.png/revision/latest",
+			cost: { seasonalCandles: { cost: 3, season: seasonId } },
+			seasonIcon: seasonId,
+		},
+	],
+] as const satisfies FriendshipTreeData;
