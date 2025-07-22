482229e805681f90e2e70a5cd9ba1dec84b71b70
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-22 18:13:30 +0100

    refactor: Remove stuff

--- packages/friendship-tree-generator/source/constants.ts
+++ packages/friendship-tree-generator/source/constants.ts
@@ -1,3 +1,5 @@
+import { emojiConstants } from "@thatskyapplication/utility";
+
 export const LINE_WIDTH = 5 as const;
 export const LINE_COLOUR = "#FFFFFF" as const;
 export const IMAGE_SIZE = 100 as const;
@@ -18,33 +20,4 @@ export const LEVEL_OFFSET_Y = 2 as const;
 export const CURRENCY_TEXT_OFFSET = 10 as const;
 export const NEXT_HEIGHT_LEVEL = 240 as const;
 export const NEXT_HEIGHT_LEVEL_SIDES_OFFSET = 60 as const;
-
-export const HEART_URL =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/d9/Heart.png/revision/latest" as const;
-
-export const QUEST_URL =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/8b/Exclamation-mark-Ray.png/revision/latest" as const;
-
-export const DYE_RED =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6c/Red-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_YELLOW =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/ad/Yellow-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_GREEN =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c8/Green-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_CYAN =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f5/Cyan-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_BLUE =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/0b/Blue-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_PURPLE =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/34/Purple-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_BLACK =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/97/Black-dye-container-icon.png/revision/latest" as const;
-
-export const DYE_WHITE =
-	"https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/64/White-dye-container-icon.png/revision/latest" as const;
+export const { CosmeticToEmoji, SeasonIdToSeasonalEmoji } = emojiConstants(true);
