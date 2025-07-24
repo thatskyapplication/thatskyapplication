3ab6a23f174d9dc7b720c74e48f21bcae1ca3018
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-24 12:50:47 +0100

    feat: Migrate Season of AURORA

--- packages/utility/source/spirits/seasons/aurora/running-wayfarer.ts
+++ packages/utility/source/spirits/seasons/aurora/running-wayfarer.ts
@@ -13,87 +13,119 @@ export default new SeasonalSpirit({
 	realm: RealmName.ValleyOfTriumph,
 	offer: {
 		seasonal: [
-			{ cosmetic: Cosmetic.EmoteWavingLight1 },
-			{ cosmetic: Cosmetic.EmoteWavingLight2 },
-			{
-				cosmetic: Cosmetic.RunningWayfarerMask,
-				cost: { seasonalCandles: 12 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing1,
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing2,
-				cost: { seasonalCandles: 16 },
-			},
-			{ cosmetic: Cosmetic.RunningWayfarerHair },
-			{
-				cosmetic: Cosmetic.EmoteWavingLight3,
-				cost: { seasonalCandles: 20 },
-			},
-			{ cosmetic: Cosmetic.EmoteWavingLight4 },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing3,
-				cost: { seasonalCandles: 24 },
-			},
-			{ cosmetic: Cosmetic.RunningWayfarerMusicSheet },
-			{
-				cosmetic: Cosmetic.RunningWayfarerCape,
-				cost: { seasonalCandles: 30 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing4,
-			},
-			{
-				cosmetic: Cosmetic.RunningWayfarerSeasonalHeart,
-				cost: { seasonalCandles: 3 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteWavingLight1 },
+				{ cosmetic: Cosmetic.EmoteWavingLight2, level: 2, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.RunningWayfarerMask,
+					cost: { seasonalCandles: 12 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing1,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing2,
+					cost: { seasonalCandles: 16 },
+				},
+				{ cosmetic: Cosmetic.RunningWayfarerHair, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteWavingLight3,
+					cost: { seasonalCandles: 20 },
+					level: 3,
+				},
+				{ cosmetic: Cosmetic.EmoteWavingLight4, level: 4, seasonPass: true },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing3,
+					cost: { seasonalCandles: 24 },
+				},
+				{ cosmetic: Cosmetic.RunningWayfarerMusicSheet, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.RunningWayfarerCape,
+					cost: { seasonalCandles: 30 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing4,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.RunningWayfarerSeasonalHeart,
+					cost: { seasonalCandles: 3 },
+					seasonPass: true,
+				},
+			],
 		],
 		current: [
-			{ cosmetic: Cosmetic.EmoteWavingLight1 },
-			{ cosmetic: Cosmetic.EmoteWavingLight2, cost: { hearts: 4 } },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing1,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.RunningWayfarerMask,
-				cost: { candles: 35 },
-			},
-			{
-				cosmetic: Cosmetic.RunningWayfarerSeasonalHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.RunningWayfarerWingBuff,
-				cost: { ascendedCandles: 2 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteWavingLight3,
-				cost: { hearts: 3 },
-			},
-			{ cosmetic: Cosmetic.EmoteWavingLight4, cost: { hearts: 6 } },
-			{ cosmetic: Cosmetic.RunningWayfarerHair, cost: { candles: 40 } },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.RunningWayfarerBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				translation: CosmeticCommon.MusicSheet,
-				cosmetic: Cosmetic.RunningWayfarerMusicSheet,
-				cost: { candles: 15 },
-			},
-			{
-				cosmetic: Cosmetic.RunningWayfarerCape,
-				cost: { candles: 75 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteWavingLight1 },
+				{ cosmetic: Cosmetic.EmoteWavingLight2, cost: { hearts: 4 }, level: 2 },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing1,
+					cost: { candles: 5 },
+				},
+				{
+					cosmetic: Cosmetic.RunningWayfarerMask,
+					cost: { candles: 35 },
+				},
+				{
+					cosmetic: Cosmetic.RunningWayfarerSeasonalHeart,
+					cost: { candles: 3 },
+					regularHeart: true,
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.RunningWayfarerWingBuff,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteWavingLight3,
+					cost: { hearts: 3 },
+					level: 3,
+				},
+				{ cosmetic: Cosmetic.EmoteWavingLight4, cost: { hearts: 6 }, level: 4 },
+				{ cosmetic: Cosmetic.RunningWayfarerHair, cost: { candles: 40 } },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.RunningWayfarerBlessing2,
+					cost: { candles: 5 },
+				},
+				{
+					translation: CosmeticCommon.MusicSheet,
+					cosmetic: Cosmetic.RunningWayfarerMusicSheet,
+					cost: { candles: 15 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.RunningWayfarerCape,
+					cost: { candles: 75 },
+				},
+			],
 		],
 	},
 	visits: {
--- packages/utility/source/spirits/seasons/aurora/seed-of-hope.ts
+++ packages/utility/source/spirits/seasons/aurora/seed-of-hope.ts
@@ -13,85 +13,115 @@ export default new SeasonalSpirit({
 	realm: RealmName.ValleyOfTriumph,
 	offer: {
 		seasonal: [
-			{ cosmetic: Cosmetic.EmoteRhythmicClap1 },
-			{ cosmetic: Cosmetic.EmoteRhythmicClap2 },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing1,
-				cost: { seasonalCandles: 8 },
-			},
-			{ cosmetic: Cosmetic.SeedOfHopeMask },
-			{
-				translation: CosmeticCommon.MusicSheet,
-				cosmetic: Cosmetic.SeedOfHopeMusicSheet,
-				cost: { seasonalCandles: 12 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing2,
-			},
-			{
-				cosmetic: Cosmetic.EmoteRhythmicClap3,
-				cost: { seasonalCandles: 16 },
-			},
-			{ cosmetic: Cosmetic.EmoteRhythmicClap4 },
-			{
-				cosmetic: Cosmetic.SeedOfHopeHair,
-				cost: { seasonalCandles: 20 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing3,
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing4,
-				cost: { seasonalCandles: 26 },
-			},
-			{ cosmetic: Cosmetic.SeedOfHopeCape },
-			{
-				cosmetic: Cosmetic.SeedOfHopeSeasonalHeart,
-				cost: { seasonalCandles: 3 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteRhythmicClap1 },
+				{ cosmetic: Cosmetic.EmoteRhythmicClap2, level: 2, seasonPass: true },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing1,
+					cost: { seasonalCandles: 8 },
+				},
+				{ cosmetic: Cosmetic.SeedOfHopeMask, seasonPass: true },
+			],
+			[
+				{
+					translation: CosmeticCommon.MusicSheet,
+					cosmetic: Cosmetic.SeedOfHopeMusicSheet,
+					cost: { seasonalCandles: 12 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing2,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteRhythmicClap3,
+					cost: { seasonalCandles: 16 },
+					level: 3,
+				},
+				{ cosmetic: Cosmetic.EmoteRhythmicClap4, level: 4, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.SeedOfHopeHair,
+					cost: { seasonalCandles: 20 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing3,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing4,
+					cost: { seasonalCandles: 26 },
+				},
+				{ cosmetic: Cosmetic.SeedOfHopeCape, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.SeedOfHopeSeasonalHeart,
+					cost: { seasonalCandles: 3 },
+					seasonPass: true,
+				},
+			],
 		],
 		current: [
-			{ cosmetic: Cosmetic.EmoteRhythmicClap1 },
-			{ cosmetic: Cosmetic.EmoteRhythmicClap2, cost: { hearts: 4 } },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing1,
-				cost: { candles: 5 },
-			},
-			{ cosmetic: Cosmetic.SeedOfHopeMask, cost: { candles: 35 } },
-			{
-				cosmetic: Cosmetic.SeedOfHopeSeasonalHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.SeedOfHopeWingBuff,
-				cost: { ascendedCandles: 2 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteRhythmicClap3,
-				cost: { hearts: 3 },
-			},
-			{ cosmetic: Cosmetic.EmoteRhythmicClap4, cost: { hearts: 6 } },
-			{
-				cosmetic: Cosmetic.SeedOfHopeHair,
-				cost: { candles: 40 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.SeedOfHopeBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				translation: CosmeticCommon.MusicSheet,
-				cosmetic: Cosmetic.SeedOfHopeMusicSheet,
-				cost: { candles: 15 },
-			},
-			{ cosmetic: Cosmetic.SeedOfHopeCape, cost: { candles: 75 } },
+			[
+				{ cosmetic: Cosmetic.EmoteRhythmicClap1 },
+				{ cosmetic: Cosmetic.EmoteRhythmicClap2, cost: { hearts: 4 } },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing1,
+					cost: { candles: 5 },
+				},
+				{ cosmetic: Cosmetic.SeedOfHopeMask, cost: { candles: 35 } },
+				{
+					cosmetic: Cosmetic.SeedOfHopeSeasonalHeart,
+					cost: { candles: 3 },
+					regularHeart: true,
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.SeedOfHopeWingBuff,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteRhythmicClap3,
+					cost: { hearts: 3 },
+					level: 3,
+				},
+				{ cosmetic: Cosmetic.EmoteRhythmicClap4, cost: { hearts: 6 }, level: 4 },
+				{
+					cosmetic: Cosmetic.SeedOfHopeHair,
+					cost: { candles: 40 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.SeedOfHopeBlessing2,
+					cost: { candles: 5 },
+				},
+				{
+					translation: CosmeticCommon.MusicSheet,
+					cosmetic: Cosmetic.SeedOfHopeMusicSheet,
+					cost: { candles: 15 },
+				},
+			],
+			[{ cosmetic: Cosmetic.SeedOfHopeCape, cost: { candles: 75 } }],
 		],
 	},
 	visits: {
