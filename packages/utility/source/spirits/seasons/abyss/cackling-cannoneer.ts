8f28f5518a479fcc91438087b4f08fafff403b61
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-24 18:19:47 +0100

    feat: Migrate Season of Abyss

--- packages/utility/source/spirits/seasons/abyss/cackling-cannoneer.ts
+++ packages/utility/source/spirits/seasons/abyss/cackling-cannoneer.ts
@@ -13,96 +13,130 @@ export default new SeasonalSpirit({
 	realm: RealmName.GoldenWasteland,
 	offer: {
 		seasonal: [
-			{ cosmetic: Cosmetic.EmoteEvilLaugh1 },
-			{ cosmetic: Cosmetic.EmoteEvilLaugh2 },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing1,
-				cost: { seasonalCandles: 12 },
-			},
-			{ cosmetic: Cosmetic.CacklingCannoneerMusicSheet },
-			{
-				cosmetic: Cosmetic.CacklingCannoneerMask,
-				cost: { seasonalCandles: 16 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing2,
-			},
-			{
-				cosmetic: Cosmetic.EmoteEvilLaugh3,
-				cost: { seasonalCandles: 20 },
-			},
-			{ cosmetic: Cosmetic.EmoteEvilLaugh4 },
-			{
-				cosmetic: Cosmetic.CacklingCannoneerCape,
-				cost: { seasonalCandles: 26 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing3,
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing4,
-				cost: { seasonalCandles: 34 },
-			},
-			{ cosmetic: Cosmetic.CacklingCannoneerHair },
-			{
-				cosmetic: Cosmetic.CacklingCannoneerSeasonalHeart,
-				cost: { seasonalCandles: 3 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteEvilLaugh1 },
+				{ cosmetic: Cosmetic.EmoteEvilLaugh2, level: 2, seasonPass: true },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing1,
+					cost: { seasonalCandles: 12 },
+				},
+				{ cosmetic: Cosmetic.CacklingCannoneerMusicSheet, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.CacklingCannoneerMask,
+					cost: { seasonalCandles: 16 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing2,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteEvilLaugh3,
+					cost: { seasonalCandles: 20 },
+					level: 3,
+				},
+				{ cosmetic: Cosmetic.EmoteEvilLaugh4, level: 4, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.CacklingCannoneerCape,
+					cost: { seasonalCandles: 26 },
+				},
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 3 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing3,
+					seasonPass: true,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 4 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing4,
+					cost: { seasonalCandles: 34 },
+				},
+				{ cosmetic: Cosmetic.CacklingCannoneerHair, seasonPass: true },
+			],
+			[
+				{
+					cosmetic: Cosmetic.CacklingCannoneerSeasonalHeart,
+					cost: { seasonalCandles: 3 },
+					seasonPass: true,
+				},
+			],
 		],
 		current: [
-			{ cosmetic: Cosmetic.EmoteEvilLaugh1 },
-			{
-				cosmetic: Cosmetic.EmoteEvilLaugh2,
-				cost: { hearts: 4 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing1,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.CacklingCannoneerMask,
-				cost: { candles: 40 },
-			},
-			{
-				cosmetic: Cosmetic.CacklingCannoneerSeasonalHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.CacklingCannoneerWingBuff,
-				cost: { ascendedCandles: 2 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteEvilLaugh3,
-				cost: { hearts: 3 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteEvilLaugh4,
-				cost: { hearts: 6 },
-			},
-			{
-				cosmetic: Cosmetic.CacklingCannoneerHair,
-				cost: { candles: 50 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.CacklingCannoneerBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.CacklingCannoneerCape,
-				cost: { candles: 70 },
-			},
-			{
-				translation: CosmeticCommon.MusicSheet,
-				cosmetic: Cosmetic.CacklingCannoneerMusicSheet,
-				cost: { candles: 15 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteEvilLaugh1 },
+				{
+					cosmetic: Cosmetic.EmoteEvilLaugh2,
+					cost: { hearts: 4 },
+					level: 2,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing1,
+					cost: { candles: 5 },
+				},
+				{
+					cosmetic: Cosmetic.CacklingCannoneerMask,
+					cost: { candles: 40 },
+				},
+				{
+					cosmetic: Cosmetic.CacklingCannoneerSeasonalHeart,
+					cost: { candles: 3 },
+					regularHeart: true,
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.CacklingCannoneerWingBuff,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteEvilLaugh3,
+					cost: { hearts: 3 },
+					level: 3,
+				},
+				{
+					cosmetic: Cosmetic.EmoteEvilLaugh4,
+					cost: { hearts: 6 },
+					level: 4,
+				},
+				{
+					cosmetic: Cosmetic.CacklingCannoneerHair,
+					cost: { candles: 50 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.CacklingCannoneerBlessing2,
+					cost: { candles: 5 },
+				},
+				{
+					cosmetic: Cosmetic.CacklingCannoneerCape,
+					cost: { candles: 70 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.MusicSheet,
+					cosmetic: Cosmetic.CacklingCannoneerMusicSheet,
+					cost: { candles: 15 },
+				},
+			],
 		],
 	},
 	visits: {
