4e5ef7cfa404661adac32a610902e4ef0589885b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-25 17:05:39 +0100

    feat: Migrate Season of Flight

--- packages/utility/source/spirits/seasons/flight/flight-guide.ts
+++ packages/utility/source/spirits/seasons/flight/flight-guide.ts
@@ -11,66 +11,87 @@ export default new GuideSpirit({
 	offer: {
 		hasInfographic: false,
 		current: [
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 1 },
-				cosmetic: Cosmetic.FlightGuideQuest1,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 1 },
-				cosmetic: Cosmetic.FlightGuideHeart1,
-				cost: { candles: 3 },
-			},
-			{ cosmetic: Cosmetic.FlightPendant },
-			{
-				cosmetic: Cosmetic.FlightGuideUltimateHairAccessory,
-				cost: { seasonalHearts: 2 },
-			},
-			{
-				cosmetic: Cosmetic.FlightGuideUltimateOutfit,
-				cost: { seasonalHearts: 2 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 2 },
-				cosmetic: Cosmetic.FlightGuideQuest2,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 2 },
-				cosmetic: Cosmetic.FlightGuideHeart2,
-				cost: { candles: 3 },
-			},
-			{
-				cosmetic: Cosmetic.FlightGuideHighFive,
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 3 },
-				cosmetic: Cosmetic.FlightGuideQuest3,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 3 },
-				cosmetic: Cosmetic.FlightGuideHeart3,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 4 },
-				cosmetic: Cosmetic.FlightGuideQuest4,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 4 },
-				cosmetic: Cosmetic.FlightGuideHeart4,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 5 },
-				cosmetic: Cosmetic.FlightGuideQuest5,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 5 },
-				cosmetic: Cosmetic.FlightGuideHeart5,
-				cost: { candles: 3 },
-			},
-			{
-				cosmetic: Cosmetic.FlightGuideFriendActionHug,
-			},
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 1 },
+					cosmetic: Cosmetic.FlightGuideQuest1,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 1 },
+					cosmetic: Cosmetic.FlightGuideHeart1,
+					cost: { candles: 3 },
+				},
+				{
+					cosmetic: Cosmetic.FlightPendant,
+					seasonPass: true,
+					children: [
+						{
+							cosmetic: Cosmetic.FlightGuideUltimateHairAccessory,
+							cost: { seasonalHearts: 2 },
+							seasonPass: true,
+						},
+						{
+							cosmetic: Cosmetic.FlightGuideUltimateOutfit,
+							cost: { seasonalHearts: 2 },
+							seasonPass: true,
+						},
+					],
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 2 },
+					cosmetic: Cosmetic.FlightGuideQuest2,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 2 },
+					cosmetic: Cosmetic.FlightGuideHeart2,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.FlightGuideHighFive,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 3 },
+					cosmetic: Cosmetic.FlightGuideQuest3,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 3 },
+					cosmetic: Cosmetic.FlightGuideHeart3,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 4 },
+					cosmetic: Cosmetic.FlightGuideQuest4,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 4 },
+					cosmetic: Cosmetic.FlightGuideHeart4,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 5 },
+					cosmetic: Cosmetic.FlightGuideQuest5,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 5 },
+					cosmetic: Cosmetic.FlightGuideHeart5,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.FlightGuideFriendActionHug,
+				},
+			],
 		],
 	},
 });
