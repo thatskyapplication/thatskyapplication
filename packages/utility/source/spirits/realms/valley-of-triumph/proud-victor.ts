a847474a1255673914a3aa8564a9342b5bb25056
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-26 20:17:05 +0100

    fix: Migrate missed Valley of Triumph spirits

--- packages/utility/source/spirits/realms/valley-of-triumph/proud-victor.ts
+++ packages/utility/source/spirits/realms/valley-of-triumph/proud-victor.ts
@@ -11,44 +11,58 @@ export default new StandardSpirit({
 	realm: RealmName.ValleyOfTriumph,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.StanceProud },
-			{
-				cosmetic: Cosmetic.ProudVictorCape1,
-				cost: { hearts: 10 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.ProudVictorBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.ProudVictorHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.WingBuffMultiple, number: 1 },
-				cosmetic: Cosmetic.ProudVictorWingBuff1,
-				cost: { ascendedCandles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.ProudVictorBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.ProudVictorMask,
-				cost: { hearts: 30 },
-			},
-			{
-				translation: { key: CosmeticCommon.WingBuffMultiple, number: 2 },
-				cosmetic: Cosmetic.ProudVictorWingBuff2,
-				cost: { ascendedCandles: 9 },
-			},
-			{
-				cosmetic: Cosmetic.ProudVictorCape2,
-				cost: { hearts: 30 },
-			},
+			[
+				{ cosmetic: Cosmetic.StanceProud },
+				{
+					cosmetic: Cosmetic.ProudVictorCape1,
+					cost: { hearts: 10 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.ProudVictorBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.ProudVictorHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.WingBuffMultiple, number: 1 },
+					cosmetic: Cosmetic.ProudVictorWingBuff1,
+					cost: { ascendedCandles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.ProudVictorBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.ProudVictorMask,
+					cost: { hearts: 30 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.WingBuffMultiple, number: 2 },
+					cosmetic: Cosmetic.ProudVictorWingBuff2,
+					cost: { ascendedCandles: 9 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.ProudVictorCape2,
+					cost: { hearts: 30 },
+				},
+			],
 		],
 	},
 });
