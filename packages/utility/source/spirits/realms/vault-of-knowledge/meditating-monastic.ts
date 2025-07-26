9aa26e1328ec718ccfb7408e15d9ee9cb7c0bd91
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-26 17:49:44 +0100

    feat: Vault of Knowledge

--- packages/utility/source/spirits/realms/vault-of-knowledge/meditating-monastic.ts
+++ packages/utility/source/spirits/realms/vault-of-knowledge/meditating-monastic.ts
@@ -11,47 +11,62 @@ export default new StandardSpirit({
 	realm: RealmName.VaultOfKnowledge,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.EmoteFloat1 },
-			{
-				cosmetic: Cosmetic.EmoteFloat2,
-				cost: { candles: 10 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.MeditatingMonasticBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.MeditatingMonasticHair,
-				cost: { hearts: 10 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.MeditatingMonasticHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.MeditatingMonasticWingBuff,
-				cost: { ascendedCandles: 3 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteFloat3,
-				cost: { candles: 7 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteFloat4,
-				cost: { candles: 10 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.MeditatingMonasticBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.MeditatingMonasticChair,
-				cost: { hearts: 30 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteFloat1 },
+				{
+					cosmetic: Cosmetic.EmoteFloat2,
+					cost: { candles: 10 },
+					level: 2,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.MeditatingMonasticBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					cosmetic: Cosmetic.MeditatingMonasticHair,
+					cost: { hearts: 10 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.MeditatingMonasticHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.MeditatingMonasticWingBuff,
+					cost: { ascendedCandles: 3 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteFloat3,
+					cost: { candles: 7 },
+					level: 3,
+				},
+				{
+					cosmetic: Cosmetic.EmoteFloat4,
+					cost: { candles: 10 },
+					level: 4,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.MeditatingMonasticBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.MeditatingMonasticChair,
+					cost: { hearts: 30 },
+				},
+			],
 		],
 	},
 });
