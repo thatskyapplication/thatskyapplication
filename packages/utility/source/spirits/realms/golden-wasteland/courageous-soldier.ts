5c51917594b5854d1b251015b419cfb9c62d903f
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-26 17:19:15 +0100

    feat: Migrate Golden Wasteland

--- packages/utility/source/spirits/realms/golden-wasteland/courageous-soldier.ts
+++ packages/utility/source/spirits/realms/golden-wasteland/courageous-soldier.ts
@@ -11,44 +11,58 @@ export default new StandardSpirit({
 	realm: RealmName.GoldenWasteland,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.StanceCourageous },
-			{
-				cosmetic: Cosmetic.CourageousSoldierHair,
-				cost: { hearts: 4 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.CourageousSoldierBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.CourageousSoldierHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.WingBuffMultiple, number: 1 },
-				cosmetic: Cosmetic.CourageousSoldierWingBuff1,
-				cost: { ascendedCandles: 2 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.CourageousSoldierBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.CourageousSoldierCape1,
-				cost: { hearts: 15 },
-			},
-			{
-				translation: { key: CosmeticCommon.WingBuffMultiple, number: 2 },
-				cosmetic: Cosmetic.CourageousSoldierWingBuff2,
-				cost: { ascendedCandles: 6 },
-			},
-			{
-				cosmetic: Cosmetic.CourageousSoldierCape2,
-				cost: { hearts: 45 },
-			},
+			[
+				{ cosmetic: Cosmetic.StanceCourageous },
+				{
+					cosmetic: Cosmetic.CourageousSoldierHair,
+					cost: { hearts: 4 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.CourageousSoldierBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.CourageousSoldierHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.WingBuffMultiple, number: 1 },
+					cosmetic: Cosmetic.CourageousSoldierWingBuff1,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.CourageousSoldierBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.CourageousSoldierCape1,
+					cost: { hearts: 15 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.WingBuffMultiple, number: 2 },
+					cosmetic: Cosmetic.CourageousSoldierWingBuff2,
+					cost: { ascendedCandles: 6 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.CourageousSoldierCape2,
+					cost: { hearts: 45 },
+				},
+			],
 		],
 	},
 });
--- packages/utility/source/spirits/realms/golden-wasteland/frightened-refugee.ts
+++ packages/utility/source/spirits/realms/golden-wasteland/frightened-refugee.ts
@@ -11,47 +11,62 @@ export default new StandardSpirit({
 	realm: RealmName.GoldenWasteland,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.EmoteDuck1 },
-			{
-				cosmetic: Cosmetic.EmoteDuck2,
-				cost: { candles: 4 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.FrightenedRefugeeBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.FrightenedRefugeeHair,
-				cost: { hearts: 3 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.FrightenedRefugeeHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.FrightenedRefugeeWingBuff,
-				cost: { ascendedCandles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteDuck3,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteDuck4,
-				cost: { candles: 5 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.FrightenedRefugeeBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.FrightenedRefugeeContrabass,
-				cost: { hearts: 5 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteDuck1 },
+				{
+					cosmetic: Cosmetic.EmoteDuck2,
+					cost: { candles: 4 },
+					level: 2,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.FrightenedRefugeeBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					cosmetic: Cosmetic.FrightenedRefugeeHair,
+					cost: { hearts: 3 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.FrightenedRefugeeHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.FrightenedRefugeeWingBuff,
+					cost: { ascendedCandles: 1 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteDuck3,
+					cost: { candles: 5 },
+					level: 3,
+				},
+				{
+					cosmetic: Cosmetic.EmoteDuck4,
+					cost: { candles: 5 },
+					level: 4,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.FrightenedRefugeeBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.FrightenedRefugeeContrabass,
+					cost: { hearts: 5 },
+				},
+			],
 		],
 	},
 });
