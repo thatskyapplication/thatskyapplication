ad1cf67e2fc07bce861507747653a1f582676013
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-26 17:19:15 +0100

    feat: Migrate Golden Wasteland

--- packages/utility/source/spirits/realms/golden-wasteland/lookout-scout.ts
+++ packages/utility/source/spirits/realms/golden-wasteland/lookout-scout.ts
@@ -11,47 +11,62 @@ export default new StandardSpirit({
 	realm: RealmName.GoldenWasteland,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.EmoteLookAround1 },
-			{
-				cosmetic: Cosmetic.EmoteLookAround2,
-				cost: { candles: 5 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.LookoutScoutBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.LookoutScoutHorn,
-				cost: { hearts: 5 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.LookoutScoutHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.LookoutScoutWingBuff,
-				cost: { ascendedCandles: 2 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteLookAround3,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteLookAround4,
-				cost: { candles: 5 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.LookoutScoutBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.LookoutScoutFaceAccessory,
-				cost: { hearts: 10 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteLookAround1 },
+				{
+					cosmetic: Cosmetic.EmoteLookAround2,
+					cost: { candles: 5 },
+					level: 2,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.LookoutScoutBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					cosmetic: Cosmetic.LookoutScoutHorn,
+					cost: { hearts: 5 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.LookoutScoutHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.LookoutScoutWingBuff,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteLookAround3,
+					cost: { candles: 5 },
+					level: 3,
+				},
+				{
+					cosmetic: Cosmetic.EmoteLookAround4,
+					cost: { candles: 5 },
+					level: 4,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.LookoutScoutBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.LookoutScoutFaceAccessory,
+					cost: { hearts: 10 },
+				},
+			],
 		],
 	},
 });
--- packages/utility/source/spirits/realms/golden-wasteland/saluting-captain.ts
+++ packages/utility/source/spirits/realms/golden-wasteland/saluting-captain.ts
@@ -11,47 +11,62 @@ export default new StandardSpirit({
 	realm: RealmName.GoldenWasteland,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.EmoteSalute1 },
-			{
-				cosmetic: Cosmetic.EmoteSalute2,
-				cost: { candles: 4 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.SalutingCaptainBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.SalutingCaptainHair,
-				cost: { hearts: 5 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.SalutingCaptainHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.SalutingCaptainWingBuff,
-				cost: { ascendedCandles: 3 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteSalute3,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.EmoteSalute4,
-				cost: { candles: 5 },
-			},
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.SalutingCaptainBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.SalutingCaptainFireworksStaff,
-				cost: { hearts: 20 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteSalute1 },
+				{
+					cosmetic: Cosmetic.EmoteSalute2,
+					cost: { candles: 4 },
+					level: 2,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.SalutingCaptainBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					cosmetic: Cosmetic.SalutingCaptainHair,
+					cost: { hearts: 5 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.SalutingCaptainHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.SalutingCaptainWingBuff,
+					cost: { ascendedCandles: 3 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.EmoteSalute3,
+					cost: { candles: 5 },
+					level: 3,
+				},
+				{
+					cosmetic: Cosmetic.EmoteSalute4,
+					cost: { candles: 5 },
+					level: 4,
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.SalutingCaptainBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.SalutingCaptainFireworksStaff,
+					cost: { hearts: 20 },
+				},
+			],
 		],
 	},
 });
