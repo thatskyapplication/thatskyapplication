2f9667f8c7bebf0a74b7d7734fada5dcf86bf9a7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-26 17:02:56 +0100

    feat: Migrate Valley of Triumph

--- packages/utility/source/spirits/realms/valley-of-triumph/bowing-medalist.ts
+++ packages/utility/source/spirits/realms/valley-of-triumph/bowing-medalist.ts
@@ -11,38 +11,50 @@ export default new StandardSpirit({
 	realm: RealmName.ValleyOfTriumph,
 	offer: {
 		current: [
-			{ cosmetic: Cosmetic.EmoteBow1 },
-			{ cosmetic: Cosmetic.EmoteBow2, cost: { candles: 3 } },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
-				cosmetic: Cosmetic.BowingMedalistBlessing1,
-				cost: { candles: 1 },
-			},
-			{
-				cosmetic: Cosmetic.BowingMedalistHair,
-				cost: { hearts: 5 },
-			},
-			{
-				translation: CosmeticCommon.Heart,
-				cosmetic: Cosmetic.BowingMedalistHeart,
-				cost: { candles: 3 },
-			},
-			{
-				translation: CosmeticCommon.WingBuff,
-				cosmetic: Cosmetic.BowingMedalistWingBuff,
-				cost: { ascendedCandles: 2 },
-			},
-			{ cosmetic: Cosmetic.EmoteBow3, cost: { candles: 4 } },
-			{ cosmetic: Cosmetic.EmoteBow4, cost: { candles: 4 } },
-			{
-				translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
-				cosmetic: Cosmetic.BowingMedalistBlessing2,
-				cost: { candles: 5 },
-			},
-			{
-				cosmetic: Cosmetic.BowingMedalistFaceAccessory,
-				cost: { hearts: 5 },
-			},
+			[
+				{ cosmetic: Cosmetic.EmoteBow1 },
+				{ cosmetic: Cosmetic.EmoteBow2, cost: { candles: 3 }, level: 2 },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 1 },
+					cosmetic: Cosmetic.BowingMedalistBlessing1,
+					cost: { candles: 1 },
+				},
+				{
+					cosmetic: Cosmetic.BowingMedalistHair,
+					cost: { hearts: 5 },
+				},
+				{
+					translation: CosmeticCommon.Heart,
+					cosmetic: Cosmetic.BowingMedalistHeart,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: CosmeticCommon.WingBuff,
+					cosmetic: Cosmetic.BowingMedalistWingBuff,
+					cost: { ascendedCandles: 2 },
+				},
+			],
+			[
+				{ cosmetic: Cosmetic.EmoteBow3, cost: { candles: 4 }, level: 3 },
+				{ cosmetic: Cosmetic.EmoteBow4, cost: { candles: 4 }, level: 4 },
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.BlessingMultiple, number: 2 },
+					cosmetic: Cosmetic.BowingMedalistBlessing2,
+					cost: { candles: 5 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.BowingMedalistFaceAccessory,
+					cost: { hearts: 5 },
+				},
+			],
 		],
 	},
 });
