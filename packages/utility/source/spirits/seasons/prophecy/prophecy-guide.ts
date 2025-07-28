f0b0161fb9fd373e0d47851d887b66d35f8ce781
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-07-28 23:56:03 +0100

    feat: Migrate Season of Prophecy

--- packages/utility/source/spirits/seasons/prophecy/prophecy-guide.ts
+++ packages/utility/source/spirits/seasons/prophecy/prophecy-guide.ts
@@ -11,54 +11,71 @@ export default new GuideSpirit({
 	offer: {
 		hasInfographic: false,
 		current: [
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 1 },
-				cosmetic: Cosmetic.ProphecyGuideQuest1,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 1 },
-				cosmetic: Cosmetic.ProphecyGuideHeart1,
-				cost: { candles: 3 },
-			},
-			{ cosmetic: Cosmetic.ProphecyPendant },
-			{
-				cosmetic: Cosmetic.ProphecyGuideDunun,
-				cost: { seasonalHearts: 2 },
-			},
-			{
-				cosmetic: Cosmetic.ProphecyGuideAnubisMask,
-				cost: { seasonalHearts: 2 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 2 },
-				cosmetic: Cosmetic.ProphecyGuideQuest2,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 2 },
-				cosmetic: Cosmetic.ProphecyGuideHeart2,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 3 },
-				cosmetic: Cosmetic.ProphecyGuideQuest3,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 3 },
-				cosmetic: Cosmetic.ProphecyGuideHeart3,
-				cost: { candles: 3 },
-			},
-			{
-				translation: { key: CosmeticCommon.QuestMultiple, number: 4 },
-				cosmetic: Cosmetic.ProphecyGuideQuest4,
-			},
-			{
-				translation: { key: CosmeticCommon.HeartMultiple, number: 4 },
-				cosmetic: Cosmetic.ProphecyGuideHeart4,
-				cost: { candles: 3 },
-			},
-			{
-				cosmetic: Cosmetic.ProphecyGuideFriendActionHug,
-			},
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 1 },
+					cosmetic: Cosmetic.ProphecyGuideQuest1,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 1 },
+					cosmetic: Cosmetic.ProphecyGuideHeart1,
+					cost: { candles: 3 },
+				},
+				{
+					cosmetic: Cosmetic.ProphecyPendant,
+					seasonPass: true,
+					children: [
+						{
+							cosmetic: Cosmetic.ProphecyGuideDunun,
+							cost: { seasonalHearts: 2 },
+							seasonPass: true,
+						},
+						{
+							cosmetic: Cosmetic.ProphecyGuideAnubisMask,
+							cost: { seasonalHearts: 2 },
+							seasonPass: true,
+						},
+					],
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 2 },
+					cosmetic: Cosmetic.ProphecyGuideQuest2,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 2 },
+					cosmetic: Cosmetic.ProphecyGuideHeart2,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 3 },
+					cosmetic: Cosmetic.ProphecyGuideQuest3,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 3 },
+					cosmetic: Cosmetic.ProphecyGuideHeart3,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					translation: { key: CosmeticCommon.QuestMultiple, number: 4 },
+					cosmetic: Cosmetic.ProphecyGuideQuest4,
+				},
+				{
+					translation: { key: CosmeticCommon.HeartMultiple, number: 4 },
+					cosmetic: Cosmetic.ProphecyGuideHeart4,
+					cost: { candles: 3 },
+				},
+			],
+			[
+				{
+					cosmetic: Cosmetic.ProphecyGuideFriendActionHug,
+				},
+			],
 		],
 	},
 });
