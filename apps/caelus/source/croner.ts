2d827afb2cde19f7b260dd8c65e1cf020feb0524
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-08-13 14:18:30 +0400

    feat(croner): Top-level error logging

--- apps/caelus/source/croner.ts
+++ apps/caelus/source/croner.ts
@@ -14,36 +14,40 @@ import { APPLICATION_ID, FLIGHT_CHECK, SUPPORT_SERVER_GUILD_ID } from "./utility
 import { GIVEAWAY_END_DATE } from "./utility/constants.js";
 
 export default function croner() {
-	new Cron("0 0 0 * * *", { timezone: TIME_ZONE }, async () => {
-		const today = skyToday();
-		const guild = GUILD_CACHE.get(SUPPORT_SERVER_GUILD_ID);
+	new Cron(
+		"0 0 0 * * *",
+		{ catch: (error) => pino.error(error, "Error during changing days."), timezone: TIME_ZONE },
+		async () => {
+			const today = skyToday();
+			const guild = GUILD_CACHE.get(SUPPORT_SERVER_GUILD_ID);
 
-		if (!guild) {
-			pino.error("Could not find the support server whilst resetting daily guides.");
-			return;
-		}
+			if (!guild) {
+				pino.error("Could not find the support server whilst resetting daily guides.");
+				return;
+			}
 
-		const me = await guild.fetchMe();
+			const me = await guild.fetchMe();
 
-		await Promise.all([
-			resetDailyGuides({ user: me.user, lastUpdatedAt: today.toJSDate() }),
-			resetDailyGuidesDistribution(),
-		]);
+			await Promise.all([
+				resetDailyGuides({ user: me.user, lastUpdatedAt: today.toJSDate() }),
+				resetDailyGuidesDistribution(),
+			]);
 
-		await distribute({
-			user: me.user,
-			lastUpdatedUserId: APPLICATION_ID,
-			lastUpdatedAt: today.toJSDate(),
-		});
+			await distribute({
+				user: me.user,
+				lastUpdatedUserId: APPLICATION_ID,
+				lastUpdatedAt: today.toJSDate(),
+			});
 
-		if (
-			today.year === GIVEAWAY_END_DATE.year &&
-			today.month === GIVEAWAY_END_DATE.month &&
-			today.day === GIVEAWAY_END_DATE.day
-		) {
-			await end();
-		}
-	});
+			if (
+				today.year === GIVEAWAY_END_DATE.year &&
+				today.month === GIVEAWAY_END_DATE.month &&
+				today.day === GIVEAWAY_END_DATE.day
+			) {
+				await end();
+			}
+		},
+	);
 
 	if (FLIGHT_CHECK) {
 		new Cron("*/5 * * * *", async () => {
