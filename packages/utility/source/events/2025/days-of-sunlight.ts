4184ed55d9aa638db61d125a83b33cb7c0db05e4
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-08-27 23:15:46 +0100

    fix(days-of-sunlight): Amend end date

--- packages/utility/source/events/2025/days-of-sunlight.ts
+++ packages/utility/source/events/2025/days-of-sunlight.ts
@@ -5,5 +5,5 @@ import { EventId } from "../../utility/event.js";
 export default new Event({
 	id: EventId.DaysOfSunlight2025,
 	start: skyDate(2_025, 9, 1),
-	end: skyDate(2_025, 9, 23),
+	end: skyDate(2_025, 9, 22),
 });
