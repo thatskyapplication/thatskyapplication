e306bc79b10427e75f9f3743a963a21dc996712b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-08-27 23:39:21 +0100

    feat(days-of-sunlight): Add patch notes

--- packages/utility/source/events/2025/days-of-sunlight.ts
+++ packages/utility/source/events/2025/days-of-sunlight.ts
@@ -1,9 +1,11 @@
 import { skyDate } from "../../dates.js";
 import { Event } from "../../models/event.js";
+import { patchNotesRoute } from "../../routes.js";
 import { EventId } from "../../utility/event.js";
 
 export default new Event({
 	id: EventId.DaysOfSunlight2025,
 	start: skyDate(2_025, 9, 1),
 	end: skyDate(2_025, 9, 22),
+	patchNotesURL: patchNotesRoute("0305"),
 });
