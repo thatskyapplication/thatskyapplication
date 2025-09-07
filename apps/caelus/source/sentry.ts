36135fde72e28d143a3fd3673b6458dc7ded2707
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-09-07 09:21:12 +0100

    fix(sentry): Set `maxBreadcrumbs` to 10

--- apps/caelus/source/sentry.ts
+++ apps/caelus/source/sentry.ts
@@ -2,5 +2,5 @@ import { init } from "@sentry/node";
 import { PRODUCTION, SENTRY_DATA_SOURCE_NAME } from "./utility/configuration.js";
 
 if (PRODUCTION && SENTRY_DATA_SOURCE_NAME) {
-	init({ dsn: SENTRY_DATA_SOURCE_NAME, sendDefaultPii: true });
+	init({ dsn: SENTRY_DATA_SOURCE_NAME, maxBreadcrumbs: 10, sendDefaultPii: true });
 }
