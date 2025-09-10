f11fbf9f8f7f96f9846de1b304dea2b28131277e
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-09-10 09:15:01 +0100

    fix(kingdom): Fix treasure candle rotation

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -131,8 +131,8 @@ const TREASURE_CANDLES_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/daylight_prairie/2.webp", CDN_URL)),
 	],
 	[RealmName.HiddenForest]: [
-		String(new URL("daily_guides/treasure_candles/hidden_forest/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/hidden_forest/3.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/hidden_forest/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/hidden_forest/2.webp", CDN_URL)),
 	],
 	[RealmName.ValleyOfTriumph]: [
