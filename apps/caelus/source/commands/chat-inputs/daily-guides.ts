92c1885a5d0eb21220bff660b7dbd0034dddc492
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-09-11 15:53:25 +0100

    fix(daily-guides): Remove unused imports

--- apps/caelus/source/commands/chat-inputs/daily-guides.ts
+++ apps/caelus/source/commands/chat-inputs/daily-guides.ts
@@ -2,22 +2,17 @@ import {
 	type APIApplicationCommandAutocompleteInteraction,
 	type APIChatInputApplicationCommandGuildInteraction,
 	type APIChatInputApplicationCommandInteraction,
-	ComponentType,
 	Locale,
-	MessageFlags,
 } from "@discordjs/core";
 import { t } from "i18next";
 import { client } from "../../discord.js";
 import {
 	dailyGuidesResponse,
-	distributionData,
-	fetchDailyGuides,
 	interactive,
 	questAutocomplete,
 	set,
 } from "../../features/daily-guides.js";
-import { SUPPORT_SERVER_GUILD_ID, SUPPORT_SERVER_INVITE_URL } from "../../utility/configuration.js";
-import { INFORMATION_ACCENT_COLOUR } from "../../utility/constants.js";
+import { SUPPORT_SERVER_GUILD_ID } from "../../utility/configuration.js";
 import { OptionResolver } from "../../utility/option-resolver.js";
 
 export default {
