ceded87f8fc0a6de6ddff471209737b11ce3a61d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-04 01:22:34 +0100

    fix(season-of-migration): Add end date assumption

--- packages/utility/source/spirits/seasons/migration/index.ts
+++ packages/utility/source/spirits/seasons/migration/index.ts
@@ -6,8 +6,8 @@ import migrationGuide from "./migration-guide.js";
 export default new Season({
 	id: SeasonId.Migration,
 	start: skyDate(2_025, 10, 20),
-	// @ts-expect-error Not yet announced.
-	end: null,
+	// Assuming 77 days after since this is not yet announced.
+	end: skyDate(2025, 12, 6),
 	guide: migrationGuide,
 	spirits: [],
 });
