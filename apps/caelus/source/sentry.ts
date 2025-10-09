022535f07bdf003b43a6cc6c9c3be798cba842a5
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-09 09:22:56 +0100

    feat(sentry): Pino integration

--- apps/caelus/source/sentry.ts
+++ apps/caelus/source/sentry.ts
@@ -1,6 +1,11 @@
-import { init } from "@sentry/node";
+import { init, pinoIntegration } from "@sentry/node";
 import { PRODUCTION, SENTRY_DATA_SOURCE_NAME } from "./utility/configuration.js";
 
 if (PRODUCTION && SENTRY_DATA_SOURCE_NAME) {
-	init({ dsn: SENTRY_DATA_SOURCE_NAME, maxBreadcrumbs: 10, sendDefaultPii: true });
+	init({
+		dsn: SENTRY_DATA_SOURCE_NAME,
+		integrations: [pinoIntegration({ error: { levels: ["error", "fatal"] } })],
+		maxBreadcrumbs: 10,
+		sendDefaultPii: true,
+	});
 }
