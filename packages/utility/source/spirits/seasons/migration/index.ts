3596fd93e932ca9dd287c35de7e39e37ef726dab
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-14 01:02:37 +0100

    chore(migration): Remove comment
    https://canary.discord.com/channels/914660297963352064/914716752687038514/1427446253800788020

--- packages/utility/source/spirits/seasons/migration/index.ts
+++ packages/utility/source/spirits/seasons/migration/index.ts
@@ -6,7 +6,6 @@ import migrationGuide from "./migration-guide.js";
 export default new Season({
 	id: SeasonId.Migration,
 	start: skyDate(2_025, 10, 20),
-	// Assuming 77 days after since this is not yet announced.
 	end: skyDate(2_026, 1, 5),
 	guide: migrationGuide,
 	spirits: [],
