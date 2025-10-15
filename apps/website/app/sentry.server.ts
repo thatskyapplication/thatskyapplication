dfcb786c41a76dff5c5353bdff6c05d394d66957
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-15 20:25:06 +0100

    fix: Add release

--- apps/website/app/sentry.server.ts
+++ apps/website/app/sentry.server.ts
@@ -1,11 +1,16 @@
 import { init, pinoIntegration } from "@sentry/react-router";
 
-if (process.env.NODE_ENV === "production" && process.env.SENTRY_DATA_SOURCE_NAME) {
+if (
+	process.env.NODE_ENV === "production" &&
+	process.env.SENTRY_DATA_SOURCE_NAME &&
+	process.env.SENTRY_RELEASE
+) {
 	init({
 		dsn: process.env.SENTRY_DATA_SOURCE_NAME,
 		enableLogs: true,
 		integrations: [pinoIntegration({ error: { levels: ["error", "fatal"] } })],
 		maxBreadcrumbs: 25,
 		sendDefaultPii: true,
+		release: process.env.SENTRY_RELEASE,
 	});
 }
