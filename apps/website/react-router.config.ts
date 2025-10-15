0d40de455ae6a54cdd7bc801a8b4f4a3b08d43ba
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-15 13:17:23 +0100

    feat: Website source maps

--- apps/website/react-router.config.ts
+++ apps/website/react-router.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "@react-router/dev/config";
+import { sentryOnBuildEnd } from "@sentry/react-router";
 
 export default {
 	future: { v8_middleware: true },
 	ssr: true,
+	buildEnd: async ({ buildManifest, reactRouterConfig, viteConfig }) =>
+		await sentryOnBuildEnd({ viteConfig, reactRouterConfig, buildManifest }),
 } satisfies Config;
--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,8 +1,27 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { sentryReactRouter } from "@sentry/react-router";
 import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-	plugins: [reactRouterHonoServer(), reactRouter(), tsconfigPaths()],
-});
+export default defineConfig((config) => ({
+	build: {
+		sourcemap: true,
+	},
+	plugins: [
+		reactRouterHonoServer(),
+		reactRouter(),
+		tsconfigPaths(),
+		sentryReactRouter(
+			{
+				org: "thatskyapplication",
+				project: "website",
+				sourceMapsUploadOptions: {
+					enabled: true,
+					filesToDeleteAfterUpload: "**/*.map",
+				},
+			},
+			config,
+		),
+	],
+}));
