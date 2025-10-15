0630046e928ef01ba871ecc861fb27f05d33a4e8
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-15 21:11:17 +0100

    feat(days-of-mischief): Add confirmed end date

--- packages/utility/source/events/2025/days-of-mischief.ts
+++ packages/utility/source/events/2025/days-of-mischief.ts
@@ -5,6 +5,5 @@ import { EventId } from "../../utility/event.js";
 export default new Event({
 	id: EventId.DaysOfMischief2025,
 	start: skyDate(2_025, 10, 27),
-	// Assuming 2 weeks after since this is not yet announced.
-	end: skyDate(2025, 11, 11),
+	end: skyDate(2_025, 11, 17),
 });
