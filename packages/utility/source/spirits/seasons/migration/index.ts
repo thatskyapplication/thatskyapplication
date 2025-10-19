2cf0f5198e2b94623345b82a8f1621d657f23f96
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-19 18:08:22 +0100

    feat(migration): Add seasonal candles rotation

--- packages/utility/source/spirits/seasons/migration/index.ts
+++ packages/utility/source/spirits/seasons/migration/index.ts
@@ -1,7 +1,8 @@
 import { skyDate } from "../../../dates.js";
+import { RealmName } from "../../../kingdom.js";
 import { Season } from "../../../models/season.js";
 import { patchNotesRoute } from "../../../routes.js";
-import { SeasonId } from "../../../season.js";
+import { RotationIdentifier, SeasonId } from "../../../season.js";
 import migrationGuide from "./migration-guide.js";
 
 export default new Season({
@@ -10,5 +11,17 @@ export default new Season({
 	end: skyDate(2_026, 1, 5),
 	guide: migrationGuide,
 	spirits: [],
+	seasonalCandlesRotation: [
+		{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+	],
 	patchNotesURL: patchNotesRoute("31"),
 });
