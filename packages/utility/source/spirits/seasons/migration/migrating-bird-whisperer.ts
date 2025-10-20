bd3b9bb56737ec9f56beed67e197760bd538914a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-20 10:46:38 +0100

    feat(migration): Add realm

--- packages/utility/source/spirits/seasons/migration/migrating-bird-whisperer.ts
+++ packages/utility/source/spirits/seasons/migration/migrating-bird-whisperer.ts
@@ -1,3 +1,4 @@
+import { RealmName } from "../../../kingdom.js";
 import { SeasonalSpirit } from "../../../models/spirits.js";
 import { SeasonId } from "../../../season.js";
 import { SpiritId } from "../../../utility/spirits.js";
@@ -5,6 +6,7 @@ import { SpiritId } from "../../../utility/spirits.js";
 export default new SeasonalSpirit({
 	id: SpiritId.MigratingBirdWhisperer,
 	seasonId: SeasonId.Migration,
+	realm: RealmName.IslesOfDawn,
 	offer: {
 		hasInfographic: false,
 		hasInfographicSeasonal: false,
--- packages/utility/source/spirits/seasons/migration/migration-guide.ts
+++ packages/utility/source/spirits/seasons/migration/migration-guide.ts
@@ -1,3 +1,4 @@
+import { RealmName } from "../../../kingdom.js";
 import { GuideSpirit } from "../../../models/spirits.js";
 import { SeasonId } from "../../../season.js";
 import { SpiritId } from "../../../utility/spirits.js";
@@ -5,6 +6,7 @@ import { SpiritId } from "../../../utility/spirits.js";
 export default new GuideSpirit({
 	id: SpiritId.MigrationGuide,
 	seasonId: SeasonId.Migration,
+	realm: RealmName.IslesOfDawn,
 	offer: {
 		inProgress: true,
 	},
