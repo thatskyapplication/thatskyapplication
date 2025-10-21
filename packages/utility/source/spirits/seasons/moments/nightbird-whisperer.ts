d817541efe49ea4da950bfdc48a06b93572d31c7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-10-21 19:01:52 +0100

    feat(nightbird-whisperer): Add upcoming visit

--- packages/utility/source/spirits/seasons/moments/nightbird-whisperer.ts
+++ packages/utility/source/spirits/seasons/moments/nightbird-whisperer.ts
@@ -1,4 +1,5 @@
 import { Cosmetic, CosmeticCommon } from "../../../cosmetics.js";
+import { skyDate } from "../../../dates.js";
 import { RealmName } from "../../../kingdom.js";
 import { SeasonalSpirit } from "../../../models/spirits.js";
 import { SeasonId } from "../../../season.js";
@@ -67,5 +68,8 @@ export default new SeasonalSpirit({
 				},
 			],
 		],
+	},
+	visits: {
+		travelling: [{ start: skyDate(2025, 10, 23), end: skyDate(2025, 10, 27) }],
 	},
 });
