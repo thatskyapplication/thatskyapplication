c2f0013c17619885cf6fbb3d2a0e10dec6a9df78
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-11-02 08:56:07 +0000

    fix(kingdom): Adjust treasure candles rotation

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -149,8 +149,8 @@ const TREASURE_CANDLES_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/2.webp", CDN_URL)),
 	],
 	[RealmName.VaultOfKnowledge]: [
-		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/2.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/2.webp", CDN_URL)),
 	],
 } as const;
 
