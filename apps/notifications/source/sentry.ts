0fd86e36ad92068002557293f0ba6c5647430580
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-11-04 21:06:34 +0000

    feat: Sentry releases everywhere

--- apps/notifications/source/sentry.ts
+++ apps/notifications/source/sentry.ts
@@ -1,12 +1,13 @@
 import { init, pinoIntegration } from "@sentry/node";
-import { PRODUCTION, SENTRY_DATA_SOURCE_NAME } from "./utility/configuration.js";
+import { PRODUCTION, SENTRY_DATA_SOURCE_NAME, SENTRY_RELEASE } from "./utility/configuration.js";
 
-if (PRODUCTION && SENTRY_DATA_SOURCE_NAME) {
+if (PRODUCTION && SENTRY_DATA_SOURCE_NAME && SENTRY_RELEASE) {
 	init({
 		dsn: SENTRY_DATA_SOURCE_NAME,
 		enableLogs: true,
 		integrations: [pinoIntegration()],
 		maxBreadcrumbs: 10,
 		sendDefaultPii: true,
+		release: SENTRY_RELEASE,
 	});
 }
