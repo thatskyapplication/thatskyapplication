b136d5b26dffed93baffb970f3997b5436f1ea38
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-11-17 09:09:31 +0000

    fix(kingdom): Swap double treasure candles for Vault of Knowledge

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -176,8 +176,8 @@ const TREASURE_CANDLES_DOUBLE_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/1.webp", CDN_URL)),
 	],
 	[RealmName.VaultOfKnowledge]: [
-		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/2.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL)),
 	],
 } as const;
 
