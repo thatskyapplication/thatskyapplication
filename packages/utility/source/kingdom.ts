646825578d084857fddc886c5a3a2664c9a24d07
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-01-08 03:27:12 +0000

    fix(kingdom): Update Daylight Prairie treasure candles

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -150,9 +150,9 @@ const TREASURE_CANDLES_ROTATION = {
 
 const TREASURE_CANDLES_DOUBLE_ROTATION = {
 	[RealmName.DaylightPrairie]: [
+		String(new URL("daily_guides/treasure_candles/daylight_prairie/2.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/daylight_prairie/3.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/daylight_prairie/1.webp", CDN_URL)),
-		String(new URL("daily_guides/treasure_candles/daylight_prairie/2.webp", CDN_URL)),
 	],
 	[RealmName.HiddenForest]: [
 		String(new URL("daily_guides/treasure_candles/hidden_forest/3.webp", CDN_URL)),
