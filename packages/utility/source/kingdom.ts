a8eeb6348215de665fe84f2a30a3c34f2b3f120c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-01-19 08:37:23 +0000

    fix(kingdom): Update treasure candles

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -135,8 +135,8 @@ const TREASURE_CANDLES_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/hidden_forest/3.webp", CDN_URL)),
 	],
 	[RealmName.ValleyOfTriumph]: [
-		String(new URL("daily_guides/treasure_candles/valley_of_triumph/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/valley_of_triumph/2.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/valley_of_triumph/1.webp", CDN_URL)),
 	],
 	[RealmName.GoldenWasteland]: [
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/2.webp", CDN_URL)),
@@ -161,8 +161,8 @@ const TREASURE_CANDLES_DOUBLE_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/hidden_forest/2.webp", CDN_URL)),
 	],
 	[RealmName.ValleyOfTriumph]: [
-		String(new URL("daily_guides/treasure_candles/valley_of_triumph/2.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/valley_of_triumph/1.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/valley_of_triumph/2.webp", CDN_URL)),
 	],
 	[RealmName.GoldenWasteland]: [
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/3.webp", CDN_URL)),
