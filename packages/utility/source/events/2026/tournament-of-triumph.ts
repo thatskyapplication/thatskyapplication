c2dda8a69ca70465659f10a8b325538cd09cba71
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-01-23 01:53:22 +0000

    feat(tournament-of-triumph): Add event tickets

--- packages/utility/source/events/2026/tournament-of-triumph.ts
+++ packages/utility/source/events/2026/tournament-of-triumph.ts
@@ -1,9 +1,32 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 1, 30), end = skyDate(2026, 2, 13);
+	start < end;
+	start = start.plus({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.TournamentOfTriumph2026,
 	start: skyDate(2026, 1, 30),
 	end: skyDate(2026, 2, 13),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 1, 30),
+				end: skyDate(2026, 2, 12),
+			},
+		],
+	},
 });
