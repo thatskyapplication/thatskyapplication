814534f06c787ed2ee000509cd8e663b7c71c9cf
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-02-04 02:15:52 +0000

    fix(days-of-love): Update end date
    Resolves #446.

--- packages/utility/source/events/2026/days-of-love.ts
+++ packages/utility/source/events/2026/days-of-love.ts
@@ -5,6 +5,5 @@ import { EventId } from "../../utility/event.js";
 export default new Event({
 	id: EventId.DaysOfLove2026,
 	start: skyDate(2026, 2, 6),
-	// Double-check this (unannounced).
-	end: skyDate(2026, 2, 24),
+	end: skyDate(2026, 2, 20),
 });
