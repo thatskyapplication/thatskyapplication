7f79a9cb49f31fad0a8e8cb86aa45b1f48f9a897
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-02-12 23:19:44 +0000

    feat(days-of-fortune): Add event tickets

--- packages/utility/source/events/2026/days-of-fortune.ts
+++ packages/utility/source/events/2026/days-of-fortune.ts
@@ -1,9 +1,32 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 2, 13), end = skyDate(2026, 2, 27);
+	start < end;
+	start = start.plus({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.DaysOfFortune2026,
 	start: skyDate(2026, 2, 13),
 	end: skyDate(2026, 2, 27),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 2, 13),
+				end: skyDate(2026, 2, 26),
+			},
+		],
+	},
 });
