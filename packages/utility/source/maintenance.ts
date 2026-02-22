597ef8ecbf5e496febd4e4b71fc1c4ff65f3d500
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-02-22 21:06:06 +0000

    fix(maintenance): Use live maintenance periods

--- packages/utility/source/maintenance.ts
+++ packages/utility/source/maintenance.ts
@@ -5,24 +5,4 @@ export const MAINTENANCE_PERIODS = [
 		start: skyDate(2026, 2, 18, 11),
 		end: skyDate(2026, 2, 18, 15),
 	},
-	{
-		start: skyDate(2026, 2, 21, 3),
-		end: skyDate(2026, 2, 21, 15),
-	},
-	{
-		start: skyDate(2026, 2, 22, 23),
-		end: skyDate(2026, 2, 22, 23, 25),
-	},
-	{
-		start: skyDate(2026, 2, 22, 13, 5),
-		end: skyDate(2026, 2, 22, 14, 0),
-	},
-	{
-		start: skyDate(2026, 2, 23, 0),
-		end: skyDate(2026, 2, 23, 0),
-	},
-	{
-		start: skyDate(2026, 2, 23, 2),
-		end: skyDate(2026, 2, 23, 3),
-	},
 ] as const;
