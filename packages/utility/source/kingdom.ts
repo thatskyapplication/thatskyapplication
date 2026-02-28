93ae5eaff30c5914d5a7d34585ab3fb613218ec1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-02-28 08:48:26 +0000

    fix(kingdom): Adjust double candles again

--- packages/utility/source/kingdom.ts
+++ packages/utility/source/kingdom.ts
@@ -162,17 +162,17 @@ const TREASURE_CANDLES_DOUBLE_ROTATION = {
 		String(new URL("daily_guides/treasure_candles/hidden_forest/1.webp", CDN_URL)),
 	],
 	[RealmName.ValleyOfTriumph]: [
-		String(new URL("daily_guides/treasure_candles/valley_of_triumph/2.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/valley_of_triumph/1.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/valley_of_triumph/2.webp", CDN_URL)),
 	],
 	[RealmName.GoldenWasteland]: [
+		String(new URL("daily_guides/treasure_candles/golden_wasteland/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/2.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/golden_wasteland/3.webp", CDN_URL)),
-		String(new URL("daily_guides/treasure_candles/golden_wasteland/1.webp", CDN_URL)),
 	],
 	[RealmName.VaultOfKnowledge]: [
-		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL)),
 		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/2.webp", CDN_URL)),
+		String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL)),
 	],
 } as const;
 
