c7700ae0fd868937ab2e9330931a5ef377fd43bb
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-02-28 23:00:36 +0000

    feat: Add Sentry to database backups

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ while true; do
 	BACKUP_FILE="/tmp/backup_${TIMESTAMP}.sql.gz"
 
 	echo "[$(date)] Starting backup process..."
+	curl --silent --fail "${SENTRY_CRONS}?status=in_progress" || echo "[$(date)] Warning: Sentry check-in failed."
 
 	if PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_DB | gzip > $BACKUP_FILE; then
 		if [ -s "$BACKUP_FILE" ]; then
@@ -17,17 +18,21 @@ while true; do
 
 				echo "[$(date)] Backup successfully uploaded to R2."
 				rm $BACKUP_FILE
+				curl --silent --fail "${SENTRY_CRONS}?status=ok" || echo "[$(date)] Warning: Sentry check-in failed."
 			else
 				echo "[$(date)] ERROR: Failed to upload backup to R2."
 				rm $BACKUP_FILE
+				curl --silent --fail "${SENTRY_CRONS}?status=error" || echo "[$(date)] Warning: Sentry check-in failed."
 			fi
 		else
 			echo "[$(date)] ERROR: Backup file is empty."
 			rm -f $BACKUP_FILE
+			curl --silent --fail "${SENTRY_CRONS}?status=error" || echo "[$(date)] Warning: Sentry check-in failed."
 		fi
 	else
 		echo "[$(date)] ERROR: pg_dump failed."
 		rm -f $BACKUP_FILE
+		curl --silent --fail "${SENTRY_CRONS}?status=error" || echo "[$(date)] Warning: Sentry check-in failed."
 	fi
 
 	# Wait 8 hours before the next backup.
