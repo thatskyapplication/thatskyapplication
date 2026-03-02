7f340f9b950ef65a1a0ff57024f8648e8ac441b9
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-03-02 14:23:52 +0000

    feat: `--format=custom`

--- database-backup.sh
+++ database-backup.sh
@@ -4,18 +4,18 @@
 while true; do
 	CYCLE_START=$(date +%s)
 	TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-	BACKUP_FILE="/tmp/backup_${TIMESTAMP}.sql.gz"
+	BACKUP_FILE="/tmp/${TIMESTAMP}.dump"
 
 	echo "[$(date)] Starting backup process..."
 	curl --silent --fail "${SENTRY_CRONS}?status=in_progress" || echo "[$(date)] Warning: Sentry check-in failed."
 
-	if PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_DB | gzip > $BACKUP_FILE; then
+	if PGPASSWORD=$POSTGRES_PASSWORD pg_dump --host=$POSTGRES_HOST --username=$POSTGRES_USER --format=custom --file=$BACKUP_FILE $POSTGRES_DB; then
 		if [ -s "$BACKUP_FILE" ]; then
 			echo "[$(date)] Backup file created successfully, uploading to R2..."
 
 			if aws --endpoint-url $AWS_S3_ENDPOINT \
 				s3 cp $BACKUP_FILE \
-				s3://$S3_BUCKET/${TIMESTAMP}.sql.gz; then
+				s3://$S3_BUCKET/${TIMESTAMP}.dump; then
 
 				echo "[$(date)] Backup successfully uploaded to R2."
 				rm $BACKUP_FILE
