42ae31fca46151318442b9c4097a4df611308f4f
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-03-04 11:27:18 +0000

    feat(community): Add new event

--- packages/utility/source/events/community.ts
+++ packages/utility/source/events/community.ts
@@ -6,4 +6,9 @@ export const COMMUNITY_EVENTS = [
 		start: skyDate(2025, 12, 31, 10),
 		marketingURL: "https://discord.gg/aurora-2-0",
 	},
+	{
+		name: "Aurora 2.0 Mega Concert",
+		start: skyDate(2026, 3, 7, 10),
+		marketingURL: "https://discord.gg/aurora-2-0",
+	},
 ] as const;
