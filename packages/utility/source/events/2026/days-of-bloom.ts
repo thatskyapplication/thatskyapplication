d86f47d6cf3e48876af28c7813179024c5a9acde
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-03-15 10:02:03 +0000

    feat(days-of-bloom): Add event tickets

--- packages/utility/source/events/2026/days-of-bloom.ts
+++ packages/utility/source/events/2026/days-of-bloom.ts
@@ -1,10 +1,33 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 3, 13), end = skyDate(2026, 4, 3);
+	start < end;
+	start = start.plus({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.DaysOfBloom2026,
 	name: "days-of-bloom",
 	start: skyDate(2026, 3, 13),
 	end: skyDate(2026, 4, 3),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 3, 13),
+				end: skyDate(2026, 4, 2),
+			},
+		],
+	},
 });
