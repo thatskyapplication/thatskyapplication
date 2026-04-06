b4f454ae62d88a43e0f4c023bfaef61e7974a6e6
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-04-06 21:01:10 +0100

    feat(maintenance): Add upcoming maintenance

--- packages/utility/source/maintenance.ts
+++ packages/utility/source/maintenance.ts
@@ -5,4 +5,8 @@ export const MAINTENANCE_PERIODS = [
 		start: skyDate(2026, 2, 18, 11),
 		end: skyDate(2026, 2, 18, 15),
 	},
+	{
+		start: skyDate(2026, 4, 8, 13),
+		end: skyDate(2026, 4, 8, 14),
+	},
 ] as const;
