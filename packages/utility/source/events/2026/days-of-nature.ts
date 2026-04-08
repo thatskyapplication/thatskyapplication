964165117386707c8ff5ee9a018fa5cf9172293b
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-04-08 01:10:58 +0100

    feat(days-of-nature): Add event tickets

--- packages/utility/source/events/2026/days-of-nature.ts
+++ packages/utility/source/events/2026/days-of-nature.ts
@@ -1,10 +1,33 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 4, 10), end = skyDate(2026, 4, 24);
+	start < end;
+	start = start.plus({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.DaysOfNature2026,
 	name: "days-of-nature",
 	start: skyDate(2026, 4, 10),
 	end: skyDate(2026, 4, 24),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 4, 10),
+				end: skyDate(2026, 4, 23),
+			},
+		],
+	},
 });
