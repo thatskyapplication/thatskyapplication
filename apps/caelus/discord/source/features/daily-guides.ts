76ab5aa10b54b9d0b81ae26bcc3084eea7ea6d46
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-04-30 10:21:30 +0100

    refactor: Dynamic experiment

--- apps/caelus/discord/source/features/daily-guides.ts
+++ apps/caelus/discord/source/features/daily-guides.ts
@@ -563,16 +563,17 @@ export async function dailyGuidesSetupType(
 		throw new Error("Received an unknown distribution type whilst setting up daily guides.");
 	}
 
-	if (
-		type === DailyGuidesDistributionType.Media &&
-		!(guild.id === SUPPORT_SERVER_GUILD_ID || guild.id === "1184485877414645801")
-	) {
-		await client.api.interactions.reply(interaction.id, interaction.token, {
-			content: `This server is not in the experiment for media-style daily guides.\n\nYou can join the [support server](${SUPPORT_SERVER_INVITE_URL}) for more information.`,
-			flags: MessageFlags.Ephemeral,
-		});
+	if (type === DailyGuidesDistributionType.Media) {
+		try {
+			await client.api.guilds.getMember(SUPPORT_SERVER_GUILD_ID, interaction.member.user.id);
+		} catch {
+			await client.api.interactions.reply(interaction.id, interaction.token, {
+				content: `You are not in the experiment for media-style daily guides.\n\nYou can join the [support server](${SUPPORT_SERVER_INVITE_URL}) for more information.`,
+				flags: MessageFlags.Ephemeral,
+			});
 
-		return;
+			return;
+		}
 	}
 
 	await setup({ guildId: interaction.guild_id, type: type ?? DailyGuidesDistributionType.Default });
