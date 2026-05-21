194fc84ca0cd2d8a54410d88b344770c6e1029c1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-05-21 10:22:44 +0100

    fix(reassuring-ranger): Include infographic

--- packages/utility/source/kingdom/seasons/moments/reassuring-ranger.ts
+++ packages/utility/source/kingdom/seasons/moments/reassuring-ranger.ts
@@ -13,7 +13,6 @@ export default new SeasonalSpirit({
 	action,
 	realm: RealmName.DaylightPrairie,
 	offer: {
-		hasInfographic: false,
 		seasonal: [
 			[
 				{ cosmetic: Cosmetic.FriendActionSideHug1 },
