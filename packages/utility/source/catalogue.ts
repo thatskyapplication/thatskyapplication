663edffa5ea44132fc905f68bf9777e45f6b0818
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-05-26 21:33:36 +0100

    fix(catalogue): Do not expose the Spring Clover Sprout

--- packages/utility/source/catalogue.ts
+++ packages/utility/source/catalogue.ts
@@ -83,10 +83,10 @@ const permanentEventStoreItems = resolveOfferFromItems([
 		packName: CosmeticPackName.TranscendentJourneyPack,
 		cost: { money: 24.99 },
 	},
-	{
-		cosmetic: Cosmetic.SpringCloverSprout,
-		cost: { money: 0.99 },
-	},
+	// {
+	// 	cosmetic: Cosmetic.SpringCloverSprout,
+	// 	cost: { money: 0.99 },
+	// },
 	{
 		cosmetic: [Cosmetic.FlOwCape, Cosmetic.FlOwFlower],
 		cosmeticDisplay: Cosmetic.FlOwCape,
