7e19d156f79c9fb76884fb9386a2001b00ec9f28
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-06-05 09:00:55 +0100

    fix(carnival): New seasonal candles rotation

--- packages/utility/source/kingdom/seasons/carnival/index.ts
+++ packages/utility/source/kingdom/seasons/carnival/index.ts
@@ -15,18 +15,32 @@ export default new Season({
 	end: skyDate(2026, 7, 3),
 	guide: carnivalGuide,
 	spirits: [carnivalAthleticDancer, carnivalJuggler, carnivalPuzzleDirector, carnivalStuntActor],
-	seasonalCandlesRotation: [
-		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
-		{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
-		{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
-		{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
-		{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
-	],
+	seasonalCandlesRotation: (now) =>
+		now >= skyDate(2026, 6, 5)
+			? [
+					{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+					{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
+					{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
+					{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
+					{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
+				]
+			: [
+					{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+					{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
+					{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
+					{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
+					{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
+					{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
+				],
 	items: [
 		{
 			translation: { key: CosmeticCommon.BouncePadMultiple, number: 2 },
