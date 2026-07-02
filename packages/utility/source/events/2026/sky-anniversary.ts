9ac14d4cd9ff952e754dcbf75955b1d25f342ff1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-07-02 17:18:43 +0100

    feat(sky-anniversary): Add event tickets

--- packages/utility/source/events/2026/sky-anniversary.ts
+++ packages/utility/source/events/2026/sky-anniversary.ts
@@ -1,10 +1,33 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 7, 3), end = skyDate(2026, 7, 24);
+	start < end;
+	start = start.plus({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.SkyAnniversary2026,
 	name: "sky-anniversary",
 	start: skyDate(2026, 7, 3),
 	end: skyDate(2026, 7, 24),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 7, 3),
+				end: skyDate(2026, 7, 23),
+			},
+		],
+	},
 });
