663c6315cd626ce7cd71785ffed39135c0f4ac36
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-07-14 23:45:06 +0100

    feat(dear-van-gogh): add seasonal candles rotation

--- packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
+++ packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
@@ -1,6 +1,7 @@
 import { skyDate } from "../../../dates.js";
 import { Season } from "../../../models/season.js";
-import { SeasonId } from "../../../season.js";
+import { RotationIdentifier, SeasonId } from "../../../season.js";
+import { RealmName } from "../../geography.js";
 import VanGoghGuide from "./dear-van-gogh-guide.js";
 
 export default new Season({
@@ -9,4 +10,16 @@ export default new Season({
 	end: skyDate(2026, 10, 2),
 	guide: VanGoghGuide,
 	spirits: [],
+	seasonalCandlesRotation: [
+		{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+	],
 });
