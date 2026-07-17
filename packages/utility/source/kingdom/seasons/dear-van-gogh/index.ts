370b275570777a6bbfcb4fcd574861482b56b220
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-07-17 10:17:26 +0100

    fix(dear-van-gogh): expose seasonal candles infographics

--- packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
+++ packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
@@ -15,19 +15,18 @@ export default new Season({
 	end: skyDate(2026, 10, 2),
 	guide: vaseWithFifteenSunflowers,
 	spirits: [dutchMemory, rusticMemory, artisticMemory, joyfulMemory],
-	// Waiting on the wiki.
-	// seasonalCandlesRotation: [
-	// 	{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
-	// 	{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
-	// 	{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
-	// 	{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
-	// 	{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
-	// 	{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
-	// 	{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
-	// 	{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
-	// 	{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
-	// 	{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
-	// ],
+	seasonalCandlesRotation: [
+		{ rotation: RotationIdentifier.Two, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+		{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
+		{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
+		{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
+		{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
+		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+	],
 	items: [
 		{
 			cosmetic: Cosmetic.WheatfieldCape,
