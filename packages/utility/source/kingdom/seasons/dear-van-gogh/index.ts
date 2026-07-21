f2e1c3f9d09906b8500160d262fcbe9da5d6802c
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-07-21 12:31:13 +0100

    fix(dear-van-gogh): swap Vault of Knowledge seasonal candles

--- packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
+++ packages/utility/source/kingdom/seasons/dear-van-gogh/index.ts
@@ -20,12 +20,12 @@ export default new Season({
 		{ rotation: RotationIdentifier.Two, realm: RealmName.HiddenForest },
 		{ rotation: RotationIdentifier.Two, realm: RealmName.ValleyOfTriumph },
 		{ rotation: RotationIdentifier.Two, realm: RealmName.GoldenWasteland },
-		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
+		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
 		{ rotation: RotationIdentifier.One, realm: RealmName.DaylightPrairie },
 		{ rotation: RotationIdentifier.One, realm: RealmName.HiddenForest },
 		{ rotation: RotationIdentifier.One, realm: RealmName.ValleyOfTriumph },
 		{ rotation: RotationIdentifier.One, realm: RealmName.GoldenWasteland },
-		{ rotation: RotationIdentifier.One, realm: RealmName.VaultOfKnowledge },
+		{ rotation: RotationIdentifier.Two, realm: RealmName.VaultOfKnowledge },
 	],
 	items: [
 		{
