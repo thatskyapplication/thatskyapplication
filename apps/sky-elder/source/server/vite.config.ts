87246e2dfcaa923a43f1720e7ded1ab0cd470c2d
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-08-01 00:00:18 +0100

    build: align dependencies

--- apps/sky-elder/source/server/vite.config.ts
+++ apps/sky-elder/source/server/vite.config.ts
@@ -9,12 +9,12 @@ export default defineConfig((config) => ({
 		emptyOutDir: true,
 		target: "ESNext",
 		sourcemap: true,
-		rollupOptions: {
+		rolldownOptions: {
 			external: [...builtinModules],
 			output: {
+				codeSplitting: false,
 				format: "cjs",
 				entryFileNames: "index.cjs",
-				inlineDynamicImports: true,
 			},
 		},
 	},
