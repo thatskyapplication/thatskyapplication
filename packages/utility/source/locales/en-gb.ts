427a5902de4dbcc9ceb5f314dd5a8df8d5c1fef3
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-08-07 00:36:58 +0100

    fix: Summer Camping new URL

--- packages/utility/source/locales/en-gb.ts
+++ packages/utility/source/locales/en-gb.ts
@@ -982,7 +982,7 @@ export default {
 			[EventId.DaysOfSunlight2026]:
 				"https://sky-children-of-the-light.fandom.com/Days_of_Sunlight/2026",
 			[EventId.SummerCamping2026]:
-				"https://sky-children-of-the-light.fandom.com/wiki/Summer_Camping_Event",
+				"https://sky-children-of-the-light.fandom.com/wiki/Summer_Camping",
 		} satisfies Record<EventIds, `https://${string}`>,
 		"sky-profile-winged-light-types": {
 			[SkyProfileWingedLightType.InferFromCatalogue]: "Infer from catalogue",
