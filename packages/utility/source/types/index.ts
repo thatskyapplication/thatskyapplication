b6b4d27ca90da28a6667960596a4e4b0eb1c5ac9
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-08-17 14:03:39 +0100

    fix: `readonly`

--- packages/utility/source/types/index.ts
+++ packages/utility/source/types/index.ts
@@ -12,22 +12,22 @@ export enum VisitType {
 }
 
 export interface BaseVisit<Type extends VisitType = VisitType> {
-	type: Type;
-	visit: number;
-	start: Temporal.ZonedDateTime;
-	end: Temporal.ZonedDateTime;
+	readonly type: Type;
+	readonly visit: number;
+	readonly start: Temporal.ZonedDateTime;
+	readonly end: Temporal.ZonedDateTime;
 }
 
 export interface TravellingSpiritVisit extends BaseVisit<VisitType.Travelling> {
-	spiritId: SpiritIds;
+	readonly spiritId: SpiritIds;
 }
 
 export interface ReturningSpiritVisit extends BaseVisit<VisitType.Returning> {
-	spiritIds: readonly SpiritIds[];
+	readonly spiritIds: readonly SpiritIds[];
 }
 
 export interface ReturningIndividualSpiritVisit extends BaseVisit<VisitType.Returning> {
-	spiritId: SpiritIds;
+	readonly spiritId: SpiritIds;
 }
 
 export type IndividualSpiritVisit = TravellingSpiritVisit | ReturningIndividualSpiritVisit;
