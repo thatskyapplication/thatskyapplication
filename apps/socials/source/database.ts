bb834812227d6cf3ef9f03bbbde67bb3946e121a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-08-28 19:55:04 +0100

    fix: log pool errors

--- apps/socials/source/database.ts
+++ apps/socials/source/database.ts
@@ -1,8 +1,10 @@
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
 import type { DB } from "@thatskyapplication/utility";
+import pino from "./pino.js";
 import { DATABASE_URL } from "./utility/configuration.js";
 
-export default new Kysely<DB>({
-	dialect: new PostgresDialect({ pool: new Pool({ connectionString: DATABASE_URL }) }),
-});
+const pool = new Pool({ connectionString: DATABASE_URL });
+pool.on("error", (error) => pino.error(error, "Database pool error."));
+
+export default new Kysely<DB>({ dialect: new PostgresDialect({ pool }) });
--- apps/website/app/database.server.ts
+++ apps/website/app/database.server.ts
@@ -2,13 +2,14 @@ import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg";
 import type { DB } from "@thatskyapplication/utility";
 import { DATABASE_URL } from "./config.server";
+import pino from "./pino";
 
-export default new Kysely<DB>({
-	dialect: new PostgresDialect({
-		pool: new Pool({
-			connectionString: DATABASE_URL,
-			connectionTimeoutMillis: 5000,
-			idle_in_transaction_session_timeout: 30000,
-		}),
-	}),
+const pool = new Pool({
+	connectionString: DATABASE_URL,
+	connectionTimeoutMillis: 5000,
+	idle_in_transaction_session_timeout: 30000,
 });
+
+pool.on("error", (error) => pino.error(error, "Database pool error."));
+
+export default new Kysely<DB>({ dialect: new PostgresDialect({ pool }) });
