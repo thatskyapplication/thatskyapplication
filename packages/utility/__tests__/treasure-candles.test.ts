3c26abd6988ab8e2a93148d2297125eb552d6883
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-09-05 08:26:31 +0100

    fix(treasure-candles): new rotation

--- packages/utility/__tests__/treasure-candles.test.ts
+++ packages/utility/__tests__/treasure-candles.test.ts
@@ -843,6 +843,22 @@ const EXPECTED_ROTATIONS = [
 			String(new URL("daily_guides/treasure_candles/valley_of_triumph/1.webp", CDN_URL)),
 		],
 	},
+	{
+		date: skyDate(2026, 9, 2),
+		expected: [String(new URL("daily_guides/treasure_candles/golden_wasteland/2.webp", CDN_URL))],
+	},
+	{
+		date: skyDate(2026, 9, 3),
+		expected: [String(new URL("daily_guides/treasure_candles/vault_of_knowledge/1.webp", CDN_URL))],
+	},
+	{
+		date: skyDate(2026, 9, 4),
+		expected: [String(new URL("daily_guides/treasure_candles/daylight_prairie/1.webp", CDN_URL))],
+	},
+	{
+		date: skyDate(2026, 9, 5),
+		expected: [String(new URL("daily_guides/treasure_candles/hidden_forest/2.webp", CDN_URL))],
+	},
 ] as const;
 
 test("Treasure candles rotations.", async (t) => {
