8e0058f125aaff13568cd821bcb2c18091e807cc
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-09-11 19:12:35 +0100

    fix(lightseekers): double seasonal light correction

--- packages/utility/source/kingdom/seasons/lightseekers/index.ts
+++ packages/utility/source/kingdom/seasons/lightseekers/index.ts
@@ -28,5 +28,8 @@ export default new Season({
 		{ translation: CosmeticCommon.UltimateProp, cosmetic: Cosmetic.LightseekerUltimateProp },
 	],
 	seasonalCandlesRotation: null,
-	doubleSeasonalLight: [{ start: skyDate(2019, 10, 22), end: skyDate(2019, 10, 30) }],
+	doubleSeasonalLight: [
+		{ start: skyDate(2019, 10, 22), end: skyDate(2019, 10, 30) },
+		{ start: skyDate(2019, 11, 10), end: skyDate(2019, 11, 11) },
+	],
 });
