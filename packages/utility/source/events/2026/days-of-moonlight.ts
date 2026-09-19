a10ab28f0a7be651c28c17b85eaa21e30e2c56f3
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-09-19 09:56:06 +0100

    feat(days-of-moonlight): add event tickets

--- packages/utility/source/events/2026/days-of-moonlight.ts
+++ packages/utility/source/events/2026/days-of-moonlight.ts
@@ -1,6 +1,19 @@
 import { skyDate } from "../../dates.js";
-import { Event } from "../../models/event.js";
+import { Event, type EventTicketsAmountData } from "../../models/event.js";
 import { EventFamilyId, EventId } from "../../utility/event.js";
+
+const eventTicketAmount: EventTicketsAmountData[] = [];
+
+for (
+	let start = skyDate(2026, 9, 19), end = skyDate(2026, 10, 10);
+	Temporal.ZonedDateTime.compare(start, end) < 0;
+	start = start.add({ days: 1 })
+) {
+	eventTicketAmount.push({
+		date: start,
+		amount: 5,
+	});
+}
 
 export default new Event({
 	id: EventId.DaysOfMoonlight2026,
@@ -8,4 +21,14 @@ export default new Event({
 	family: EventFamilyId.DaysOfMoonlight,
 	start: skyDate(2026, 9, 19),
 	end: skyDate(2026, 10, 10),
+	eventTickets: {
+		amount: eventTicketAmount,
+		pool: [
+			{
+				amount: 15,
+				start: skyDate(2026, 9, 19),
+				end: skyDate(2026, 10, 9),
+			},
+		],
+	},
 });
